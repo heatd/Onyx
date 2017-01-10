14e0b0628ae8256c382e7f9f3c5985ed167c92c1
Pedro Falcato <pedro.falcato@gmail.com>
2017-01-10 15:53:40 +0000

    Reorganized the kernel's file structure.
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

---
+++ kernel/kernel/mm/page.c
@@ -0,0 +1,11 @@
+/*----------------------------------------------------------------------
+ * Copyright (C) 2016, 2017 Pedro Falcato
+ *
+ * This file is part of Spartix, and is made available under
+ * the terms of the GNU General Public License version 2.
+ *
+ * You can redistribute it and/or modify it under the terms of the GNU
+ * General Public License version 2 as published by the Free Software
+ * Foundation.
+ *----------------------------------------------------------------------*/
+//#include <kernel/page.h>
