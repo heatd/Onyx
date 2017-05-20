c41c9ab057373f8f98997b46fa1dfe29d530a11c
Pedro Falcato <pedro.falcato@gmail.com>
2017-05-20 21:47:41 +0100

    input: Add better input management.
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/kernel/input_manager.c
+++ kernel/kernel/input_manager.c
@@ -19,27 +19,55 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+#include <kernel/compiler.h>
 #include <kernel/log.h>
 #include <kernel/tty.h>
 #include <kernel/irq.h>
 #include <kernel/keys.h>
-unsigned int keys[200] =
+unsigned int default_keymap[200] =
 { 
-	0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\b',
-	'\t',
-	'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
-	0, 'a', 's', 'd', 'f', 'g', 'h',
-	'j', 'k', 'l', ';', '\'', '`', 0, '\\', 'z', 'x', 'c', 'v', 'b',
-	'n', 'm', ',', '.', '/', 0, '*', 0,
-	' ', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '7', '8', '9', '-',
-	'4', '5', '6', '+', '1', '2', '3', '0',
-	'.', 0, 0, 0, 0, 0
+	KEYMAP_KEY_ESC, '1', '2', '3', '4', '5',
+	 '6', '7', '8', '9', '0', '-', '=', '\b', '\t',
+	'q', 'w', 'e', 'r', 't', 'y', 'u', 'i',
+	 'o', 'p', '[', ']', '\n', KEYMAP_KEY_LCTRL, 
+	 'a', 's', 'd', 'f', 'g', 'h',
+	'j', 'k', 'l', ';', '\'', '`', KEYMAP_KEY_LSHIFT, '\\', 'z', 'x', 'c', 'v', 'b',
+	'n', 'm', ',', '.', '/', KEYMAP_KEY_RSHIFT, '*', KEYMAP_KEY_ALT,
+	' ', KEYMAP_KEY_CAPS_LOCK, KEYMAP_KEY_F1, KEYMAP_KEY_F2, KEYMAP_KEY_F3, KEYMAP_KEY_F4, 
+	KEYMAP_KEY_F5, KEYMAP_KEY_F6, KEYMAP_KEY_F7, KEYMAP_KEY_F8, KEYMAP_KEY_F9, 
+	KEYMAP_KEY_F10, KEYMAP_KEY_KEYPAD_NUMLCK,
+	KEYMAP_KEY_KEYPAD_SCRLK, '7', '8', '9', '-',
+	'4', '5', '6', '+',
+	'1', '2', '3', '0',
+	'.', 0, 0, 0, KEYMAP_KEY_F11, KEYMAP_KEY_F12
 };
-char num_shft[] = {'!','\"','#','$','%','&','/','(',')','='};
-struct input_state
+unsigned int default_shift_keymap[200] =
+{ 
+	KEYMAP_KEY_ESC, '!', '@', '#', '$', '%',
+	 '^', '&', '*', '(', ')', '_', '+', '\b', '\t',
+	'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I',
+	 'O', 'P', '{', '}', '\n', KEYMAP_KEY_LCTRL, 
+	 'A', 'S', 'D', 'F', 'G', 'H',
+	'J', 'K', 'L', ':', '\"', '`', KEYMAP_KEY_LSHIFT, '|', 'Z', 'X', 'C', 'V', 'B',
+	'N', 'M', '<', '>', '?', KEYMAP_KEY_RSHIFT, '*', KEYMAP_KEY_ALT,
+	' ', KEYMAP_KEY_CAPS_LOCK, KEYMAP_KEY_F1, KEYMAP_KEY_F2, KEYMAP_KEY_F3, KEYMAP_KEY_F4, 
+	KEYMAP_KEY_F5, KEYMAP_KEY_F6, KEYMAP_KEY_F7, KEYMAP_KEY_F8, KEYMAP_KEY_F9, 
+	KEYMAP_KEY_F10, KEYMAP_KEY_KEYPAD_NUMLCK,
+	KEYMAP_KEY_KEYPAD_SCRLK, '7', '8', '9', '-',
+	'4', '5', '6', '+',
+	'1', '2', '3', '0',
+	'.', 0, 0, 0, KEYMAP_KEY_F11, KEYMAP_KEY_F12
+};
+
+struct keymap keymap = 
+{
+	.keymap = default_keymap,
+	.shift_keymap = default_shift_keymap
+};
+static struct input_state
 {
 	_Bool lshift_pressed;
-	_Bool caps_pressed;
+	_Bool caps_enabled;
 	_Bool lctrl_pressed;
 	_Bool fn_pressed;
 	_Bool winkey_pressed;
@@ -49,32 +77,84 @@ struct input_state
 	_Bool rctrl_pressed;
 	_Bool rshift_pressed;
 	_Bool shift_pressed;
-	int key_pressed;
-} input_state;
+	unsigned int key_pressed;
+} input_state = {0};
 
 void input_callback(void *payload, size_t payload_size);
 void send_event_to_kernel(uint8_t keycode)
 {
-	if(keycode == 0x2A || keycode == 0x36)
-	{
-		input_state.shift_pressed = true;
-		return;
-	}
-	if(keycode == 0xAA || keycode == 0xB6)
-	{
-		input_state.shift_pressed = false;
-		return;
-	}
-	if(keycode & 0x80)
-		return;
 	if(irq_schedule_work(input_callback, sizeof(uint8_t), &keycode))
 	{
 		ERROR("ps2keyb", "Couldn't schedule IRQ work!\n");
 	}
 }
+unsigned int input_process_special_key(unsigned int key, _Bool is_release)
+{
+	/* Simple way to shorten the code instead of a huge if statement */
+	_Bool keystate = is_release ? false : true;
+	input_state.key_pressed = is_release ? 0 : key;
+	switch(key)
+	{
+		case KEYMAP_KEY_LSHIFT:
+			input_state.lshift_pressed = keystate;
+			input_state.shift_pressed = keystate;
+			break;
+		case KEYMAP_KEY_RSHIFT:
+			input_state.lshift_pressed = keystate;
+			input_state.shift_pressed = keystate;
+			break;
+		case KEYMAP_KEY_LCTRL:
+			input_state.lctrl_pressed = keystate;
+			break;
+		case KEYMAP_KEY_FN:
+			input_state.fn_pressed = keystate;
+			break;
+		case KEYMAP_KEY_WINKEY:
+			input_state.winkey_pressed = keystate;
+			break;
+		case KEYMAP_KEY_ALT:
+			input_state.lalt_pressed = keystate;
+			break;
+		case KEYMAP_KEY_ALTGR:
+			input_state.altgr_pressed = keystate;
+			break;
+		case KEYMAP_KEY_MENU:
+			input_state.menukey_pressed = keystate;
+			break;
+		case KEYMAP_KEY_RCTRL:
+			input_state.rctrl_pressed = keystate;
+			break;
+		default:
+			if(is_release)
+				return (unsigned int) -1; 
+			return key;
+	}
+	return (unsigned int) -1;
+}
+/* Process the keypress and return -1 if it's a non-printable key */
+unsigned int input_process_keypress(uint8_t keycode)
+{
+	_Bool is_release = false;
+	if(keycode & 0x80)
+		is_release = true;
+	keycode &= 0x7F;
+
+	unsigned int key = 0;
+
+	/* TODO: Add alt support */
+	if(input_state.shift_pressed || input_state.caps_enabled) key = keymap.shift_keymap[keycode-1];
+	else key = keymap.keymap[keycode-1];
+
+	return input_process_special_key(key, is_release);
+}
 void input_callback(void *payload, size_t payload_size)
 {
 	uint8_t keycode = *(uint8_t*) payload;
-	printk("Keycode: %x\n", keycode);
-	tty_recieved_character(0);
+	unsigned int key = input_process_keypress(keycode);
+	if(key != (unsigned int) -1)
+		tty_recieved_character((char) key);
+}
+__init void init_input_state(void)
+{
+	memset(&input_state, 0, sizeof(struct input_state));
 }
