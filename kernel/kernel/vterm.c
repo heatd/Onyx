cddd84f3a4b035b859b96425b607789465f59ef0
heatd <pedro.falcato@gmail.com>
2019-08-13 04:25:33 +0100

    vterm: Use vmalloc.
    Signed-off-by: heatd <pedro.falcato@gmail.com>

--- kernel/kernel/vterm.c
+++ kernel/kernel/vterm.c
@@ -959,8 +959,9 @@ void vterm_init(struct tty *tty)
 	vt->columns = fb->width / font->width;
 	vt->rows = fb->height / font->height;
 	vt->fb = fb;
-	vt->cells = zalloc(vt->columns * vt->rows
-		* sizeof(*vt->cells));
+	vt->cells = vmalloc(vm_align_size_to_pages(vt->columns * vt->rows
+		* sizeof(*vt->cells)), VM_TYPE_REGULAR, VM_WRITE | VM_NOEXEC);
+	assert(vt->cells != NULL);
 
 	vt->fg = default_fg;
 	vt->bg = default_bg;
