ee75cc1f29481a0d1306c242c539ef643211c073
heatd <pedro.falcato@gmail.com>
2020-09-16 18:52:17 +0100

    dash: Fix dash build.
    Signed-off-by: heatd <pedro.falcato@gmail.com>

--- scripts/setup_build.sh
+++ scripts/setup_build.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
+
+if [ "$SYSROOT" = "" ]; then
+	SYSROOT=$PWD/sysroot
+fi
+
 cd usystem
+
 gn gen out/
+
+cd dash
+./configure --prefix=/ --bindir=/usr/bin --host=x86_64-onyx --enable-static
+cd ..
+
 cd ..
 
