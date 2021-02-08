75337b50235d2ad7a282a00e87a525a965b2fb97
heatd <pedro.falcato@gmail.com>
2021-02-08 03:38:57 +0000

    buildpkg: Explicitly require bash.
    Signed-off-by: heatd <pedro.falcato@gmail.com>

--- scripts/setup_build.sh
+++ scripts/setup_build.sh
@@ -8,8 +8,13 @@ if [ "$BUILDPKG_BIN" = "" ]; then
 	BUILDPKG_BIN=$PWD/buildpkg/buildpkg
 fi
 
+if [ "$BUILDPKG_BIN_PY_WRAPPER" = "" ]; then
+	BUILDPKG_BIN_PY_WRAPPER=$PWD/buildpkg/buildpkg_gn_wrapper
+fi
+
 export SYSROOT
 export BUILDPKG_BIN
+export BUILDPKG_BIN_PY_WRAPPER
 
 cd usystem
 
