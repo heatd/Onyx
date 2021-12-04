807e30ef5f7fa5482ba8835aea36c2232e6ba8c8
Pedro Falcato <pedro.falcato@gmail.com>
2021-12-04 07:15:00 +0000

    Add scripts to automate disk image creation
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- scripts/create_standard_fs.sh
+++ scripts/create_standard_fs.sh
@@ -1,6 +1,43 @@
 #!/bin/sh
+set -e
+
+TEMP=$(getopt -o "" --long 'no-strip,no-c++' -n 'create_standard_fs.sh' -- "$@")
+
+eval set -- "$TEMP"
+
+strip_bins="1"
+no_cxx=0
+
+unset TEMP
+
+while true; do
+	case "$1" in
+		'--no-strip')
+			strip_bins="0"
+			shift
+		;;
+		'--no-c++')
+			no_cxx="1"
+			shift
+		;;
+		'--')
+			shift
+			break
+		;;
+	esac
+done
+
+
+export ONYX_ARCH=$(./scripts/onyx_arch.sh)
+export HOST=$(./scripts/arch-to-host.sh $ONYX_ARCH)
+
+. scripts/toolchain/detect_toolchain.sh
 
 MNTROOT=$1
+
+if [ -d "$MNTROOT" ]; then
+    rm -rf "$MNTROOT"
+fi
 
 mkdir -p $MNTROOT/usr
 mkdir -p $MNTROOT/root
@@ -21,4 +58,24 @@ ln -sf usr/bin $MNTROOT/bin
 
 cp defaults/* $MNTROOT/etc
 
-scripts/install_sysroot_to_mnt.sh $MNTROOT
+cp -rv sysroot/* "$MNTROOT"
+
+COMPILER_SLIBS_ARGS=
+
+if [ "$strip_bins" = "1" ]; then
+    COMPILER_SLIBS_ARGS="$COMPILER_SLIBS_ARGS --strip"
+fi
+
+if [ "$no_cxx" = "1" ]; then
+    COMPILER_SLIBS_ARGS="$COMPILER_SLIBS_ARGS --no-cxx"
+fi
+
+./scripts/install_compiler_slibs.sh "$MNTROOT" "$COMPILER_SLIBS_ARGS"
+
+if [ "$strip_bins" = "1" ]; then
+    dirs="usr/bin sbin usr/lib"
+
+    for dir in $dirs; do
+        find "$MNTROOT/$dir" -type f -exec sh -c '(! echo {} | grep -q .*[.]o) && (file {} | grep ELF)' \; -exec "$STRIP" {} \; || true
+    done
+fi
