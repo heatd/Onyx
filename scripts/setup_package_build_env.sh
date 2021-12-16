1b48c26bb6df52383740cf2be127c86acaf37fb1
Pedro Falcato <pedro.falcato@gmail.com>
2021-12-16 15:09:37 +0000

    setup_package_build_env: Force --sysroot in CC, CXX
    Provide variables for cross compilation prefix and
    a variable signaling if we're using LLVM.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- scripts/setup_package_build_env.sh
+++ scripts/setup_package_build_env.sh
@@ -2,13 +2,14 @@ if [ -z "$SYSROOT" ]; then
 	echo "Error: SYSROOT must be set!"
 fi
 
-export HOST=$ONYX_ARCH-onyx
+export HOST=$ONYX_TARGET
 
 if [ -z "$CLANG_PATH" ]; then
-	export CC=$HOST-gcc
-	export CXX=$HOST-g++
-	export LD=$HOST-ld
+	export CC="$HOST-gcc --sysroot=$SYSROOT"
+	export CXX="$HOST-g++ --sysroot=$SYSROOT"
+	export LD="$HOST-ld --sysroot=$SYSROOT"
 	export STRIP=$HOST-strip
+	export ONYX_CROSS_COMPILE_PREFIX=$HOST-
 else
 
 export ONYX_CLANG_ARGS="--target=$ONYX_ARCH-unknown-onyx --sysroot=$SYSROOT"
@@ -20,5 +21,7 @@ export READELF="${CLANG_PATH}/bin/llvm-readelf"
 export LD="${CLANG_PATH}/bin/ld.lld --sysroot=$SYSROOT"
 export OBJDUMP="${CLANG_PATH}/bin/llvm-objdump"
 export RANLIB="${CLANG_PATH}/bin/llvm-ranlib"
+export ONYX_USING_LLVM="yes"
+export ONYX_CROSS_COMPILE_PREFIX="${CLANG_PATH}/bin/"
 
 fi
