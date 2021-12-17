44a5fdf5f9e272f578020e695019b6f05a00f8fa
Pedro Falcato <pedro.falcato@gmail.com>
2021-12-17 12:31:09 +0000

    system_benchmark: Add IO write benchmark
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

---
+++ usystem/tests/system_benchmark/src/io_write.cpp
@@ -0,0 +1,45 @@
+/*
+* Copyright (c) 2021 Pedro Falcato
+* This file is part of Onyx, and is released under the terms of the MIT License
+* check LICENSE at the root directory for more information
+*/
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdexcept>
+#include <vector>
+
+#include <sys/random.h>
+
+#include <benchmark/benchmark.h>
+
+static void io_write_seq_bench(benchmark::State& state)
+{
+    int fd = open("tmpfile", O_RDWR | O_CREAT | O_EXCL);
+
+    if (fd < 0)
+    {
+        throw std::runtime_error("Failed to open fd");
+    }
+    
+    unlink("tmpfile");
+
+    size_t bytes_written = 0;
+
+    std::vector<char> page(state.range(), 0);
+
+    if (getrandom(page.data(), page.size(), 0) < 0)
+        throw std::runtime_error("Failed to get random");
+
+    while (state.KeepRunning())
+    {
+        if (write(fd, page.data(), page.size()) < 0)
+            throw std::runtime_error("Failed to write");
+        bytes_written += page.size();
+    }
+
+    state.SetBytesProcessed(bytes_written);
+
+    close(fd);
+}
+
+BENCHMARK(io_write_seq_bench)->RangeMultiplier(2)->Range(8, 8<<13);
