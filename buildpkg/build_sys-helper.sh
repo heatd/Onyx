dfa93753afb263cfca32eeb725355b0fab2fde4c
Pedro Falcato <pedro.falcato@gmail.com>
2022-01-15 10:07:56 +0000

    ci: Build Onyx ports in nightlies
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- buildpkg/build_sys-helper.sh
+++ buildpkg/build_sys-helper.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-source ${ONYX_ROOT}/scripts/setup_package_build_env.sh
+. ${ONYX_ROOT}/scripts/setup_package_build_env.sh
 
 exec ${ONYX_ROOT}/buildpkg/buildpkg $1 build+package
--- scripts/setup_package_build_env.sh
+++ scripts/setup_package_build_env.sh
@@ -5,23 +5,31 @@ fi
 export HOST=$ONYX_TARGET
 
 if [ -z "$CLANG_PATH" ]; then
-	export CC="$HOST-gcc --sysroot=$SYSROOT"
-	export CXX="$HOST-g++ --sysroot=$SYSROOT"
-	export LD="$HOST-ld --sysroot=$SYSROOT"
+	export CC="$HOST-gcc"
+	export CXX="$HOST-g++"
+	export LD="$HOST-ld"
+	export CFLAGS="--sysroot=$SYSROOT"
+	export CXXFLAGS=$CFLAGS
+	export CPPFLAGS=$CFLAGS
+	export LDFLAGS=$CFLAGS
 	export STRIP=$HOST-strip
 	export ONYX_CROSS_COMPILE_PREFIX=$HOST-
 else
 
 export ONYX_CLANG_ARGS="--target=$ONYX_ARCH-unknown-onyx --sysroot=$SYSROOT"
-export CC="${CLANG_PATH}/bin/clang $ONYX_CLANG_ARGS"
-export CXX="${CLANG_PATH}/bin/clang++ $ONYX_CLANG_ARGS"
+export CC="${CLANG_PATH}/bin/clang"
+export CXX="${CLANG_PATH}/bin/clang++"
 export AR="${CLANG_PATH}/bin/llvm-ar"
 export STRIP="${CLANG_PATH}/bin/llvm-strip"
 export READELF="${CLANG_PATH}/bin/llvm-readelf"
-export LD="${CLANG_PATH}/bin/ld.lld --sysroot=$SYSROOT"
+export LD="${CLANG_PATH}/bin/ld.lld"
 export OBJDUMP="${CLANG_PATH}/bin/llvm-objdump"
 export RANLIB="${CLANG_PATH}/bin/llvm-ranlib"
 export ONYX_USING_LLVM="yes"
+export CFLAGS="$ONYX_CLANG_ARGS"
+export CPPFLAGS=$ONYX_CLANG_ARGS
+export CXXFLAGS="$ONYX_CLANG_ARGS"
+export LDFLAGS="--sysroot=$SYSROOT"
 export ONYX_CROSS_COMPILE_PREFIX="${CLANG_PATH}/bin/"
 
 fi
