f635ae377361d6f4977d94a5787480c2876f0b96
Pedro Falcato <pedro.falcato@gmail.com>
2022-08-21 05:05:29 +0100

    system_benchmark: Add fork benchmark
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

---
+++ usystem/tests/system_benchmark/src/fork.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2022 Pedro Falcato
+ * This file is part of Onyx, and is released under the terms of the MIT License
+ * check LICENSE at the root directory for more information
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <unistd.h>
+
+#include <benchmark/benchmark.h>
+
+static void fork_bench(benchmark::State& state)
+{
+    while (state.KeepRunning())
+    {
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            throw std::runtime_error("Failed to open fd");
+        }
+        else if (pid == 0)
+        {
+            _exit(0);
+        }
+    }
+}
+
+BENCHMARK(fork_bench);
