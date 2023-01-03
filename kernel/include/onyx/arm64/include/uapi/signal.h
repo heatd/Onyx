0b855abf9fd0444eef146406320635f0af42f2eb
Pedro Falcato <pedro.falcato@gmail.com>
2023-01-03 01:25:01 +0000

    arm64: Fix build
    Provide a stub signal.h that allows compilation.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

---
+++ kernel/include/onyx/arm64/include/uapi/signal.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023 Pedro Falcato
+ *
+ * This file is part of Onyx, and is released under the terms of the MIT License
+ * check LICENSE at the root directory for more information
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef _ONYX_UAPI_SIGNAL_H
+#define _ONYX_UAPI_SIGNAL_H
+
+/* Stub signal.h to satisfy header deps for in-progress architectures. */
+
+typedef struct mcontext
+{
+    unsigned char dummy;
+} mcontext_t;
+
+#endif
