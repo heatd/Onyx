486bdb1dc7bc9f462a754383ce0a8ba6a293ca29
Pedro Falcato <pedro.falcato@gmail.com>
2023-01-03 02:43:30 +0000

    riscv: Add vfork
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/arch/riscv64/syscall.cpp
+++ kernel/arch/riscv64/syscall.cpp
@@ -25,7 +25,7 @@ extern syscall_callback_t syscall_table_64[];
 long do_syscall64(registers_t *frame)
 {
     /* In case of a fork or sigreturn, adjust %rdi so it points to the frame */
-    if (frame->a7 == SYS_fork || frame->a7 == SYS_sigreturn)
+    if (frame->a7 == SYS_fork || frame->a7 == SYS_sigreturn || frame->a7 == SYS_vfork)
         frame->a0 = (unsigned long) frame;
 
     /* sigaltstack's implementation requires the syscall frame as the 3rd argument */
