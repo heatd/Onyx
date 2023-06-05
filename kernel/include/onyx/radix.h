de85eaf10a48ab30d8082a5e9fad3b28f0d65cdc
Pedro Falcato <pedro.falcato@gmail.com>
2023-06-05 06:32:11 +0100

    radix: Add marks
    Add marks, similar to xarray marks, that can be set on non-NULL entries,
    propagating up the tree and allowing for efficient discovery of
    particular tagged entries.
    
    The regular iteration code now makes use of this system for both
    iterations on tags and normal iteration, through the use of a special
    PRESENT tag that is implicitly maintained by radix tree store() functionality.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/include/onyx/radix.h
+++ kernel/include/onyx/radix.h
@@ -11,6 +11,7 @@
 #include <stddef.h>
 
 #include <onyx/assert.h>
+#include <onyx/compiler.h>
 
 #include <onyx/expected.hpp>
 
@@ -25,16 +26,38 @@ constexpr size_t rt_nr_entries = 64;
 constexpr size_t rt_entry_shift = 6;
 constexpr size_t rt_entry_mask = rt_nr_entries - 1;
 constexpr size_t rt_max_order = 11;
+constexpr size_t nr_marks = 5;
 
 } // namespace radix
+
+/* We can have marks, and mark entries as such. These marks get propagated up the tree, such that we
+ * can efficiently search for such entries. We also have a special mark called PRESENT, hidden from
+ * users, that lets us easily search the tree for present entries.
+ */
+#define RA_MARK_0       0
+#define RA_MARK_1       1
+#define RA_MARK_2       2
+#define RA_MARK_3       3
+#define RA_MARK_PRESENT 4
 
 using radix::rt_entry_t;
 
 struct radix_tree_node
 {
+    static constexpr unsigned int marks_nr_entries =
+        radix::rt_nr_entries / sizeof(unsigned long) / 8;
+
     rt_entry_t entries[radix::rt_nr_entries];
     struct radix_tree_node *parent;
     unsigned long offset;
+    unsigned long marks[radix::nr_marks][marks_nr_entries];
+
+    bool mark_empty(unsigned int mark);
+    __attribute__((always_inline)) inline bool check_mark(unsigned int mark, unsigned int entry)
+    {
+        constexpr unsigned int bpw = sizeof(unsigned long) * 8;
+        return marks[mark][entry / bpw] & (1UL << (entry % bpw));
+    }
 };
 
 class radix_tree
@@ -70,6 +93,27 @@ private:
      */
     expected<radix_tree_node *, int> copy_level(int level, const radix_tree_node *table,
                                                 copy_cb_t cb, void *ctx);
+
+    /**
+     * @brief Given a table and the index in that table, propagate a tag up the tree
+     *
+     * @param table Tree node
+     * @param tabindex Index in the table
+     * @param mark Mark to propagate
+     * @param set True if we should set, else unset
+     */
+    static void propagate_tag(radix_tree_node *table, unsigned int tabindex, unsigned int mark,
+                              bool set);
+
+    /**
+     * @brief Given a table and the index in that table, clear the tags and propagate them up the
+     * tree.
+     * Supposed to be used when clearing entries.
+     *
+     * @param table Tree node
+     * @param tabindex Index in the table
+     */
+    static void clear_all_tags(radix_tree_node *table, unsigned int tabindex);
 
 public:
     radix_tree() = default;
@@ -150,13 +194,33 @@ public:
         unsigned long current_location{0};
         unsigned long end;
         int depth{0};
+        unsigned int mark{RA_MARK_PRESENT};
 
         cursor(radix_tree *tree, unsigned long end = -1ul) : tree_{tree}, end{end}
         {
         }
 
+        /**
+         * @brief Find the next index to the given mark
+         *
+         * @return Next index. If not found, returns radix::nr_entries.
+         */
+        unsigned int find_next_index();
+
+        /**
+         * @brief Move the index to the next valid one
+         *
+         */
+        void move_index();
+
     public:
-        static cursor from_range(radix_tree *tree, unsigned long start, unsigned long end = -1ul);
+        static cursor from_range_on_marks(radix_tree *tree, unsigned int mark, unsigned long start,
+                                          unsigned long end = -1ul);
+
+        static cursor from_range(radix_tree *tree, unsigned long start, unsigned long end = -1ul)
+        {
+            return from_range_on_marks(tree, RA_MARK_PRESENT, start, end);
+        }
 
         static cursor from_index(radix_tree *tree, unsigned long index = 0)
         {
@@ -198,6 +262,33 @@ public:
 
         return true;
     }
+
+    /**
+     * @brief Set a mark on an index
+     *
+     * @param index Index to mark
+     * @param mark The mark to set
+     */
+    void set_mark(unsigned long index, unsigned int mark);
+
+    // TODO(pedro): Should get_table_entry be private?
+    /**
+     * @brief Get the table entry for a given index
+     *
+     * @param index Radix tree index
+     * @param table Pointer to a pointer to a table. Gets filled on success.
+     * @param tabindex Pointer to a table index. Gets filled on success
+     * @return True if we got the entry, else false.
+     */
+    bool get_table_entry(unsigned long index, radix_tree_node **table, unsigned int *tabindex);
+
+    /**
+     * @brief Clear a mark on an index
+     *
+     * @param index Index to mark
+     * @param mark The mark to clear
+     */
+    void clear_mark(unsigned long index, unsigned int mark);
 };
 
 #endif
