4a842b49d0caebf19802faacebee516550e5b96b
Pedro Falcato <pedro.falcato@gmail.com>
2023-09-07 17:29:47 +0100

    static_key: Add static_key support
    Add static_key support (see
    https://docs.kernel.org/staging/static-keys.html).
    
    This commit only introduces traditional boolean-like branches that do
    not get rid of branching.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

---
+++ kernel/include/onyx/generic/platform/jump_label.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023 Pedro Falcato
+ * This file is part of Onyx, and is released under the terms of the MIT License
+ * check LICENSE at the root directory for more information
+ *
+ * SPDX-License-Identifier: MIT
+ */
+#ifndef _ONYX_GENERIC_PLATFORM_JUMP_LABEL_H
+#define _ONYX_GENERIC_PLATFORM_JUMP_LABEL_H
+
+/* Header for platforms that do not have jump labels set up. */
+#define ARCH_HAS_JUMP_LABEL 0
+
+#endif
