fd86d8cefec145f9e5a8842c9b0fae9e85d77eee
Pedro Falcato <pedro.falcato@gmail.com>
2023-10-18 01:33:36 +0100

    riscv64/code_patch: Fix serialization on the patching CPU
    We forgot to do fence.i for the current, patching CPU. This is obviously
    incorrect as the icache and dcache will dsync.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/arch/riscv64/code_patch.cpp
+++ kernel/arch/riscv64/code_patch.cpp
@@ -96,6 +96,8 @@ static void end_hotpatch(unsigned long flags) RELEASE(hotpatch_lock)
 
     /* Release hotpatch_in_progress (makes APs serialize and go away) */
     hotpatch_in_progress.store(false, mem_order::release);
+    /* Serialize the icache ourselves */
+    serialize_insns();
     irq_restore(flags);
     /* Wait a bit for the serializing cpus to go away. Now they should have a consistent .text and
      * icache stream. Notice we still hold the hotpatch lock, there's no way anyone can mess with
