0a178b6b537acf482efbfe7be1f4fd0c24bacc44
Pedro Falcato <pedro.falcato@gmail.com>
2024-01-26 17:43:29 +0000

    time: Fix date calculation
    The date calculation had lots of problems, and was giving out an extra
    day. Fix it, and add it to time.cpp instead of having it in PC's rtc
    driver.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

---
+++ kernel/include/onyx/date.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2016 - 2024 Pedro Falcato
+ * This file is part of Onyx, and is released under the terms of the MIT License
+ * check LICENSE at the root directory for more information
+ *
+ * SPDX-License-Identifier: MIT
+ */
+#ifndef _ONYX_DATE_H
+#define _ONYX_DATE_H
+
+#include <onyx/types.h>
+
+#include <uapi/time.h>
+
+typedef struct date
+{
+    int seconds;
+    int minutes;
+    int hours;
+    int day;
+    int month;
+    int year;
+    time_t unixtime;
+} date_t;
+
+u64 get_unix_time(const date_t *udate);
+
+#endif
