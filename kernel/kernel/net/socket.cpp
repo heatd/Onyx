ebbd0bc02ea88121a81e9a5eb1ffa39c962ff580
Pedro Falcato <pedro.falcato@gmail.com>
2024-02-16 04:52:20 +0000

    socket: Fix socket_write()'s error codes
    Fixes afunix_test.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/kernel/net/socket.cpp
+++ kernel/kernel/net/socket.cpp
@@ -276,10 +276,7 @@ size_t socket_write(size_t offset, size_t len, void *buffer, struct file *file)
     msg.msg_name = nullptr;
     msg.msg_namelen = 0;
 
-    ssize_t res = s->sendmsg(&msg, fd_flags_to_msg_flags(file));
-    if (res < 0)
-        return errno = -res, -1;
-    return res;
+    return s->sendmsg(&msg, fd_flags_to_msg_flags(file));
 }
 
 ssize_t socket_write_iter(file *filp, size_t offset, iovec_iter *iter, unsigned int flags)
