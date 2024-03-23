b6ba16ed7a69bf0af3e49e1ce887b8fada277480
Pedro Falcato <pedro.falcato@gmail.com>
2024-03-23 22:36:23 +0000

    percpu: Make it C
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/kernel/percpu.c
+++ kernel/kernel/percpu.c
@@ -23,18 +23,16 @@ extern unsigned char percpu_base;
 /* Define errno somewhere */
 PER_CPU_VAR(int __true_errno) = 0;
 
-extern "C"
-{
 PER_CPU_VAR(unsigned long __cpu_base) = 0;
-};
-extern "C" int *__errno_location()
+
+int *__errno_location()
 {
     return get_per_cpu_ptr(__true_errno);
 }
 
 bool percpu_inited = false;
 
-unsigned long *percpu_bases = nullptr;
+unsigned long *percpu_bases = NULL;
 unsigned long nr_bases = 0;
 
 void percpu_add_percpu(unsigned long base)
@@ -42,7 +40,7 @@ void percpu_add_percpu(unsigned long base)
     nr_bases++;
     percpu_bases =
         (unsigned long *) realloc((unsigned long *) percpu_bases, nr_bases * sizeof(unsigned long));
-    assert(percpu_bases != nullptr);
+    assert(percpu_bases != NULL);
     percpu_bases[nr_bases - 1] = base;
 }
 
@@ -69,7 +67,7 @@ unsigned long percpu_init_for_cpu(unsigned int cpu)
     size_t percpu_size = (unsigned long) &__percpu_end - (unsigned long) &__percpu_start;
 
     void *buffer = zalloc(percpu_size);
-    assert(buffer != nullptr);
+    assert(buffer != NULL);
 
     /* TODO: percpu_add_percpu needs to be called in-order, should fix? */
     percpu_add_percpu((unsigned long) buffer);
@@ -82,9 +80,7 @@ unsigned long percpu_init_for_cpu(unsigned int cpu)
 unsigned long percpu_get_area(unsigned int cpu)
 {
     if (cpu >= nr_bases)
-    {
         return 0;
-    }
 
     return percpu_bases[cpu];
 }
@@ -94,9 +90,9 @@ int percpu_map_master_copy()
     size_t percpu_size = (unsigned long) &__percpu_end - (unsigned long) &__percpu_start;
     size_t nr_pages = vm_size_to_pages(percpu_size);
     unsigned long percpu_virtual_start = (unsigned long) &percpu_base;
-    auto phys_base =
+    unsigned long phys_base =
         ((unsigned long) &percpu_base) - KERNEL_VIRTUAL_BASE + get_kernel_phys_offset();
-    auto ret = map_pages_to_vaddr((void *) percpu_virtual_start, (void *) phys_base, nr_pages,
-                                  VM_READ | VM_WRITE);
+    void *ret = map_pages_to_vaddr((void *) percpu_virtual_start, (void *) phys_base, nr_pages,
+                                   VM_READ | VM_WRITE);
     return ret ? 0 : -1;
 }
