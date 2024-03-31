adf87795d2a5276356e43a08a903ceac70053b63
Pedro Falcato <pedro.falcato@gmail.com>
2024-03-31 03:43:47 +0100

    libfs: Add no-op libfs routines for certain fops
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

---
+++ kernel/include/onyx/libfs.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2024 Pedro Falcato
+ * This file is part of Onyx, and is released under the terms of the MIT License
+ * check LICENSE at the root directory for more information
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef _ONYX_LIBFS_H
+#define _ONYX_LIBFS_H
+
+#include <errno.h>
+#include <stdlib.h>
+
+#include <onyx/types.h>
+
+struct inode;
+struct dentry;
+struct file;
+
+static inline struct inode *libfs_no_open(struct dentry *dir, const char *name)
+{
+    errno = ENOENT;
+    return NULL;
+}
+
+static inline struct inode *libfs_no_creat(const char *name, int mode, struct dentry *dir)
+{
+    errno = EROFS;
+    return NULL;
+}
+
+static inline struct inode *libfs_no_symlink(const char *name, const char *linkpath,
+                                             struct dentry *dir)
+{
+    errno = EROFS;
+    return NULL;
+}
+
+static inline struct inode *libfs_no_mkdir(const char *name, mode_t mode, struct dentry *dir)
+{
+    errno = EROFS;
+    return NULL;
+}
+
+static inline struct inode *libfs_no_mknod(const char *name, mode_t mode, dev_t dev,
+                                           struct dentry *dir)
+{
+    errno = EROFS;
+    return NULL;
+}
+
+static inline int libfs_no_link(struct file *target_ino, const char *name, struct dentry *dir)
+{
+    return -EROFS;
+}
+
+static inline int libfs_no_ftruncate(size_t length, struct file *node)
+{
+    return -EROFS;
+}
+
+static inline int libfs_no_fallocate(int mode, off_t offset, off_t len, struct file *node)
+{
+    return -EROFS;
+}
+
+static inline char *libfs_no_readlink(struct file *ino)
+{
+    errno = EINVAL;
+    return NULL;
+}
+
+static inline int libfs_no_unlink(const char *name, int flags, struct dentry *dir)
+{
+    return -EROFS;
+}
+
+#endif
