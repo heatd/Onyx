e2532541a57711b3ef44f787d4f9d0f293097598
Pedro Falcato <pedro.falcato@gmail.com>
2024-04-30 22:31:54 +0100

    atomic: Add some useful atomic macros and API
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

---
+++ kernel/include/onyx/atomic.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024 Pedro Falcato
+ * This file is part of Onyx, and is released under the terms of the MIT License
+ * check LICENSE at the root directory for more information
+ *
+ * SPDX-License-Identifier: MIT
+ */
+#ifndef _ONYX_ATOMIC_H
+#define _ONYX_ATOMIC_H
+
+#define READ_ONCE(var)       (__atomic_load_n(&(var), __ATOMIC_RELAXED))
+#define WRITE_ONCE(var, val) (__atomic_store_n(&(var), (val), __ATOMIC_RELAXED))
+
+#define atomic_and_relaxed(var, mask) (__atomic_and_fetch(&(var), mask, __ATOMIC_RELAXED))
+#define atomic_or_relaxed(var, mask)  (__atomic_or_fetch(&(var), mask, __ATOMIC_RELAXED))
+
+#endif
