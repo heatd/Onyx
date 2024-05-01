a47c8455ed79363d772cf2c0094d89bd584c869c
Pedro Falcato <pedro.falcato@gmail.com>
2024-05-01 22:12:41 +0100

    scripts/ci: Install texinfo
    Needed on macOS CI now.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- scripts/ci/install_macos_deps.sh
+++ scripts/ci/install_macos_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-brew install zstd gmp mpfr libmpc wget unzip make ninja coreutils gnu-getopt
+brew install zstd gmp mpfr libmpc wget unzip make ninja coreutils gnu-getopt texinfo
 mkdir gn_bin/
 cd gn_bin
 wget -q https://chrome-infra-packages.appspot.com/dl/gn/gn/mac-amd64/+/latest -O gn.zip
