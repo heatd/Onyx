b40f03b7b027dee5a94dccbdedb0da6fe8f0ad98
Pedro Falcato <pedro.falcato@gmail.com>
2024-07-31 00:49:28 +0100

    seqcount: Add seqcount_t
    Add seqcount_t, that implements linux(ish) sequence counters, with
    proper memory barriers.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

---
+++ kernel/include/onyx/seqcount_types.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024 Pedro Falcato
+ * This file is part of Onyx, and is released under the terms of the GPLv2 License
+ * check LICENSE at the root directory for more information
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+#ifndef _ONYX_SEQCOUNT_TYPES_H
+#define _ONYX_SEQCOUNT_TYPES_H
+
+typedef unsigned int seqcount_t;
+
+#endif
