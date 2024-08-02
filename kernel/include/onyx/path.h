5dd47f3df5af45253779767297d9a5b39fc68bd9
Pedro Falcato <pedro.falcato@gmail.com>
2024-08-02 03:23:13 +0100

    namei: Track the mount in struct path
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/include/onyx/path.h
+++ kernel/include/onyx/path.h
@@ -14,32 +14,37 @@
 struct path
 {
     struct dentry *dentry;
+    struct mount *mount;
 };
 
 static inline void path_get(struct path *p)
 {
     dentry_get(p->dentry);
+    mnt_get(p->mount);
 }
 
 static inline void path_put(struct path *p)
 {
     if (p->dentry)
         dentry_put(p->dentry);
+    if (p->mount)
+        mnt_put(p->mount);
 }
 
 static inline void path_init(struct path *p)
 {
     p->dentry = NULL;
+    p->mount = NULL;
 }
 
 static inline bool path_is_null(struct path *p)
 {
-    return !p->dentry;
+    return !p->dentry && !p->mount;
 }
 
 static inline bool path_is_equal(struct path *p1, struct path *p2)
 {
-    return p1->dentry == p2->dentry;
+    return p1->mount == p2->mount && p1->dentry == p2->dentry;
 }
 
 #endif
