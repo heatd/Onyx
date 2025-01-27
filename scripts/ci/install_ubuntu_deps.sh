a883bd85a9934f219e012a54d8cbc46b07a7de06
Pedro Falcato <pedro.falcato@gmail.com>
2025-01-27 22:46:31 +0000

    github: Install gn from apt
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- scripts/ci/install_ubuntu_deps.sh
+++ scripts/ci/install_ubuntu_deps.sh
@@ -5,8 +5,4 @@ set -e
 # the base image
 
 sudo apt-get update && sudo apt-get install libmpc-dev libgmp3-dev bison flex libmpfr-dev ninja-build clang lld \
-parted mtools meson libfl2 pkgconf qemu-system uuid-dev gettext xorriso
-mkdir gn_bin/
-cd gn_bin
-wget -q https://chrome-infra-packages.appspot.com/dl/gn/gn/linux-amd64/+/latest -O gn.zip
-unzip gn.zip
+parted mtools meson libfl2 pkgconf qemu-system uuid-dev gettext xorriso generate-ninja
