heatd/Onyx

commit 66b6df9ee38a1c747c383064f3aa3fefd48f6797

sched: Add thread migration
Add thread migration on wakeup, with some better rebalancing of tasks.
This radically improves performance on SMP.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/sched/scheduler.cpp
@@ -75,7 +75,8 @@ static bool is_initialized = false;
 
 void sched_append_to_queue(int priority, unsigned int cpu, thread_t *thread);
 void sched_block(thread *thread);
-void __sched_append_to_queue(int priority, unsigned int cpu, thread_t *thread);
+static void __sched_append_to_queue(int priority, unsigned int cpu, thread_t *thread);
+static void ___sched_append_to_queue(int priority, unsigned int cpu, struct thread *thread);
 
 int sched_rbtree_cmp(const void *t1, const void *t2);
 static rb_tree glbl_thread_list = {.cmp_func = sched_rbtree_cmp};
@@ -85,6 +86,7 @@ PER_CPU_VAR(spinlock scheduler_lock) = STATIC_SPINLOCK_INIT;
 PER_CPU_VAR(thread *thread_queues_head[NUM_PRIO]);
 PER_CPU_VAR(thread *thread_queues_tail[NUM_PRIO]);
 PER_CPU_VAR(thread *current_thread);
+PER_CPU_VAR(unsigned int tasks_in_queues);
 
 void thread_append_to_global_list(thread *t)
 {
@@ -231,11 +233,12 @@ thread_t *__sched_find_next(unsigned int cpu)
         if (current_thread->status == THREAD_RUNNABLE)
         {
             /* Re-append the last thread to the queue */
-            __sched_append_to_queue(current_thread->priority, cpu, current_thread);
+            ___sched_append_to_queue(current_thread->priority, cpu, current_thread);
         }
         else
         {
             add_per_cpu(runnable_delta, -1);
+            add_per_cpu(tasks_in_queues, -1);
         }
 
         spin_unlock_irqrestore(&current_thread->lock, cpu_flags);
@@ -303,11 +306,15 @@ unsigned long nrun = 0;
 
 void calc_avenrun()
 {
-    unsigned long nr_runnable = 0;
+    unsigned long nr_runnable = 0, nr_runnable2 = 0;
 
     for (unsigned int i = 0; i < get_nr_cpus(); i++)
+    {
         nr_runnable += other_cpu_get(runnable_delta, i);
+        nr_runnable2 += other_cpu_get(tasks_in_queues, i);
+    }
 
+    DCHECK(nr_runnable == nr_runnable2);
     if ((long) nr_runnable < 0)
         panic("calc_avenrun: negative nr runnable %ld", nr_runnable);
     nrun = nr_runnable;
@@ -499,7 +506,7 @@ void sched_idle(void *ptr)
     }
 }
 
-void __sched_append_to_queue(int priority, unsigned int cpu, thread *thread)
+static void ___sched_append_to_queue(int priority, unsigned int cpu, struct thread *thread)
 {
     MUST_HOLD_LOCK(get_per_cpu_ptr_any(scheduler_lock, cpu));
 
@@ -527,6 +534,12 @@ void __sched_append_to_queue(int priority, unsigned int cpu, thread *thread)
     }
 }
 
+static void __sched_append_to_queue(int priority, unsigned int cpu, struct thread *thread)
+{
+    add_per_cpu_any(tasks_in_queues, 1, cpu);
+    ___sched_append_to_queue(priority, cpu, thread);
+}
+
 void sched_append_to_queue(int priority, unsigned int cpu, thread_t *thread)
 {
     spin_lock(get_per_cpu_ptr_any(scheduler_lock, cpu));
@@ -538,8 +551,6 @@ void sched_append_to_queue(int priority, unsigned int cpu, thread_t *thread)
     add_per_cpu(runnable_delta, 1);
 }
 
-PER_CPU_VAR(unsigned long active_threads) = 0;
-
 unsigned int sched_allocate_processor(void)
 {
     unsigned int nr_cpus = get_nr_cpus();
@@ -548,13 +559,14 @@ unsigned int sched_allocate_processor(void)
 
     for (unsigned int i = 0; i < nr_cpus; i++)
     {
-        unsigned long active_threads_for_cpu = get_per_cpu_any(active_threads, i);
+        unsigned long active_threads_for_cpu = get_per_cpu_any(tasks_in_queues, i);
         if (active_threads_for_cpu < active_threads_min)
         {
             dest_cpu = i;
             active_threads_min = active_threads_for_cpu;
         }
     }
+
     return dest_cpu;
 }
 
@@ -566,7 +578,6 @@ void thread_add(thread_t *thread, unsigned int cpu_num)
     thread->cpu = cpu_num;
     trace_sched_cpu_assign(thread->id, thread->owner ? thread->owner->pid_ : 0,
                            thread->owner ? thread->owner->comm : NULL, thread->cpu);
-    add_per_cpu_any(active_threads, 1, cpu_num);
     /* Append the thread to the queue */
     sched_append_to_queue(thread->priority, cpu_num, thread);
 }
@@ -921,6 +932,7 @@ void __thread_wake_up(thread *thread, unsigned int cpu)
 {
     MUST_HOLD_LOCK(&thread->lock);
     MUST_HOLD_LOCK(get_per_cpu_ptr_any(scheduler_lock, cpu));
+    unsigned int new_cpu;
 
     /* 1st case: The thread we're "waking up" is running.
      * In this case, just set the status and return, nothing else needed.
@@ -935,7 +947,17 @@ void __thread_wake_up(thread *thread, unsigned int cpu)
     if (thread->status == THREAD_RUNNABLE)
         return;
 
+    new_cpu = sched_allocate_processor();
     thread->status = THREAD_RUNNABLE;
+    if (new_cpu != cpu)
+    {
+        /* Release the locks and reacquire them in proper order, then reappend to the queue. */
+        sched_unlock(thread, CPU_FLAGS_NO_IRQ);
+        thread->cpu = new_cpu;
+        (void) sched_lock(thread);
+        cpu = new_cpu;
+    }
+
     __sched_append_to_queue(thread->priority, cpu, thread);
     add_per_cpu(runnable_delta, 1);
 
@@ -943,9 +965,7 @@ void __thread_wake_up(thread *thread, unsigned int cpu)
     {
         auto curr = get_current_thread();
         if (thread->priority > curr->priority)
-        {
             sched_should_resched();
-        }
     }
     else
     {
@@ -962,9 +982,7 @@ void __thread_wake_up(thread *thread, unsigned int cpu)
 void thread_wake_up(thread_t *thread)
 {
     unsigned long f = sched_lock(thread);
-
     __thread_wake_up(thread, thread->cpu);
-
     sched_unlock(thread, f);
 }
 

kernel/kernel/mm/vm.cpp
@@ -1526,6 +1526,24 @@ static void vm_destroy_area(vm_area_struct *region)
     vma_destroy(region);
 }
 
+static void kick_mm_remote(void *ctx)
+{
+    struct mm_address_space *mm = (struct mm_address_space *) ctx;
+    (void) mm;
+    struct mm_address_space *newmm = get_current_thread()->get_aspace();
+    if (!newmm)
+        newmm = &kernel_address_space;
+    /* TODO: We have no way of knowing if we're on a mm */
+    if (!get_current_thread()->owner)
+        vm_set_aspace(newmm);
+}
+
+static void kick_mm(struct mm_address_space *mm)
+{
+    /* TODO: Ugh, broadcast */
+    smp::sync_call(kick_mm_remote, mm, cpumask::all_but_one(get_cpu_nr()));
+}
+
 /**
  * @brief Destroys an address space.
  *
@@ -1568,6 +1586,9 @@ void vm_destroy_addr_space(struct mm_address_space *mm)
 
     struct arch_mm_address_space old_arch_mmu;
     vm_set_pgd(&old_arch_mmu, own_addrspace);
+
+    g.unlock();
+    kick_mm(mm);
 
     if (free_pgd)
         vm_free_arch_mmu(&old_arch_mmu);

kernel/kernel/ssp.cpp
@@ -22,6 +22,7 @@
 #include <onyx/internal_abi.h>
 #include <onyx/panic.h>
 #include <onyx/random.h>
+#include <onyx/types.h>
 
 #if UINT32_MAX == UINTPTR_MAX
 #define STACK_CHK_GUARD 0xdeadc0de
@@ -39,17 +40,20 @@ extern "C" __attribute__((noreturn, used)) void __stack_chk_fail()
 namespace abi
 {
 
+unsigned long generate_stack_canary()
+{
+    unsigned long guard;
+    arc4random_buf(&guard, sizeof(uintptr_t));
+    return guard;
+}
+
 extern "C" __attribute__((used)) void init_ssp_for_cpu(unsigned int cpu_nr)
 {
-    uintptr_t new_guard = 0;
-    arc4random_buf(&new_guard, sizeof(uintptr_t));
+    static uintptr_t new_guard = generate_stack_canary();
 
     auto abi_details = get_abi_data();
-
     if (cpu_nr == 0)
-    {
         __stack_chk_guard = new_guard;
-    }
 
     abi_details->canary = new_guard;
 }

commit f21a7ff5dce22fd2acfa75ce8e3b99c4f3457e20

sched: Fix sched_lock
sched_lock wasn't safe when thread migration was involved, which ended
up causing havoc and crashes.

Fixes: 66b6df9ee3 ("sched: Add thread migration")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/sched/scheduler.cpp
@@ -191,11 +191,20 @@ unsigned long sched_lock(thread *thread)
     /* 2nd - Lock the thread */
 
     assert(thread->cpu < percpu_get_nr_bases());
-    spinlock *l = get_per_cpu_ptr_any(scheduler_lock, thread->cpu);
+    unsigned long cpu_flags, _;
 
-    unsigned long cpu_flags = spin_lock_irqsave(l);
-    unsigned long _ = spin_lock_irqsave(&thread->lock);
-    (void) _;
+    for (;;)
+    {
+        unsigned int cpu = READ_ONCE(thread->cpu);
+        struct spinlock *l = get_per_cpu_ptr_any(scheduler_lock, cpu);
+        cpu_flags = spin_lock_irqsave(l);
+        _ = spin_lock_irqsave(&thread->lock);
+        if (thread->cpu == cpu)
+            break;
+        (void) _;
+        spin_unlock_irqrestore(&thread->lock, CPU_FLAGS_NO_IRQ);
+        spin_unlock_irqrestore(l, cpu_flags);
+    }
 
     return cpu_flags;
 }