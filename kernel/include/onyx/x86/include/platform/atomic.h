dbbcd1bd678bac201599c102779d389d83cc1efc
Pedro Falcato <pedro.falcato@gmail.com>
2025-02-09 22:10:05 +0000

    x86: Add implementations for the new smp_mb() interfaces
    x86 can efficiently implement all of the new interfaces. Any atomic
    (LOCK prefixed or LOCK-implied, like xchg) provides a full memory
    barrier, so we can use a simple xchg for smp_store_mb(). This also
    allows for eliding smp_mb() around atomics or spinlocks.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/include/onyx/x86/include/platform/atomic.h
+++ kernel/include/onyx/x86/include/platform/atomic.h
@@ -9,3 +9,21 @@
 #define smp_wmb() __asm__ __volatile__("" ::: "memory")
 #define smp_rmb() __asm__ __volatile__("" ::: "memory")
 #define smp_mb()  __asm__ __volatile__("lock addl $0, -4(%%rsp)" ::: "memory", "cc")
+
+#define __x86_xchg(ptr, val)                                                       \
+    ({                                                                             \
+        __typeof__(*(ptr)) v = val;                                                \
+        __asm__ __volatile__("xchg %0, %1" : "+r"(v), "+m"(*ptr)::"memory", "cc"); \
+        v;                                                                         \
+    })
+
+/* x86 atomic ops imply a full memory barrier. xchg is implicitly an atomic op (with an implicit
+ * LOCK prefix too). This is more efficient than doing WRITE_ONCE + smp_mb. */
+#define smp_store_mb(ptr, val) __x86_xchg(ptr, val)
+
+/* every smp_mb around atomics (including spinlock, which involved atomics itself) can be elided */
+/* clang-format off */
+#define smp_mb__before_atomic() do {} while (0)
+#define smp_mb__after_atomic() do {} while (0)
+#define smp_mb__after_spinlock() do {} while (0)
+/* clang-format on */
