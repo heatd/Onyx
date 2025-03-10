eb599b046e88c963b5f41bce10dc08f41cc561fb
Pedro Falcato <pedro.falcato@gmail.com>
2025-03-10 22:19:45 +0000

    madvise: add MADV_DONTDUMP and DODUMP support
    Also expose vma iterators and other interfaces in an internal header
    file.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

---
+++ kernel/kernel/mm/vma_internal.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2016 - 2025 Pedro Falcato
+ * This file is part of Onyx, and is released under the terms of the GPLv2 License
+ * check LICENSE at the root directory for more information
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+#ifndef _ONYX_VMA_INTERNAL_H
+#define _ONYX_VMA_INTERNAL_H
+
+#include <onyx/maple_tree.h>
+#include <onyx/mm_address_space.h>
+
+struct vma_iterator
+{
+    unsigned long index;
+    unsigned long end;
+    struct mm_address_space *mm;
+    struct ma_state mas;
+};
+
+#define VMA_ITERATOR(name, mm, index, end)           \
+    struct vma_iterator name = {index, (end) -1, mm, \
+                                MA_STATE_INIT(&(mm)->region_tree, index, (end) -1)}
+
+static inline void vmi_destroy(struct vma_iterator *vmi)
+{
+    mas_destroy(&vmi->mas);
+}
+
+struct vm_area_struct *vma_prepare_modify(struct vma_iterator *vmi, struct vm_area_struct *vma,
+                                          unsigned long start, unsigned long end);
+#endif
