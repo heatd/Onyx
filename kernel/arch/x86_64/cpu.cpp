heatd/Onyx

commit 3dc08f8cfcdc78aa8a3d708e6d1483f48c6d91ce

x86: Clear IA32_MISC_ENABLE_LIMIT_CPUID_MAXVAL at boot
This msr stops us from seeing the whole cpuid for the CPU, in case we're
Windows NT 4. We're not Windows NT 4.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/arch/x86_64/cpu.cpp
@@ -108,6 +108,15 @@ static void __cpu_identify(void)
     uint32_t ebx = 0;
     uint32_t ecx = 0;
     uint32_t edx = 0;
+
+    cpu_get_name();
+
+    /* IA32_MISC_ENABLE has a cpuid maxval limiting function. It's really important we clear it
+     * straight away, so we can access all cpuid bits with no problem. This only applies to Intel.
+     */
+    if (bootcpu_info.manufacturer == X86_CPU_MANUFACTURER_INTEL)
+        clear_msr_mask(IA32_MISC_ENABLE, IA32_MISC_ENABLE_LIMIT_CPUID_MAXVAL);
+
     if (__get_cpuid(CPUID_FEATURES, &eax, &ebx, &ecx, &edx))
         bootcpu_info.caps[0] = edx | ((uint64_t) ecx << 32);
 
@@ -147,7 +156,6 @@ static void __cpu_identify(void)
 	}
 #endif
 
-    cpu_get_name();
     cpu_get_sign();
     x86_do_alternatives();
     jump_label_init();

kernel/include/onyx/x86/msr.h
@@ -63,6 +63,13 @@ static inline uint64_t rdmsr(uint32_t msr)
     return (uint64_t) lo | ((uint64_t) hi << 32);
 }
 
+static inline void clear_msr_mask(uint32_t msr, uint64_t mask)
+{
+    uint64_t val = rdmsr(msr);
+    val &= ~mask;
+    wrmsr(msr, val);
+}
+
 #endif
 
 #endif

commit ebe1d42ad37f10719464cf72aa6e2fd090c9f52e

x86: Clear IA32_MISC_ENABLE_LIMIT_CPUID_MAXVAL for APs
We were previously only clearing LIMIT_CPUID_MAXVAL in the BSP, which is
good for the kernel's cpuid detection but incorrect for, e.g, userspace.

Fixes: 3dc08f8 ("x86: Clear IA32_MISC_ENABLE_LIMIT_CPUID_MAXVAL at
boot")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/arch/x86_64/cpu.cpp
@@ -331,7 +331,7 @@ void x86_setup_standard_control_registers(void)
 void x86_init_percpu_intel(void)
 {
     uint64_t misc_enable = rdmsr(IA32_MISC_ENABLE);
-    misc_enable &= ~IA32_MISC_ENABLE_XD_BIT_DISABLE;
+    misc_enable &= ~(IA32_MISC_ENABLE_XD_BIT_DISABLE | IA32_MISC_ENABLE_LIMIT_CPUID_MAXVAL);
 
     if (x86_has_cap(X86_FEATURE_ERMS))
         misc_enable |= IA32_MISC_ENABLE_FAST_STRINGS_ENABLE;