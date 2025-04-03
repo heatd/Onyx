heatd/Onyx

commit 3a27c4c3bb88af0e84a14b26e23f88021bc53ffd

kasan: Unpoison the struct slab when doing TYPESAFE_BY_RCU freeing
This shuts up erroneous REDZONE KASAN triggers when doing
TYPESAFE_BY_RCU slab freeing (since RCU code is compiled with KASAN on).

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/mm/slab.c
@@ -1323,6 +1323,11 @@ static void kmem_cache_free_slab(struct slab *slab)
     struct slab_cache *cache = slab->cache;
 
     // Free it from the free list
+#if CONFIG_KASAN
+    /* If we're going to call_rcu, we need to have an unpoisoned struct slab (so RCU doesn't trip on
+     * this) */
+    asan_unpoison_shadow((unsigned long) slab, sizeof(struct slab));
+#endif
     list_remove(&slab->slab_list_node);
     kmem_slab_unaccount_pages(slab, cache->flags);
     if (cache->flags & SLAB_TYPESAFE_BY_RCU)

commit cfdef777943cde6f1aea1713eb688dd8cedf0771

slab: Fix KASAN check
We were doing #if CONFIG_KASAN instead of #ifdef...

Fixes: 3a27c4c3bb88 ("kasan: Unpoison the struct slab when doing TYPESAFE_BY_RCU freeing")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/mm/slab.c
@@ -1330,7 +1330,7 @@ static void kmem_cache_free_slab(struct slab *slab)
     struct slab_cache *cache = slab->cache;
 
     // Free it from the free list
-#if CONFIG_KASAN
+#ifdef CONFIG_KASAN
     /* If we're going to call_rcu, we need to have an unpoisoned struct slab (so RCU doesn't trip on
      * this) */
     asan_unpoison_shadow((unsigned long) slab, sizeof(struct slab));