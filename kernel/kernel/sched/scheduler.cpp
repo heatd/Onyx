heatd/Onyx

commit 067286cd3d6348227341f670d0feba49209e0760

sched: Add sched_getaffinity and sched_setaffinity
Add getaffinity and setaffinity syscalls. They're still stubbed and we
don't actually properly support affinity yet. However, it's good enough
for _SC_NPROCESSORS_{ONLN,CONF}.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/sched/scheduler.cpp
@@ -1309,3 +1309,28 @@ bool __can_sleep_internal()
         return true;
     return !sched_is_preemption_disabled() && !irq_is_disabled();
 }
+
+int sys_sched_setaffinity(pid_t pid, size_t cpusetsize, const void *cpu_set)
+{
+    /* TODO: Actually implement this, instead of just being here as a placeholder. The blocker here
+     * is partly that pids aren't tids... */
+    return 0;
+}
+
+int sys_sched_getaffinity(pid_t pid, size_t cpusetsize, void *cpu_set)
+{
+    /* TODO: Same as above, this is a placeholder */
+    cpumask online_cpus = smp::get_online_cpumask();
+    if (cpusetsize < sizeof(cpumask))
+        return -EINVAL;
+
+    if (copy_to_user(cpu_set, &online_cpus.mask, sizeof(cpumask)))
+        return -EFAULT;
+    if (cpusetsize > sizeof(cpumask))
+    {
+        if (user_memset((u8 *) cpu_set + sizeof(cpumask), 0, cpusetsize - sizeof(cpumask)))
+            return -EFAULT;
+    }
+
+    return 0;
+}

commit 6291762a51a2fd304e94155dd12b9cf6dda76f97

sched: Fix the return value of sched_getaffinity
sched_getaffinity(2) says:
"On  success,  the raw sched_getaffinity() system call returns the number of
bytes placed copied into the mask buffer; this will be the minimum of
cpusetsize and the size (in bytes) of the cpumask_t data type that is used
internally by the kernel to represent the CPU set bit mask."

Thus, fix the return value to return the number of bytes copied. We don't track
the max cpu id we've seen (maybe we should, eventually), so we always transfer
at least the size of a struct cpumask.

This change in return value also allows us to skip the user_memset, which is
done by the libc itself[1][2].

[1] https://github.com/heatd/musl/blob/0784374d561435f7c787a555aeab8ede699ed298/src/sched/affinity.c#L21
[2] https://sourceware.org/git/?p=glibc.git;a=blob;f=sysdeps/unix/sysv/linux/sched_getaffinity.c;h=1c66bebff67ccdc5053f363f030aa53b1b64c3e8;hb=refs/heads/release/2.41/master#l40

Link: https://man7.org/linux/man-pages/man2/sched_setaffinity.2.html
Fixes: 067286cd3d63 ("sched: Add sched_getaffinity and sched_setaffinity")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/sched/scheduler.cpp
@@ -1452,16 +1452,14 @@ int sys_sched_getaffinity(pid_t pid, size_t cpusetsize, void *cpu_set)
     spin_unlock_irqrestore(&task->thr->lock, flags);
     rcu_read_unlock();
 
+    /* Linux man-page verbiage (and, indeed, the implementation) allows us to not require a full
+     * cpumask from userspace (i.e if we only have 8 bits, a byte would do). However, we don't
+     * really do that kind of tracking, and this level of micro-optimization is useless. */
     if (cpusetsize < sizeof(cpumask))
         return -EINVAL;
 
     if (copy_to_user(cpu_set, &mask, sizeof(cpumask)))
         return -EFAULT;
-    if (cpusetsize > sizeof(cpumask))
-    {
-        if (user_memset((u8 *) cpu_set + sizeof(cpumask), 0, cpusetsize - sizeof(cpumask)))
-            return -EFAULT;
-    }
 
-    return 0;
+    return sizeof(cpumask);
 }