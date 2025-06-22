heatd/Onyx

commit 067286cd3d6348227341f670d0feba49209e0760

sched: Add sched_getaffinity and sched_setaffinity
Add getaffinity and setaffinity syscalls. They're still stubbed and we
don't actually properly support affinity yet. However, it's good enough
for _SC_NPROCESSORS_{ONLN,CONF}.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/sched/scheduler.cpp
@@ -1309,3 +1309,28 @@ bool __can_sleep_internal()
         return true;
     return !sched_is_preemption_disabled() && !irq_is_disabled();
 }
+
+int sys_sched_setaffinity(pid_t pid, size_t cpusetsize, const void *cpu_set)
+{
+    /* TODO: Actually implement this, instead of just being here as a placeholder. The blocker here
+     * is partly that pids aren't tids... */
+    return 0;
+}
+
+int sys_sched_getaffinity(pid_t pid, size_t cpusetsize, void *cpu_set)
+{
+    /* TODO: Same as above, this is a placeholder */
+    cpumask online_cpus = smp::get_online_cpumask();
+    if (cpusetsize < sizeof(cpumask))
+        return -EINVAL;
+
+    if (copy_to_user(cpu_set, &online_cpus.mask, sizeof(cpumask)))
+        return -EFAULT;
+    if (cpusetsize > sizeof(cpumask))
+    {
+        if (user_memset((u8 *) cpu_set + sizeof(cpumask), 0, cpusetsize - sizeof(cpumask)))
+            return -EFAULT;
+    }
+
+    return 0;
+}

commit 0c89d85a474b61e28c996a8bbc82dea862a9acd4

sched: Check for cpusetsize alignment in sched_getaffinity
This is not documented on sched_getaffinity(2), but the Linux implementation
checks for unsigned long alignment. This is most correct, since the passed
cpu_set is documented by the syscall itself (i.e, not the wrapper) to be
dealing in unsigned longs. Thus, add this check.

Fixes: 067286cd3d63 ("sched: Add sched_getaffinity and sched_setaffinity")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/sched/scheduler.cpp
@@ -1429,6 +1429,11 @@ int sys_sched_getaffinity(pid_t pid, size_t cpusetsize, void *cpu_set)
     struct cpumask mask;
     unsigned long flags;
 
+    /* The passed size should be unsigned long aligned, since the cpu_set is de-facto of type
+     * unsigned long */
+    if (cpusetsize & (sizeof(unsigned long) - 1))
+        return -EINVAL;
+
     rcu_read_lock();
     if (!pid)
         task = get_current_process();