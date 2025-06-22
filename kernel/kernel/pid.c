heatd/Onyx

commit 1a5c11ea3ff65f57f5f99805b90a6a2b73826eaa

pid: Rework pids and pid management
Use maple trees with RCU support, and improve lots of stuff. TTYs now
hold pid references directly.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/pid.c
@@ -1,0 +1,342 @@
+/*
+ * Copyright (c) 2020 - 2024 Pedro Falcato
+ * This file is part of Onyx, and is released under the terms of the GPLv2 License
+ * check LICENSE at the root directory for more information
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+#define DEFINE_CURRENT
+#include <onyx/err.h>
+#include <onyx/maple_tree.h>
+#include <onyx/mm/slab.h>
+#include <onyx/pid.h>
+#include <onyx/process.h>
+#include <onyx/rculist.h>
+
+static struct maple_tree pid_tree = MTREE_INIT(pid_tree, MT_FLAGS_USE_RCU | MT_FLAGS_ALLOC_RANGE);
+static unsigned long pid_next = 1;
+static struct slab_cache *pid_cache;
+
+__init static void pid_subsys_init(void)
+{
+    pid_cache =
+        kmem_cache_create("pid", sizeof(struct pid), _Alignof(struct pid), KMEM_CACHE_PANIC, NULL);
+}
+
+#define PID_MAX 4096
+
+struct pid *pid_alloc(struct process *leader)
+{
+    unsigned long pid_id;
+    int err;
+
+    struct pid *pid = kmem_cache_alloc(pid_cache, GFP_ATOMIC);
+    if (!pid)
+        return ERR_PTR(-ENOMEM);
+
+    pid->refcount = REFCOUNT_INIT(1);
+    for (int i = 0; i < PIDTYPE_MAX; i++)
+        INIT_LIST_HEAD(&pid->member_list[i]);
+    spinlock_init(&pid->lock);
+    pid->proc = leader;
+
+    err = mtree_alloc_cyclic(&pid_tree, &pid_id, pid, 1, PID_MAX, &pid_next, GFP_KERNEL);
+    if (err < 0)
+    {
+        kmem_cache_free(pid_cache, pid);
+        return ERR_PTR(err);
+    }
+
+    CHECK(pid_id > 0);
+    /* TODO: Concurrency weirdness? */
+    leader->pid_struct = pid;
+    pid->pid_ = pid_id;
+    leader->pid_ = pid_id;
+    return pid;
+}
+
+struct pid *pid_lookup(pid_t pid)
+{
+    struct pid *p = NULL;
+    MA_STATE(mas, &pid_tree, pid, pid + 1);
+    mas_lock(&mas);
+    p = mas_find(&mas, pid + 1);
+    mas_unlock(&mas);
+    if (unlikely(p->pid_ != pid))
+        return NULL;
+    return p;
+}
+
+struct pid *pid_lookup_ref(pid_t pid)
+{
+    struct pid *p = NULL;
+    MA_STATE(mas, &pid_tree, pid, pid + 1);
+    mas_lock(&mas);
+    p = mas_find(&mas, pid + 1);
+    mas_unlock(&mas);
+    if (unlikely(p->pid_ != pid))
+        return NULL;
+    if (unlikely(!refcount_inc_not_zero(&p->refcount)))
+        return NULL;
+
+    return p;
+}
+
+bool pgrp_is_in_session(struct pid *pid, struct pid *session)
+{
+    /* rcu read lock or appropriate locks held */
+    struct process *proc;
+    if (list_is_empty_rcu(&pid->member_list[PIDTYPE_PGRP]))
+        return false;
+
+    proc = list_first_entry(&pid->member_list[PIDTYPE_PGRP], struct process, pgrp_node);
+    return proc->session == session;
+}
+
+void pid_destroy(struct pid *pid)
+{
+    for (int i = 0; i < PIDTYPE_MAX; i++)
+        DCHECK(list_is_empty(&pid->member_list[i]));
+    kfree_rcu(pid, rcu);
+}
+
+static bool pid_empty(struct pid *pid)
+{
+    for (int i = 0; i < PIDTYPE_MAX; i++)
+    {
+        if (!list_is_empty_rcu(&pid->member_list[i]))
+            return false;
+    }
+
+    return true;
+}
+
+static void free_pid(struct pid *pid)
+{
+    mtree_erase(&pid_tree, pid->pid_);
+    put_pid(pid);
+}
+
+void pid_remove_process(struct pid *pid, struct process *proc, enum pid_type type)
+{
+    spin_lock(&pid->lock);
+
+    switch (type)
+    {
+        case PIDTYPE_PGRP:
+            list_remove_rcu(&proc->pgrp_node.__lh);
+            break;
+        case PIDTYPE_SID:
+            list_remove_rcu(&proc->session_node.__lh);
+            break;
+        default:
+            UNREACHABLE();
+    }
+
+    spin_unlock(&pid->lock);
+
+    if (pid_empty(pid))
+        free_pid(pid);
+}
+
+void pid_add_process(struct pid *pid, struct process *proc, enum pid_type type)
+{
+    spin_lock(&pid->lock);
+
+    switch (type)
+    {
+        case PIDTYPE_PGRP:
+            list_add_tail_rcu(&proc->pgrp_node.__lh, &pid->member_list[type]);
+            break;
+        case PIDTYPE_SID:
+            list_add_tail_rcu(&proc->session_node.__lh, &pid->member_list[type]);
+            break;
+        default:
+            UNREACHABLE();
+    }
+
+    spin_unlock(&pid->lock);
+}
+
+int sys_setpgid(pid_t pid, pid_t pgid)
+{
+    struct process *target;
+    struct pid *pgrp, *old_pgrp;
+    int err;
+
+    pgrp = NULL;
+    /* If pid == 0, pid = our pid; if pgid == 0, pgid = pid */
+    if (!pid)
+        pid = current->pid_;
+
+    if (!pgid)
+        pgid = pid;
+
+    if (pid < 0 || pgid < 0)
+        return -EINVAL;
+
+    target = get_process_from_pid(pid);
+
+    /* Error out if the process doesn't exist, isn't us or isn't our child. */
+    if (!target || (target != current && target->parent != current))
+    {
+        if (target)
+            process_put(target);
+        return -ESRCH;
+    }
+
+    /* Can't do setpgid for a child that has exec'd (the only way that flag is cleared). */
+    if (target->parent == current && !(target->flags & PROCESS_FORKED))
+    {
+        err = -EACCES;
+        goto err1;
+    }
+
+    write_lock(&tasklist_lock);
+
+    pgrp = pid_lookup(pgid);
+    if (!pgrp)
+    {
+        err = -ESRCH;
+        goto err;
+    }
+
+    err = -EPERM;
+    if (target->session != current->session)
+        goto err;
+
+    if (pgrp != target->process_group)
+    {
+        /* If session leader, oh no! */
+        if (target->session == target->pid_struct)
+            goto err;
+
+        if (pgid != pid && !pgrp_is_in_session(pgrp, target->session))
+            goto err;
+
+        old_pgrp = target->process_group;
+        if (old_pgrp)
+            pid_remove_process(old_pgrp, target, PIDTYPE_PGRP);
+
+        pid_add_process(pgrp, target, PIDTYPE_PGRP);
+        target->process_group = pgrp;
+    }
+
+    write_unlock(&tasklist_lock);
+    process_put(target);
+    return 0;
+err:
+    write_unlock(&tasklist_lock);
+err1:
+    process_put(target);
+    return err;
+}
+
+pid_t sys_getpgid(pid_t pid)
+{
+    struct process *target;
+    if (pid < 0)
+        return -EINVAL;
+
+    /* If pid == 0, pid = us */
+    if (!pid)
+        pid = current->pid_;
+
+    target = get_process_from_pid(pid);
+
+    if (!target)
+        return -ESRCH;
+
+    rcu_read_lock();
+    pid = rcu_dereference(target->process_group)->pid_;
+    rcu_read_unlock();
+    return pid;
+}
+
+pid_t sys_setsid(void)
+{
+    if (current->process_group == current->pid_struct)
+    {
+        // Oops, we're process group leader, can't call setsid
+        return -EPERM;
+    }
+
+    // Lets make ourselves process group leaders
+    pid_remove_process(current->process_group, current, PIDTYPE_PGRP);
+    current->process_group = current->pid_struct;
+    pid_add_process(current->process_group, current, PIDTYPE_PGRP);
+
+    // and create a session on our pid
+    pid_remove_process(current->session, current, PIDTYPE_SID);
+    current->session = current->pid_struct;
+    pid_add_process(current->session, current, PIDTYPE_SID);
+
+    // Initially, we won't have a controlling terminal
+    current->ctty = NULL;
+    return pid_nr(current->session);
+}
+
+pid_t sys_getsid(pid_t pid)
+{
+    struct process *proc;
+    if (!pid)
+        pid = current->pid_;
+
+    proc = get_process_from_pid(pid);
+    if (!proc)
+        return -ESRCH;
+
+    rcu_read_lock();
+    pid = pid_nr(proc->session);
+    rcu_read_unlock();
+    process_put(proc);
+    return pid;
+}
+
+int pid_kill_pgrp(struct pid *pid, int sig, int flags, siginfo_t *info)
+{
+    int signals_sent = 0;
+    struct process *proc;
+
+    pgrp_for_every_member(pid, proc, PIDTYPE_PGRP)
+    {
+        if (may_kill(sig, proc, info) < 0)
+            continue;
+        if (kernel_raise_signal(sig, proc, 0, info) < 0)
+            break;
+
+        signals_sent++;
+    }
+
+    return signals_sent != 0 ? 0 : -EPERM;
+}
+
+bool pid_is_orphaned_and_has_stopped_jobs(struct pid *pgrp, struct process *ignore)
+{
+    // Definition of orphaned process group:
+    // "A process group in which the parent of every member is
+    // either itself a member of the group or is not a member of the group's session."
+
+    bool has_stopped = false;
+    struct process *proc;
+
+    pgrp_for_every_member(pgrp, proc, PIDTYPE_PGRP)
+    {
+        if (proc == ignore)
+            continue;
+
+        if (proc->signal_group_flags & SIGNAL_GROUP_STOPPED)
+            has_stopped = true;
+
+        // Ignore init, since it has no parent
+        if (!proc->parent)
+            continue;
+
+        /* Not orphan */
+        if (proc->parent->process_group != pgrp || proc->parent->session == ignore->session)
+            return false;
+    }
+
+    return has_stopped;
+}

kernel/include/onyx/cpumask.h
@@ -206,4 +206,20 @@ static inline struct cpumask cpumask_all_but_one(unsigned long cpu)
     return c;
 }
 
+static inline void cpumask_set_atomic(struct cpumask* mask, unsigned long cpu)
+{
+    unsigned long long_idx = cpu / LONG_SIZE_BITS;
+    unsigned long bit_idx = cpu % LONG_SIZE_BITS;
+
+    __atomic_or_fetch(&mask->mask[long_idx], (1UL << bit_idx), __ATOMIC_RELAXED);
+}
+
+static inline void cpumask_unset_atomic(struct cpumask* mask, unsigned long cpu)
+{
+    unsigned long long_idx = cpu / LONG_SIZE_BITS;
+    unsigned long bit_idx = cpu % LONG_SIZE_BITS;
+
+    __atomic_and_fetch(&mask->mask[long_idx], ~(1UL << bit_idx), __ATOMIC_RELAXED);
+}
+
 #endif

kernel/include/onyx/list.h
@@ -284,8 +284,8 @@ static inline void *extrusive_list_get_element(struct extrusive_list_head *list,
     }
     else
     {
-        struct extrusive_list_head *current = (struct extrusive_list_head *) *saveptr;
-        struct extrusive_list_head *next = current->next;
+        struct extrusive_list_head *curr = (struct extrusive_list_head *) *saveptr;
+        struct extrusive_list_head *next = curr->next;
         *saveptr = next;
         if (!next)
             return NULL;

kernel/include/onyx/pid.h
@@ -1,7 +1,9 @@
 /*
- * Copyright (c) 2020 Pedro Falcato
+ * Copyright (c) 2020 - 2024 Pedro Falcato
  * This file is part of Onyx, and is released under the terms of the GPLv2 License
  * check LICENSE at the root directory for more information
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
  */
 
 #ifndef _ONYX_PGRP_H
@@ -9,15 +11,19 @@
 
 #include <stdio.h>
 
-#include <onyx/auto_resource.h>
 #include <onyx/fnv.h>
 #include <onyx/list.h>
-#include <onyx/refcount.h>
-#include <onyx/scoped_lock.h>
+#include <onyx/rculist.h>
+#include <onyx/rcupdate.h>
+#include <onyx/ref.h>
 #include <onyx/spinlock.h>
 #include <onyx/types.h>
 
 #include <uapi/signal.h>
+
+#ifdef __cplusplus
+#include <onyx/scoped_lock.h>
+#endif
 
 enum pid_type
 {
@@ -28,26 +34,18 @@ enum pid_type
 
 struct process;
 
-struct pid : public refcountable
+struct pid
 {
-private:
+    refcount_t refcount;
     pid_t pid_;
-    mutable spinlock lock;
-    list_head member_list[PIDTYPE_MAX];
-    list_head_cpp<pid> _hashtable_node;
+    struct spinlock lock;
+    struct process *proc;
+    struct list_head member_list[PIDTYPE_MAX];
+    struct rcu_head rcu;
 
-public:
-    pid(process *leader);
-
-    ~pid()
-    {
-        for (int i = 0; i < PIDTYPE_MAX; i++)
-            assert(list_is_empty(&member_list[i]));
-        remove_from_hashtable(*this);
-    }
-
+#ifdef __cplusplus
     template <typename Callable>
-    void for_every_member(Callable callable, pid_type type = PIDTYPE_PGRP) const
+    void for_every_member(Callable callable, pid_type type = PIDTYPE_PGRP)
     {
         scoped_lock g{lock};
 
@@ -58,69 +56,57 @@ public:
             callable(proc);
         }
     }
+#endif
 
-    /**
-     * @brief Adds a process to the process group.
-     * Note: process::pgrp_lock must be locked.
-     *
-     * @param p Process
-     * @param type Context of the usage of the pid
-     */
-    void add_process(process *p, pid_type type);
-
-    /**
-     * @brief Removes a process from the process group(usually either
-     * because it switched process groups or died). process::pgrp_lock must also be locked.
-     *
-     * @param p Process
-     * @param type Context of the usage of the pid
-     */
-    void remove_process(process *p, pid_type type);
-
-    void inherit(process *proc, pid_type type);
-
-    list_head &hashtable_node()
-    {
-        return _hashtable_node;
-    }
-
-    static fnv_hash_t hash_pid(const pid_t &pid)
-    {
-        return fnv_hash(&pid, sizeof(pid));
-    }
-
-    static fnv_hash_t hash(pid &grp)
-    {
-        return hash_pid(grp.pid_);
-    }
-
-    pid_t get_pid() const
-    {
-        return pid_;
-    }
-
-    static void add_to_hashtable(pid &p);
-    static void remove_from_hashtable(pid &p);
-
-    bool is_in_session(pid *session);
-
-    using auto_pid = auto_resource<pid>;
-
-    static auto_pid lookup(pid_t pid);
-
-    bool is_orphaned_and_has_stopped_jobs(process *ignore) const;
-
-    int kill_pgrp(int sig, int flags, siginfo_t *info) const;
-
-    bool is(enum pid_type type)
-    {
-        return !list_is_empty(&member_list[type]);
-    }
+/* XXX: Kind of a PITA to generically iterate because of session_node vs pgrp_node... */
+#define pgrp_for_every_member(pid, pos, pid_type) \
+    list_for_each_entry (pos, &(pid)->member_list[pid_type], pgrp_node.__lh)
 };
 
-static inline pid::auto_pid pid_create(process *leader)
+__BEGIN_CDECLS
+
+struct pid *pid_alloc(struct process *leader);
+bool pgrp_is_in_session(struct pid *pid, struct pid *session);
+
+void pid_destroy(struct pid *pid);
+
+void _Z11stack_tracev(void);
+
+static inline void get_pid(struct pid *pid)
 {
-    return new pid(leader);
+    refcount_inc(&pid->refcount);
 }
+
+static inline bool get_pid_not_zero(struct pid *pid)
+{
+    return refcount_inc_not_zero(&pid->refcount);
+}
+
+static inline void put_pid(struct pid *pid)
+{
+    if (refcount_dec_and_test(&pid->refcount))
+        pid_destroy(pid);
+}
+
+int pid_kill_pgrp(struct pid *pid, int sig, int flags, siginfo_t *info);
+bool pid_is_orphaned_and_has_stopped_jobs(struct pid *pgrp, struct process *ignore);
+
+void pid_remove_process(struct pid *pid, struct process *proc, enum pid_type type);
+void pid_add_process(struct pid *pid, struct process *proc, enum pid_type type);
+
+struct pid *pid_lookup(pid_t pid);
+struct pid *pid_lookup_ref(pid_t pid);
+
+static inline bool pid_is(struct pid *pid, enum pid_type type)
+{
+    return !list_is_empty_rcu(&pid->member_list[type]);
+}
+
+static inline pid_t pid_nr(struct pid *pid)
+{
+    return pid->pid_;
+}
+
+__END_CDECLS
 
 #endif

kernel/include/onyx/process.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 - 2024 Pedro Falcato
+ * Copyright (c) 2016 - 2025 Pedro Falcato
  * This file is part of Onyx, and is released under the terms of the GPLv2 License
  * check LICENSE at the root directory for more information
  *
@@ -50,8 +50,10 @@ struct proc_event_sub;
 struct tty;
 struct pid;
 
+__BEGIN_CDECLS
 static void process_get(struct process *process);
 static void process_put(struct process *process);
+__END_CDECLS
 
 #define PROCESS_FORKED (1 << 0)
 #define PROCESS_SECURE (1 << 1)
@@ -153,25 +155,16 @@ struct process
 
     struct itimer timers[ITIMER_COUNT] CPP_DFLINIT;
 
-#ifdef __cplusplus
-    pid::auto_pid pid_struct{};
-#else
     struct pid *pid_struct;
-#endif
 
     struct spinlock pgrp_lock CPP_DFLINIT;
     LIST_HEAD_CPP(process) pgrp_node;
-#ifdef __cplusplus
-    pid::auto_pid process_group{};
-#else
+
     struct pid *process_group;
-#endif
+
     LIST_HEAD_CPP(process) session_node;
-#ifdef __cplusplus
-    pid::auto_pid session{};
-#else
+
     struct pid *session;
-#endif
 
     struct rlimit rlimits[RLIM_NLIMITS + 1] CPP_DFLINIT;
     RWSLOCK rlimit_lock CPP_DFLINIT;
@@ -179,6 +172,8 @@ struct process
     struct tty *ctty CPP_DFLINIT;
 
     struct vfork_completion *vfork_compl CPP_DFLINIT;
+    /* There might be a nicer place to put this? */
+    struct rcu_head rcu_head;
 
 #ifdef __cplusplus
     process();
@@ -284,6 +279,8 @@ private:
 #endif
 };
 
+__BEGIN_CDECLS
+
 struct thread *process_create_main_thread(struct process *proc, thread_callback_t callback,
                                           void *sp);
 
@@ -328,6 +325,10 @@ __attribute__((pure)) static inline struct process *get_current_process()
     return (thread == NULL) ? NULL : (struct process *) thread->owner;
 }
 
+#ifdef DEFINE_CURRENT
+#define current get_current_process()
+#endif
+
 static inline mode_t get_current_umask()
 {
     if (unlikely(!get_current_process()))
@@ -346,6 +347,10 @@ static inline mode_t do_umask(mode_t mode)
  * @return The number of active processes
  */
 pid_t process_get_active_processes();
+
+extern rwslock_t tasklist_lock;
+
+__END_CDECLS
 
 #ifdef __cplusplus
 

kernel/include/onyx/rculist.h
@@ -56,4 +56,9 @@ static inline void list_remove_rcu(struct list_head *node)
     for (pos = list_entry_rcu((head)->next, __typeof__(*pos), member); &pos->member != (head); \
          pos = list_entry_rcu(pos->member.next, __typeof__(*pos), member))
 
+static inline bool list_is_empty_rcu(const struct list_head *head)
+{
+    return READ_ONCE(head->next) == head;
+}
+
 #endif

kernel/include/onyx/ref.h
@@ -6,9 +6,11 @@
 
 #ifndef _ONYX_REF_H
 #define _ONYX_REF_H
+
 #include <stdbool.h>
 
-#include <onyx/spinlock.h>
+#include <onyx/atomic.h>
+#include <onyx/compiler.h>
 
 struct ref
 {
@@ -16,8 +18,50 @@ struct ref
     void (*release)(struct ref *ref);
 };
 
+__BEGIN_CDECLS
+
 void ref_init(struct ref *ref, unsigned long refcount, void (*releasefunc)(struct ref *));
 bool ref_grab(struct ref *ref);
 void ref_release(struct ref *ref);
+
+typedef struct __refcount_t
+{
+    unsigned int refs;
+} refcount_t;
+
+#define REFCOUNT_INIT(refcount) ((refcount_t){.refs = (refcount)})
+
+static inline void refcount_inc(refcount_t *r)
+{
+    __atomic_add_fetch(&r->refs, 1, __ATOMIC_RELAXED);
+}
+
+static inline bool refcount_dec_and_test(refcount_t *r)
+{
+    return !__atomic_sub_fetch(&r->refs, 1, __ATOMIC_RELAXED);
+}
+
+static inline bool refcount_inc_not_zero(refcount_t *r)
+{
+    unsigned int old, expected;
+
+    old = READ_ONCE(r->refs);
+    do
+    {
+        expected = old;
+        if (unlikely(!old))
+            return false;
+        old = cmpxchg(&r->refs, expected, expected + 1);
+    } while (unlikely(old != expected));
+
+    return true;
+}
+
+static inline unsigned int refcount_read(const refcount_t *r)
+{
+    return __atomic_load_n(&r->refs, __ATOMIC_RELAXED);
+}
+
+__END_CDECLS
 
 #endif

kernel/include/onyx/rwlock.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 - 2023 Pedro Falcato
+ * Copyright (c) 2017 - 2025 Pedro Falcato
  * This file is part of Onyx, and is released under the terms of the GPLv2 License
  * check LICENSE at the root directory for more information
  *
@@ -70,15 +70,15 @@ static inline void rwlock_init(struct rwlock *lock)
 
 __END_CDECLS
 
-#ifdef __cplusplus
-
-struct CAPABILITY("rwslock") rwslock
+typedef struct CAPABILITY("rwslock") rwslock
 {
-private:
-    unsigned long lock{0};
+    unsigned long lock;
 
-public:
-    constexpr rwslock() = default;
+#ifdef __cplusplus
+    constexpr rwslock()
+    {
+        lock = 0;
+    }
 
     void lock_read() ACQUIRE_SHARED(this);
     void lock_write() ACQUIRE(this);
@@ -88,7 +88,48 @@ public:
 
     int try_read() TRY_ACQUIRE_SHARED(0, this);
     int try_write() TRY_ACQUIRE(0, this);
-};
+#endif
+} rwslock_t;
+
+CONSTEXPR static inline void rwslock_init(struct rwslock *rwl)
+{
+    rwl->lock = 0;
+}
+
+__BEGIN_CDECLS
+
+void __read_lock(struct rwslock *lock) ACQUIRE_SHARED(lock);
+void __read_unlock(struct rwslock *lock) RELEASE_SHARED(lock);
+void __write_lock(struct rwslock *lock) ACQUIRE(lock);
+void __write_unlock(struct rwslock *lock) RELEASE(lock);
+
+static inline void read_lock(struct rwslock *lock) ACQUIRE_SHARED(lock)
+{
+    sched_disable_preempt();
+    __read_lock(lock);
+}
+
+static inline void read_unlock(struct rwslock *lock) RELEASE_SHARED(lock)
+{
+    __read_unlock(lock);
+    sched_enable_preempt();
+}
+
+static inline void write_lock(struct rwslock *lock) ACQUIRE(lock)
+{
+    sched_disable_preempt();
+    __write_lock(lock);
+}
+
+static inline void write_unlock(struct rwslock *lock) RELEASE(lock)
+{
+    __write_unlock(lock);
+    sched_enable_preempt();
+}
+
+__END_CDECLS
+
+#ifdef __cplusplus
 
 #define RWSLOCK rwslock
 
@@ -207,7 +248,7 @@ public:
 };
 
 #else
-#define RWSLOCK unsigned long
+#define RWSLOCK struct rwslock
 #endif
 
 #endif

commit 46f0d4c8ffe1ee116db0d00bca97bdc8e016cc82

pid: Remove user-controllable integer overflows from lookup
The kernel_api_test SetPgrpTest.HandlesBadPids can trigger integer overflow
and UBSAN warnings from unprivileged userspace due to the way we're calculating
the "max" for maple tree lookups.

To fix it, we must first check if it's not < 0 (if it is, it's not a valid pid
anyway); then, cast it to an unsigned int (we've proven the casting will
not change the value, since > 0) and only then can we + 1.

Fixes: 1a5c11ea3ff6 ("pid: Rework pids and pid management")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/pid.c
@@ -59,9 +59,13 @@ struct pid *pid_alloc(struct process *leader)
 struct pid *pid_lookup(pid_t pid)
 {
     struct pid *p = NULL;
-    MA_STATE(mas, &pid_tree, pid, pid + 1);
+
+    if (pid < 0)
+        return NULL;
+
+    MA_STATE(mas, &pid_tree, pid, (unsigned int) pid + 1);
     mas_lock(&mas);
-    p = mas_find(&mas, pid + 1);
+    p = mas_find(&mas, (unsigned int) pid + 1);
     mas_unlock(&mas);
     if (unlikely(!p || p->pid_ != pid))
         return NULL;
@@ -71,9 +75,13 @@ struct pid *pid_lookup(pid_t pid)
 struct pid *pid_lookup_ref(pid_t pid)
 {
     struct pid *p = NULL;
-    MA_STATE(mas, &pid_tree, pid, pid + 1);
+
+    if (pid < 0)
+        return NULL;
+
+    MA_STATE(mas, &pid_tree, pid, (unsigned int) pid + 1);
     mas_lock(&mas);
-    p = mas_find(&mas, pid + 1);
+    p = mas_find(&mas, (unsigned int) pid + 1);
     mas_unlock(&mas);
     if (unlikely(!p || p->pid_ != pid))
         return NULL;