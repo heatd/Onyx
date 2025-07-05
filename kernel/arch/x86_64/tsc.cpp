3af4cdcdfde3f642840d91ef9ddc67c7171fbefe
Pedro Falcato <pedro.falcato@gmail.com>
2025-07-05 19:58:28 +0100

    x86: Fix TSC timer behavior when frequency > U32_MAX
    On certain cpus (like my AMD Ryzen 9 9900X), the TSC frequency can be
    advertised/measured to be larger than UINT32_MAX. Our old logic would
    assert on this, and would fall flat on its face.
    
    To avoid this, switch to a new cyc2ns/ns2cyc conversion logic taken from
    Linux. Namely, we calculate mult and shift and use those as the main
    parameters to do these sorts of conversions.
    
    For now, we're only doing this logic in tsc.cpp, but it really should be
    taken to generic clocksource and timer logic.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/arch/x86_64/tsc.cpp
+++ kernel/arch/x86_64/tsc.cpp
@@ -1,15 +1,16 @@
 /*
- * Copyright (c) 2017 - 2021 Pedro Falcato
+ * Copyright (c) 2017 - 2025 Pedro Falcato
  * This file is part of Onyx, and is released under the terms of the GPLv2 License
  * check LICENSE at the root directory for more information
  *
  * SPDX-License-Identifier: GPL-2.0-only
  */
-
+#define pr_fmt(fmt) "x86/tsc: " fmt
 #include <fractions.h>
 #include <stdbool.h>
 #include <stdint.h>
 
+#include <onyx/bug.h>
 #include <onyx/clock.h>
 #include <onyx/cpu.h>
 #include <onyx/log.h>
@@ -21,6 +22,8 @@
 
 static struct fp_32_64 ticks_per_ns;
 static struct fp_32_64 ns_per_tick;
+static u32 tsc_mult, tsc_shift;
+static u32 tsc_remult, tsc_reshift;
 
 uint64_t tsc_get_ticks(void);
 hrtime_t tsc_elapsed_ns(hrtime_t start, hrtime_t end);
@@ -37,90 +40,156 @@ struct clocksource tsc_clock = {
 
 #define TSC_MAX_COUNT UINT64_MAX
 
+static PER_CPU_VAR(u64 last_cycle);
+static PER_CPU_VAR(u64 last_ns);
+
 hrtime_t tsc_get_ns(void)
 {
-    hrtime_t ticks = rdtsc();
+    unsigned long flags = irq_save_and_disable();
+    hrtime_t ns;
+    u64 delta, last_ns_;
+    u64 ticks = rdtsc();
 
-    /*if(ticks < tsc_clock.last_cycle)
-    {
-        printk("ticks %lu\n", ticks);
-        panic("AHHHHHHH");
-        tsc_clock.base += fract_div_u64_fract(TSC_MAX_COUNT, &ticks_per_ns);
-    }*/
-
-    __atomic_store_n(&tsc_clock.last_cycle, ticks, __ATOMIC_RELAXED);
-
-    return tsc_clock.base + tsc_clock.monotonic_warp + (u64_mul_u64_fp32_64(ticks, ticks_per_ns));
+    WARN_ON(ticks < get_per_cpu(last_cycle));
+    delta = ticks - get_per_cpu(last_cycle);
+    WRITE_ONCE(tsc_clock.last_cycle, ticks);
+    last_ns_ = get_per_cpu(last_ns);
+    ns = last_ns_ + ((delta * tsc_mult) >> tsc_shift);
+    write_per_cpu(last_cycle, ticks);
+    write_per_cpu(last_ns, ns);
+    ns += tsc_clock.base + tsc_clock.monotonic_warp;
+    irq_restore(flags);
+    return ns;
 }
 
 hrtime_t tsc_get_counter_from_ns(hrtime_t t)
 {
-    hrtime_t tsc_ns_time = t - tsc_clock.base - tsc_clock.monotonic_warp;
-    if (t > tsc_ns_time)
+    long delta = t - clocksource_get_time();
+    if (delta <= 0)
+        delta = 16;
+    return (delta * tsc_remult) >> tsc_reshift;
+}
+
+/**
+ * clocks_calc_mult_shift - calculate mult/shift factors for scaled math of clocks
+ * @mult:	pointer to mult variable
+ * @shift:	pointer to shift variable
+ * @from:	frequency to convert from
+ * @to:		frequency to convert to
+ * @maxsec:	guaranteed runtime conversion range in seconds
+ *
+ * The function evaluates the shift/mult pair for the scaled math
+ * operations of clocksources and clockevents.
+ *
+ * @to and @from are frequency values in HZ. For clock sources @to is
+ * NSEC_PER_SEC == 1GHz and @from is the counter frequency. For clock
+ * event @to is the counter frequency and @from is NSEC_PER_SEC.
+ *
+ * The @maxsec conversion range argument controls the time frame in
+ * seconds which must be covered by the runtime conversion with the
+ * calculated mult and shift factors. This guarantees that no 64bit
+ * overflow happens when the input value of the conversion is
+ * multiplied with the calculated mult factor. Larger ranges may
+ * reduce the conversion accuracy by choosing smaller mult and shift
+ * factors.
+ */
+void clocks_calc_mult_shift(u32 *mult, u32 *shift, u32 from, u32 to, u32 maxsec)
+{
+    u64 tmp;
+    u32 sft, sftacc = 32;
+
+    /*
+     * Calculate the shift factor which is limiting the conversion
+     * range:
+     */
+    tmp = ((u64) maxsec * from) >> 32;
+    while (tmp)
     {
-        printk("tsc_ns_time bad time\n");
-        printk("Base %lx, warp %lx, t %lx, cpu %u\n", tsc_clock.base, tsc_clock.monotonic_warp, t,
-               get_cpu_nr());
-        printk("tsc_ns_time: %lx\n", tsc_ns_time);
-        panic("bad tsc ns");
+        tmp >>= 1;
+        sftacc--;
     }
 
-    return u64_mul_u64_fp32_64(tsc_ns_time, ns_per_tick);
+    /*
+     * Find the conversion shift/mult pair which has the best
+     * accuracy and fits the maxsec conversion range:
+     */
+    for (sft = 32; sft > 0; sft--)
+    {
+        tmp = (u64) to << sft;
+        tmp += from / 2;
+        tmp /= from;
+        if ((tmp >> sftacc) == 0)
+            break;
+    }
+    *mult = tmp;
+    *shift = sft;
 }
 
 static bool tsc_enabled = false;
 
-#undef TESTING_TSC
+static void do_tsc_freq(u32 freq, u32 scale)
+{
+    u64 sec;
+    /*
+     * Calc the maximum number of seconds which we can run before
+     * wrapping around. For clocksources which have a mask > 32-bit
+     * we need to limit the max sleep time to have a good
+     * conversion precision. 10 minutes is still a reasonable
+     * amount. That results in a shift value of 24 for a
+     * clocksource with mask >= 40-bit and f >= 4GHz. That maps to
+     * ~ 0.06ppm granularity for NTP.
+     */
+    sec = TSC_MAX_COUNT;
+    sec /= freq;
+    sec /= scale;
+    if (!sec)
+        sec = 1;
+    else if (sec > 600 && /* cs->mask > UINT_MAX */ true)
+        sec = 600;
+
+    clocks_calc_mult_shift(&tsc_mult, &tsc_shift, freq, NS_PER_SEC / scale, sec * scale);
+    clocks_calc_mult_shift(&tsc_remult, &tsc_reshift, NS_PER_SEC / scale, freq, sec * scale);
+}
+
 void tsc_init(void)
 {
+    u64 freq;
     if (!x86_has_usable_tsc())
     {
-        INFO("tsc", "Invariant/Constant TSC not available - tsc is not able to be "
-                    "used as a clock source\n");
+        pr_info("Invariant/Constant TSC not available - tsc is not able to be used as a clock source\n");
         return;
     }
-    uint64_t freq = x86_get_tsc_rate();
+    
+    freq = x86_get_tsc_rate();
+    do_tsc_freq(freq / 1000, 1000);
+    pr_info("Frequency: %lu Hz\n", freq);
 
-    INFO("tsc", "Frequency: %lu Hz\n", freq);
-    assert(freq <= UINT32_MAX);
-
-    fp_32_64_div_32_32(&ns_per_tick, (uint32_t) freq, NS_PER_SEC);
-
-    fp_32_64_div_32_32(&ticks_per_ns, NS_PER_SEC, (uint32_t) freq);
+    fp_32_64_div_32_32(&ns_per_tick, (uint32_t) min(freq, (u64) UINT32_MAX), NS_PER_SEC);
+    fp_32_64_div_32_32(&ticks_per_ns, NS_PER_SEC, (uint32_t) min(freq, (u64) UINT32_MAX));
 
     // printk("ticks per ns: %lu/%lu\n", ticks_per_ns.numerator, ticks_per_ns.denominator);
 
     tsc_clock.rate = freq;
-    tsc_clock.monotonic_warp = -u64_mul_u64_fp32_64(rdtsc(), ticks_per_ns);
+    tsc_clock.monotonic_warp = -((rdtsc() * tsc_mult) >> tsc_shift);
     tsc_clock.last_cycle = rdtsc();
     tsc_clock.ticks_per_ns = &ticks_per_ns;
 
     register_clock_source(&tsc_clock);
 
     tsc_enabled = true;
-
-#ifdef TESTING_TSC
-    // printk("Sleeping. \n");
-    // printk("gtc %lu\n", get_tick_count());
-    printk("Start %lu\n", get_tick_count());
-    unsigned long start = tsc_clock.get_ns();
-
-    while (tsc_clock.get_ns() - start < (NS_PER_SEC * 2))
-        ;
-    printk("Slept. gtc %lu\n", get_tick_count());
-#endif
 }
 
 hrtime_t tsc_elapsed_ns(hrtime_t start, hrtime_t end)
 {
     hrtime_t delta = clock_delta_calc(start, end);
-    return u64_mul_u64_fp32_64(delta, ticks_per_ns);
+    return (delta * tsc_mult) >> tsc_shift;
 }
 
 void tsc_setup_vdso(struct vdso_time *time)
 {
     if (!tsc_enabled)
         return;
-    time->ticks_per_ns = ticks_per_ns;
+    time->mult = tsc_mult;
+    time->shift = tsc_shift;
     time->using_tsc = x86_has_usable_tsc();
 }
