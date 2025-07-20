762fa7660b707f6dca39ee65897e10eed1953d72
Pedro Falcato <pedro.falcato@gmail.com>
2025-07-20 22:01:49 +0100

    fs: Move ->symlink into inode_operations
    It's not a file op, and as such should be in inode_operations.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/include/onyx/inode.h
+++ kernel/include/onyx/inode.h
@@ -54,7 +54,6 @@ struct file_ops
     __close close;
     __getdirent getdirent;
     __ioctl ioctl;
-    __symlink symlink;
     void *(*mmap)(struct vm_area_struct *area, struct file *node);
     int (*on_open)(struct file *node);
     short (*poll)(void *poll_file, short events, struct file *node);
@@ -82,6 +81,7 @@ struct inode_operations
     int (*ftruncate)(size_t length, struct file *node);
     struct inode *(*mkdir)(struct dentry *dentry, mode_t mode, struct dentry *dir);
     struct inode *(*mknod)(struct dentry *dentry, mode_t mode, dev_t dev, struct dentry *dir);
+    __symlink symlink;
     char *(*readlink)(struct dentry *dentry);
     int (*unlink)(const char *name, int flags, struct dentry *dir);
     int (*magic_jump)(struct dentry *dentry, struct inode *inode, struct nameidata *data);
