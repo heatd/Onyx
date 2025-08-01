heatd/Onyx

commit 0d3e0f663e6ddfe234f8aec12921875e8751be48

namei: Do actual open of struct file in vfs_open
Move all open(2) fun stuff (perms checks, bookkeeping, etc) to
vfs_open(). Doing this in vfs_open() fixes a longstanding problem where
various parts of the kernel get half-initialized struct files, that then
cause problems when e.g doing ->release().

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/fs/namei.cpp
@@ -742,6 +742,73 @@ out:
     return st;
 }
 
+static bool may_noatime(file *f)
+{
+    creds_guard g;
+    return g.get()->euid == 0 || f->f_ino->i_uid == g.get()->euid;
+}
+
+static expected<file *, int> complete_open(struct file *filp, unsigned int flags)
+{
+    /* Given a half-open (as in half-initialized) file, complete the open() per open(2) semantics */
+    int err = 0;
+
+    /* Let's check for permissions */
+    if (!file_can_access(filp, open_to_file_access_flags(flags)))
+    {
+        err = -EACCES;
+        goto err_free_half;
+    }
+
+    // O_NOATIME can only be used when the euid of the process = owner of file, or
+    // when we're privileged (root).
+    if (flags & O_NOATIME)
+    {
+        if (!may_noatime(filp))
+        {
+            err = -EPERM;
+            goto err_free_half;
+        }
+    }
+
+    if (S_ISDIR(filp->f_ino->i_mode))
+    {
+        if (flags & O_RDWR || flags & O_WRONLY || (flags & O_CREAT && !(flags & O_DIRECTORY)))
+        {
+            err = -EISDIR;
+            goto err_free_half;
+        }
+    }
+
+    /* Call the fops on_open. This is required before we call any filesystem methods. */
+    if (filp->f_ino->i_fops->on_open)
+    {
+        err = filp->f_ino->i_fops->on_open(filp);
+        if (err < 0)
+            goto err_free_half;
+    }
+
+    if (flags & O_TRUNC)
+    {
+        int st = ftruncate_vfs(0, filp);
+        if (st < 0)
+        {
+            fd_put(filp);
+            return unexpected<int>{st};
+        }
+    }
+
+    filp->f_seek = 0;
+    filp->f_flags = flags;
+    return filp;
+
+err_free_half:
+    close_vfs(filp->f_ino);
+    path_put(&filp->f_path);
+    file_free(filp);
+    return unexpected<int>{err};
+}
+
 expected<file *, int> vfs_open(int dirfd, const char *name, unsigned int open_flags, mode_t mode)
 {
     const unsigned int flags = open_flags & O_DIRECTORY ? LOOKUP_MUST_BE_DIR : 0;
@@ -796,7 +863,7 @@ expected<file *, int> vfs_open(int dirfd, const char *name, unsigned int open_fl
 
     inode_ref(p.dentry->d_inode);
     new_file->f_path = p;
-    return new_file;
+    return complete_open(new_file, open_flags);
 }
 
 static int do_lookup_parent_last(nameidata &data)

kernel/include/onyx/namei.h
@@ -1,4 +1,3 @@
-
 /*
  * Copyright (c) 2020 - 2023 Pedro Falcato
  * This file is part of Onyx, and is released under the terms of the GPLv2 License

kernel/kernel/fs/file.cpp
@@ -830,66 +830,6 @@ bool may_noatime(file *f)
     return g.get()->euid == 0 || f->f_ino->i_uid == g.get()->euid;
 }
 
-static expected<struct file *, int> try_to_open(int dirfd, const char *filename, int flags,
-                                                mode_t mode)
-{
-    auto ex = vfs_open(dirfd, filename, flags, mode);
-    if (ex.has_error())
-        return unexpected<int>{ex.error()};
-
-    struct file *ret = ex.value();
-
-    DCHECK(ret != nullptr);
-
-    if (ret)
-    {
-        /* Let's check for permissions */
-        if (!file_can_access(ret, open_to_file_access_flags(flags)))
-        {
-            fd_put(ret);
-            return unexpected<int>{-EACCES};
-        }
-
-        // O_NOATIME can only be used when the euid of the process = owner of file, or
-        // when we're privileged (root).
-        if (flags & O_NOATIME)
-        {
-            if (!may_noatime(ret))
-            {
-                fd_put(ret);
-                return unexpected<int>{-EPERM};
-            }
-        }
-
-        if (S_ISDIR(ret->f_ino->i_mode))
-        {
-            if (flags & O_RDWR || flags & O_WRONLY || (flags & O_CREAT && !(flags & O_DIRECTORY)))
-            {
-                fd_put(ret);
-                return unexpected<int>{-EISDIR};
-            }
-        }
-
-        if (flags & O_TRUNC)
-        {
-            int st = ftruncate_vfs(0, ret);
-            if (st < 0)
-            {
-                fd_put(ret);
-                return unexpected<int>{st};
-            }
-        }
-    }
-
-    if (ret)
-    {
-        ret->f_seek = 0;
-        ret->f_flags = flags;
-    }
-
-    return ret;
-}
-
 /* TODO: Add O_PATH */
 #define VALID_OPEN_FLAGS                                                                       \
     (O_RDONLY | O_WRONLY | O_RDWR | O_CREAT | O_DIRECTORY | O_EXCL | O_NOFOLLOW | O_NONBLOCK | \
@@ -910,23 +850,11 @@ int do_sys_open(const char *filename, int flags, mode_t mode, int dirfd)
     int fd_num = -1;
 
     /* Open/creat the file */
-    auto ex = try_to_open(dirfd, filename, flags, mode);
+    auto ex = vfs_open(dirfd, filename, flags, mode);
     if (ex.has_error())
-    {
         return ex.error();
-    }
 
     struct file *file = ex.value();
-
-    if (file->f_ino->i_fops->on_open)
-    {
-        int st = file->f_ino->i_fops->on_open(file);
-        if (st < 0)
-        {
-            fd_put(file);
-            return st;
-        }
-    }
 
     /* Allocate a file descriptor and a file description for the file */
     fd_num = open_with_vnode(file, flags);

commit 5dfdea47b95863218135b3896e7fd7b4ccdf2389

fs/open: Set f_flags before calling on_open
->on_open() can sometimes expect f_flags to be properly filled before
being called. One such case is the named pipe code, that directly deals
with O_NONBLOCK there. As such, set that beforehand.

Fixes: 0d3e0f663e6d ("namei: Do actual open of struct file in vfs_open")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/fs/namei.cpp
@@ -759,6 +759,9 @@ static expected<file *, int> complete_open(struct file *filp, unsigned int flags
         }
     }
 
+    filp->f_seek = 0;
+    filp->f_flags = flags;
+
     /* Call the fops on_open. This is required before we call any filesystem methods. */
     if (filp->f_ino->i_fops->on_open)
     {
@@ -777,8 +780,6 @@ static expected<file *, int> complete_open(struct file *filp, unsigned int flags
         }
     }
 
-    filp->f_seek = 0;
-    filp->f_flags = flags;
     return filp;
 
 err_free_half: