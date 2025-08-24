heatd/Onyx

commit fd6fb4834c2d0466903b614e264b59ec06254017

mm: Fix ordering of stores on active_mask maintenance
Essentially, these are two invariants we _really_ want:

1) any cpu on the active_mask _may_ be using the mm
2) _no_ cpu not on the active mask will be using the mm

With the previous code, we could observe the following:

Example 1: vm_load_aspace

vm_load_arch_mmu(&aspace->arch_mmu);
/* some instructions */
cpumask_set_atomic(&aspace->active_mask, cpu);

where the CPU can do TLB fills in arbitrary order, including finishing
the aspace load or the thread switch.

Example 2: sched_load_thread

cpumask_unset_atomic(&mm->active_mask, cpu);
<some other CPU that has observed this active_mask sends a TLB
shootdown>
native::arch_load_process(thread->owner, thread, cpu);
   vm_load_arch_mmu
	paging_load_cr3
		if (oldpml == pml)
			return;
where the CPU running sched_load_thread will never get an IPI, thus will
never shoot the TLB down.

Fix it by fixing ordering here. We tolerate a CPU being extraneously in
an active_mask, so there will be a small overlap (in context switches)
where a CPU may be set in two active masks.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/sched/scheduler.cpp
@@ -447,14 +447,16 @@ void sched_load_thread(struct thread *prev, thread *thread, unsigned int cpu)
     if (thread->owner)
     {
         /* Clear ourselves from the mm mask and drop the active_mm, if we had one */
-        cpumask_unset_atomic(&mm->active_mask, cpu);
-
-        native::arch_load_process(thread->owner, thread, cpu);
-
         if (prev->active_mm)
         {
             mmdrop(mm);
             prev->active_mm = NULL;
+        }
+
+        if (mm != thread->owner->get_aspace())
+        {
+            cpumask_unset_atomic(&mm->active_mask, cpu);
+            native::arch_load_process(thread->owner, thread, cpu);
         }
     }
     else

kernel/kernel/mm/vm.c
@@ -2626,10 +2626,10 @@ struct mm_address_space *mm_fork(void)
  */
 void vm_load_aspace(struct mm_address_space *aspace, unsigned int cpu)
 {
-    vm_load_arch_mmu(&aspace->arch_mmu);
     if (unlikely(cpu == -1U))
         cpu = get_cpu_nr();
     cpumask_set_atomic(&aspace->active_mask, cpu);
+    vm_load_arch_mmu(&aspace->arch_mmu);
 }
 
 /**

commit 826a4c1dc88956f86c5995394a032768204ca6e7

sched: Fix UAF when clearing the active_mask on active_mm's
Since the linked commit, we could very easily drop an active mm, and
then unset the bit. This is wrong and an obvious UAF.

Fixes: fd6fb4834c2d ("mm: Fix ordering of stores on active_mask maintenance")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/sched/scheduler.cpp
@@ -438,6 +438,7 @@ void sched_decrease_quantum(clockevent *ev)
 void sched_load_thread(struct thread *prev, thread *thread, unsigned int cpu)
 {
     struct mm_address_space *mm = prev->active_mm ?: prev->aspace;
+    bool cleared = false;
     write_per_cpu(current_thread, thread);
 
     errno = thread->errno_val;
@@ -449,13 +450,19 @@ void sched_load_thread(struct thread *prev, thread *thread, unsigned int cpu)
         /* Clear ourselves from the mm mask and drop the active_mm, if we had one */
         if (prev->active_mm)
         {
+            /* Clear ourselves from the active mask before dropping the mm. We must do it this way,
+             * since mmdrop() can very well just free the mm. */
+            cpumask_unset_atomic(&mm->active_mask, cpu);
+            cleared = true;
             mmdrop(mm);
             prev->active_mm = NULL;
         }
 
         if (mm != thread->owner->get_aspace())
         {
-            cpumask_unset_atomic(&mm->active_mask, cpu);
+            /* We didn't clear it yet? Clear it now. */
+            if (!cleared)
+                cpumask_unset_atomic(&mm->active_mask, cpu);
             native::arch_load_process(thread->owner, thread, cpu);
         }
     }