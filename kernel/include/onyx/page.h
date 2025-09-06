heatd/Onyx

commit aee336075f5926043939a359fac956ea43024476

filemap: Remove the locking in vmo_get
We don't need to lock in vmo_get. We can simply rely on the fact that
our radix tree never reclaims tables, plus some clever retry loop +
try_get operation.

This solves a scalability problem when e.g executing too much of a
single process, on e.g a build.

Tested with make -j16 -sss on musl.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/include/onyx/page.h
@@ -269,6 +269,20 @@ static inline unsigned long page_ref(struct page *p)
     return newrefs;
 }
 
+static inline bool page_try_get(struct page *p)
+{
+    unsigned int old = READ_ONCE(p->ref), expected;
+    do
+    {
+        if (old == 0)
+            return false;
+        expected = old;
+        old = cmpxchg_relaxed(&p->ref, old, old + 1);
+    } while (old != expected);
+
+    return true;
+}
+
 static inline unsigned long page_ref_many(struct page *p, unsigned long c)
 {
     return __atomic_add_fetch(&p->ref, c, __ATOMIC_ACQUIRE);

kernel/kernel/mm/vm_object.cpp
@@ -119,10 +119,7 @@ struct page *vm_object::insert_page_unlocked(unsigned long off, struct page *pag
  */
 vmo_status_t vmo_get(vm_object *vmo, size_t off, unsigned int flags, struct page **ppage)
 {
-    vmo_status_t st = VMO_STATUS_OK;
     struct page *p = nullptr;
-
-    scoped_lock g{vmo->page_lock};
 
 #if 1
     if (vmo->ino && !(vmo->flags & VMO_FLAG_DEVICE_MAPPING))
@@ -132,20 +129,27 @@ vmo_status_t vmo_get(vm_object *vmo, size_t off, unsigned int flags, struct page
     if (off >= vmo->size)
         return VMO_STATUS_BUS_ERROR;
 
-    auto ex = vmo->vm_pages.get(off >> PAGE_SHIFT);
-    if (ex.has_value())
+    do
+    {
+        auto ex = vmo->vm_pages.get(off >> PAGE_SHIFT);
+        if (!ex.has_value())
+            return VMO_STATUS_NON_EXISTENT;
         p = (struct page *) ex.value();
 
-    if (!p)
-        st = VMO_STATUS_NON_EXISTENT;
+        if (!page_try_get(p))
+            continue;
+        auto ex2 = vmo->vm_pages.get(off >> PAGE_SHIFT);
+        if (!ex2.has_value() || (struct page *) ex2.value() != p)
+        {
+            page_unref(p);
+            continue;
+        }
 
-    if (st == VMO_STATUS_OK)
-    {
-        page_pin(p);
-        *ppage = p;
-    }
+        break;
+    } while (1);
 
-    return st;
+    *ppage = p;
+    return VMO_STATUS_OK;
 }
 
 /**

commit 0cfd67d5808f74f66611e82e32598f6004fa4d36

mm: Fix vm_object page removal on reclaim
Before aee336075f59, simply holding the vm_object's lock would mean
that vm_obj_remove_page could simply exclude itself from anyone
trying to grab a page. Now, after that commit, vmo_get stopped
taking the lock. This manifested itself in brokenness, with reclaim
racing with filemap code trying to grab a page from the cache.

To fix that, we introduce the concept of a "frozen page", which is
simply a page with refcount = 0. The code in vmo_get() already
recognized such a concept, with page_try_get() aborting() if refcount =
0. This concept is used in a change in semantics, where
vm_obj_remove_page() itself freezes the page before removing it and
returning (note that freezing is an atomic operation, and may fail).

Fixes: aee336075f59 ("filemap: Remove the locking in vmo_get")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/include/onyx/page.h
@@ -293,6 +293,16 @@ static inline unsigned long __page_unref(struct page *p)
     return __atomic_sub_fetch(&p->ref, 1, __ATOMIC_RELEASE);
 }
 
+static inline bool page_ref_freeze(struct page *page, unsigned int expected)
+{
+    return cmpxchg_relaxed(&page->ref, expected, 0) == expected;
+}
+
+static inline void page_ref_unfreeze(struct page *page, unsigned int refs)
+{
+    WRITE_ONCE(page->ref, refs);
+}
+
 #define page_unref(p) free_page(p)
 
 static inline unsigned long page_unref_many(struct page *p, unsigned long c)

kernel/kernel/mm/reclaim.c
@@ -333,8 +333,9 @@ static enum lru_result shrink_page(struct reclaim_data *data,
     }
 
     unlock_page(page);
-    DCHECK(page->ref == 2);
-    page_unref(page);
+    DCHECK(page->ref == 0);
+    /* One ref for "us". ideally we would be able to free frozen pages, but we can't. */
+    page_ref_unfreeze(page, 1);
     if (!page_flag_set(page, PAGE_FLAG_ANON))
         dec_page_stat(page, NR_FILE);
 

kernel/kernel/mm/swap.c
@@ -803,6 +803,8 @@ static void swap_cache_remove(struct vm_object *obj, struct page *page)
         return;
     page_clear_swap(page);
     swap_final_put(entry);
+    /* Refs were previously frozen, add one ref for "us" */
+    page_ref_unfreeze(page, 1);
 }
 
 static int do_protnone(swp_entry_t swp, struct vm_pf_context *context)

kernel/kernel/mm/vm_object.cpp
@@ -567,28 +567,19 @@ void vm_obj_clean_page(struct vm_object *obj, struct page *page)
 
 bool vm_obj_remove_page(struct vm_object *obj, struct page *page)
 {
-    bool ret = false;
     DCHECK_PAGE(page_locked(page), page);
     DCHECK_PAGE(page_vmobj(page) == obj, page);
     DCHECK_PAGE(page->ref != 0, page);
 
-    spin_lock(&obj->page_lock);
-    /* Under the lock, pages can't get their references incremented, so we check if pins == 2 here.
-     * We take into account the mapcount, because virtual references can be punted off.
-     */
-
     unsigned int expected_refs = 2 + (page_mapcount(page) > 0);
-    if (__atomic_load_n(&page->ref, __ATOMIC_RELAXED) > expected_refs)
-        goto out;
+    if (!page_ref_freeze(page, expected_refs))
+        return false;
 
     obj->vm_pages.store(page_pgoff(page), 0);
     if (page_test_swap(page))
         swap_unset_swapcache(swpval_to_swp_entry(page->priv));
-    /* We do not need to reset owner here, because we're the only reference */
-    ret = true;
-out:
-    spin_unlock(&obj->page_lock);
-    return ret;
+    page->owner = NULL;
+    return true;
 }
 
 long vm_obj_get_page_references(struct vm_object *obj, struct page *page, unsigned int *vm_flags)