heatd/Onyx

commit 7099d45e68e014c97dd1350c9f273bcdd68511f6

uart8250: Add console driver
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/drivers/serial/uart8250/uart8250.cpp
@@ -166,6 +166,40 @@ ssize_t uart8250_port::write_serial(const void *buffer, size_t size)
     return size;
 }
 
+int uart8250_port::write_console(const char *buf, size_t len, unsigned int flags)
+{
+    if (flags & (CONSOLE_WRITE_ATOMIC | CONSOLE_WRITE_PANIC))
+    {
+        if (!(flags & CONSOLE_WRITE_PANIC) && !spin_try_lock(&lock_))
+            return -EAGAIN;
+    }
+    else
+        spin_lock(&lock_);
+
+    for (size_t i = 0; i < len; i++)
+    {
+        auto byte = static_cast<uint8_t>(buf[i]);
+
+        // Needed for basic printing as a debug console
+        if (byte == '\n') [[unlikely]]
+            write_byte('\r');
+        write_byte(static_cast<uint8_t>(*(buf + i)));
+    }
+
+    if (!(flags & CONSOLE_WRITE_PANIC))
+        spin_unlock(&lock_);
+
+    return 0;
+}
+
+static int uart8250_write_con(const char *buf, size_t len, unsigned int flags, struct console *con)
+{
+    uart8250_port *port = (uart8250_port *) con->priv;
+    return port->write_console(buf, len, flags);
+}
+
+static const struct console_ops uart8250_con_ops = {.write = uart8250_write_con};
+
 /**
  * @brief Initialises the serial port as a standard serial port
  *
@@ -175,6 +209,11 @@ bool uart8250_port::init()
 {
     if (!early_init())
         return false;
+
+    con = (struct console *) kmalloc(sizeof(*con), GFP_KERNEL);
+    console_init(con, "uart8250", &uart8250_con_ops);
+    con->priv = this;
+    con_register(con);
 
     install_irq(irq_, uart8250_irq, dev_, IRQ_FLAG_REGULAR, this);
 

kernel/drivers/serial/uart8250/uart8250.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 - 2023 Pedro Falcato
+ * Copyright (c) 2020 - 2024 Pedro Falcato
  * This file is part of Onyx, and is released under the terms of the MIT License
  * check LICENSE at the root directory for more information
  *
@@ -11,6 +11,7 @@
 
 #include <stdint.h>
 
+#include <onyx/console.h>
 #include <onyx/dev.h>
 #include <onyx/dpc.h>
 #include <onyx/driver.h>
@@ -82,6 +83,7 @@ class uart8250_port : public serial_port
     unsigned int irq_;
     spinlock lock_;
     device *dev_;
+    struct console *con;
 
     static constexpr uint32_t serial_clock = 115200;
     static constexpr uint16_t default_baud_rate = 38400;
@@ -153,6 +155,8 @@ public:
     ssize_t write_serial(const void *buffer, size_t size) override;
 
     irqstatus_t on_irq();
+
+    int write_console(const char *buf, size_t len, unsigned int flags);
 };
 
 #endif

commit 8099199db18b761f7b3b9c8af97d052f5271874e

uart8250: Fix the spin_try_lock in console atomic paths
The spin_try_lock's check is backwards.

Fixes: 7099d45e68e0 ("uart8250: Add console driver")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/drivers/serial/uart8250/uart8250.cpp
@@ -170,7 +170,7 @@ int uart8250_port::write_console(const char *buf, size_t len, unsigned int flags
 {
     if (flags & (CONSOLE_WRITE_ATOMIC | CONSOLE_WRITE_PANIC))
     {
-        if (!(flags & CONSOLE_WRITE_PANIC) && !spin_try_lock(&lock_))
+        if (!(flags & CONSOLE_WRITE_PANIC) && spin_try_lock(&lock_))
             return -EAGAIN;
     }
     else