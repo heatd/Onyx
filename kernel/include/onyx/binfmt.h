heatd/Onyx

commit f9e2cae892c2d8b8a17d522d8bcc5c7daee7220f

binfmt: Add shebang. ahci: Possibly fix bug.
Signed-off-by: heatd <pedro.falcato@gmail.com>

kernel/include/onyx/binfmt.h
@@ -27,11 +27,14 @@ struct binfmt_args
 	uint8_t *file_signature;
 	char *filename;
 	char **argv, **envp;
+	int *argc;
 	struct file *file;
 	char *interp_path;
 	bool needs_interp;
 	struct exec_state *state;
 };
+
+#define BINFMT_SIGNATURE_LENGTH     100
 
 #ifdef __cplusplus
 extern "C"{

kernel/kernel/binfmt/shebang.cpp
@@ -1,0 +1,199 @@
+/*
+* Copyright (c) 2020 Pedro Falcato
+* This file is part of Onyx, and is released under the terms of the MIT License
+* check LICENSE at the root directory for more information
+*/
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <onyx/binfmt.h>
+#include <onyx/process.h>
+
+static char *find_space_or_tab(const char *str, size_t len)
+{
+	while(len--)
+	{
+		if(*str == ' ' || *str == '\t')
+			return (char *) str;
+		str++;
+	}
+
+	return NULL;
+}
+
+static char *find_space_or_tab_or_zero(const char *str, size_t len)
+{
+	while(len--)
+	{
+		if(*str == ' ' || *str == '\t' || *str == '\0')
+			return (char *) str;
+		str++;
+	}
+
+	return NULL;
+}
+
+static char *find_not_space_nor_tab(const char *str, size_t len)
+{
+	while(len--)
+	{
+		if(*str != ' ' && *str != '\t')
+			return (char *) str;
+		str++;
+	}
+
+	return NULL;
+}
+
+#if 0
+static int count_argv(char **argv)
+{
+	int argc = 0;
+	while(*argv++)
+		argc++;
+	return argc;
+}
+
+void dump_argv(char **argv)
+{
+	while(*argv)
+		printk("arg %s\n", *argv++);
+}
+#endif
+
+void *shebang_load(struct binfmt_args *args)
+{
+	char *buf = (char *) args->file_signature;
+	char *end = buf + BINFMT_SIGNATURE_LENGTH;
+
+	char *p = strnchr(buf, BINFMT_SIGNATURE_LENGTH, '\n');
+	if(!p)
+	{
+		/* If we don't have a newline in the buffer, check for a space or tab.
+		 * If we can find it, the interpreter path isn't truncated, so we can continue.
+		 */
+		p = find_not_space_nor_tab(buf + 2, BINFMT_SIGNATURE_LENGTH - 2);
+		
+		/* The entire buffer is spaces/tabs, so it's a bad executable */
+		if(!p)
+			return errno = ENOEXEC, nullptr;
+		
+		/* Interpreter path truncated. */
+		if(!find_space_or_tab_or_zero(p, end - p))
+			return errno = ENOEXEC, nullptr;
+		p = end - 1;
+	}
+
+	/* Now we need to null terminate the buffer and overwrite spaces/tabs at the end with zeroes */
+	*p = '\0';
+	p--;
+
+	while(p > buf)
+	{
+		if(*p == ' ' || *p == '\t')
+			*p = '\0';
+		else
+			break;
+		p--;
+	}
+
+	/* Point to buf + 2(the length of #!) and go past the initial spaces/tabs */
+	char *interp = buf + 2;
+
+	while(*interp == ' ' || *interp == '\t')
+		interp++;
+
+	/* There's no path :( */
+	if(*interp == '\0')
+		return errno = ENOEXEC, nullptr;
+
+	char *arg = find_space_or_tab_or_zero(interp, end - interp);
+	if(*arg != '\0')
+	{
+		while(*arg == ' ' || *arg == '\t')
+			*arg++ = '\0';
+	}
+	else
+		arg = nullptr;
+
+	interp = strdup(interp);
+	if(!interp)
+		return errno = ENOMEM, nullptr;
+
+	char **old_kargs = args->argv;
+
+	bool argc_is_zero = args->argc == 0;
+
+	/* Calculate the new argc */
+	int argc = *args->argc + (arg != nullptr ? 2 : 1);
+
+	if(argc_is_zero) argc++;
+
+	char **new_argv = (char **) calloc(sizeof(void *), argc + 1);
+	if(!new_argv)
+	{
+		free(interp);
+		return errno = ENOMEM, nullptr;
+	}
+
+	int curr = 0;
+	new_argv[curr++] = interp;
+	if(arg)  new_argv[curr++] = arg;
+	
+	/* We take the time to insert an argv[0] with the script's name forcefully. */
+	if(argc_is_zero) new_argv[curr++] = args->filename;
+
+	for(int i = 0; i < *args->argc + 1; i++)
+	{
+		char *arg = old_kargs[i];
+		if(i == 0)
+			arg = args->filename;
+
+		new_argv[curr++] = arg;
+	}
+
+	char **new_args = process_copy_envarg(new_argv, true, &argc);
+	
+	free(new_argv);
+
+	if(!new_args)
+	{
+		free(interp);
+		return errno = ENOMEM, nullptr;
+	}
+
+	args->argv = new_args;
+	*args->argc = argc;
+
+	free(old_kargs);
+
+	args->interp_path = interp;
+	args->needs_interp = false;
+
+	void *entry = bin_do_interp(args);
+
+	free(interp);
+	args->interp_path = NULL;
+
+	return entry;
+}
+
+bool shebang_is_valid(uint8_t *signature)
+{
+	return memcmp(signature, "#!", 2) == 0;
+}
+
+struct binfmt shebang_binfmt = {
+	.signature = (unsigned char *) "#!",
+	.size_signature = 2,
+	.is_valid_exec = shebang_is_valid,
+	.callback = shebang_load,
+	.next = NULL
+};
+
+__init void __elf_init()
+{
+	install_binfmt(&shebang_binfmt);
+}

kernel/acpica/components/utilities/utclib.c
@@ -450,39 +450,6 @@ strcmp (
     return ((unsigned char) *String1 - (unsigned char) *String2);
 }
 
-
-/*******************************************************************************
- *
- * FUNCTION:    strchr
- *
- * PARAMETERS:  String          - Search string
- *              ch              - character to search for
- *
- * RETURN:      Ptr to char or NULL if not found
- *
- * DESCRIPTION: Search a string for a character
- *
- ******************************************************************************/
-
-char *
-strchr (
-    const char              *String,
-    int                     ch)
-{
-
-
-    for ( ; (*String); String++)
-    {
-        if ((*String) == (char) ch)
-        {
-            return ((char *) String);
-        }
-    }
-
-    return (NULL);
-}
-
-
 /*******************************************************************************
  *
  * FUNCTION:    strncmp

kernel/drivers/ahci/ahci.c
@@ -111,9 +111,9 @@ irqstatus_t ahci_irq(struct irq_context *ctx, void *cookie)
 		if(ports & (1U << i))
 		{
 			uint32_t port_is = port->port->interrupt_status;
+			port->port->interrupt_status = port_is;
 			dev->hba->interrupt_status = (1U << i);
 			ahci_do_port_irqs(port);
-			port->port->interrupt_status = port_is;
 		}
 
 		spin_unlock_irqrestore(&port->port_lock);

kernel/kernel/Makefile
@@ -8,7 +8,7 @@ kern-y+= acpi.o acpica_port.o arc4random.o binfmt.o copy.o cppnew.o cpprt.o crc3
 kern-$(CONFIG_UBSAN)+= ubsan.o
 
 kern-y+= fonts/font.o drm/drm.o drm/software.o binfmt/elf.o binfmt/elf64.o binfmt/exec.o \
-	libdict/rb_tree.o libdict/tree_common.o libdict/wb_tree.o time/tickless.o
+	libdict/rb_tree.o libdict/tree_common.o libdict/wb_tree.o time/tickless.o binfmt/shebang.o
 
 kern-$(CONFIG_KTRACE)+= ktrace.o
 

kernel/kernel/binfmt.c
@@ -61,7 +61,10 @@ void *bin_do_interp(struct binfmt_args *_args)
 		return NULL;
 	}
 
-	read_vfs(0, 100, args.file_signature, file);
+	if(read_vfs(0, BINFMT_SIGNATURE_LENGTH, args.file_signature, file) < 0)
+	{
+		return NULL;
+	}
 
 	args.filename = args.interp_path;
 	args.file = file;

kernel/kernel/binfmt/elf64.c
@@ -157,7 +157,7 @@ void *elf64_load_static(struct binfmt_args *args, Elf64_Ehdr *header)
 
 void *elf64_load_dyn(struct binfmt_args *args, Elf64_Ehdr *header)
 {
-	bool is_interp = args->interp_path != NULL;
+	bool is_interp = args->needs_interp;
 
 	struct process *current = get_current_process();
 	size_t program_headers_size = header->e_phnum * header->e_phentsize;

kernel/kernel/binfmt/exec.c
@@ -269,7 +269,7 @@ int sys_execve(char *p, char *argv[], char *envp[])
 	}
 	
 	/* Setup the binfmt args */
-	file = malloc(100);
+	file = zalloc(BINFMT_SIGNATURE_LENGTH);
 	if(!file)
 	{
 		st = -ENOMEM;
@@ -279,7 +279,7 @@ int sys_execve(char *p, char *argv[], char *envp[])
 	unsigned long old = thread_change_addr_limit(VM_KERNEL_ADDR_LIMIT);
 
 	/* Read the file signature */
-	if(read_vfs(0, 100, file, exec_file) < 0)
+	if(read_vfs(0, BINFMT_SIGNATURE_LENGTH, file, exec_file) < 0)
 	{
 		st = -errno;
 		goto error;
@@ -292,9 +292,15 @@ int sys_execve(char *p, char *argv[], char *envp[])
 	args.envp = kenv;
 	args.file = exec_file;
 	args.state = &state;
+	args.argc = &argc;
 
 	/* Load the actual binary */
 	void *entry = load_binary(&args);
+
+	/* Update the pointers since the binary loader might've changed them(for example, shebang does that) */
+	karg = args.argv;
+	kenv = args.envp;
+
 	if(!entry)
 	{
 		st = -errno;
@@ -354,6 +360,7 @@ error_die_signal:
 	kernel_raise_signal(SIGKILL, current, SIGNAL_FORCE, NULL);
 
 	/* This sched_yield should execute the signal handler */
+	/* TODO: This doesn't work because we're in kernel space */
 	sched_yield();
 	return -1;
 

commit fc801f98418c86dc1ef6aa791d8855e84c2cca84

binfmt/shebang: Refuse to load as an interpreter
The following trivial shebang can result in a kernel stack
overflow crash:

/test.sh:
 #!/test.sh

Fix it by detecting whether we're being executed in an interpreter
fashion, and outright refuse to execute. This stops the overflow by
never actually recursing.

Fixes: f9e2cae892c2 ("binfmt: Add shebang. ahci: Possibly fix bug.")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/include/onyx/binfmt.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 - 2021 Pedro Falcato
+ * Copyright (c) 2017 - 2025 Pedro Falcato
  * This file is part of Onyx, and is released under the terms of the GPLv2 License
  * check LICENSE at the root directory for more information
  *
@@ -34,6 +34,7 @@ struct binfmt_args
     struct file *file;
     char *interp_path;
     bool needs_interp;
+    bool is_interp;
     struct exec_state *state;
 };
 

kernel/kernel/binfmt.cpp
@@ -76,6 +76,7 @@ void *bin_do_interp(struct binfmt_args *_args)
 
     args.filename = args.interp_path;
     args.file = file;
+    args.is_interp = true;
 
     return load_binary(&args);
 }

kernel/kernel/binfmt/exec.cpp
@@ -439,6 +439,7 @@ int sys_execve(const char *p, const char **argv, const char **envp)
     args.file = exec_file;
     args.state = &state;
     args.argc = &argc;
+    args.is_interp = false;
 
     /* Load the actual binary */
     entry = load_binary(&args);

kernel/kernel/binfmt/shebang.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 - 2023 Pedro Falcato
+ * Copyright (c) 2020 - 2025 Pedro Falcato
  * This file is part of Onyx, and is released under the terms of the GPLv2 License
  * check LICENSE at the root directory for more information
  *
@@ -49,26 +49,20 @@ static char *find_not_space_nor_tab(const char *str, size_t len)
     return nullptr;
 }
 
-#if 0
-static int count_argv(char **argv)
-{
-	int argc = 0;
-	while(*argv++)
-		argc++;
-	return argc;
-}
-
-void dump_argv(char **argv)
-{
-	while(*argv)
-		printk("arg %s\n", *argv++);
-}
-#endif
-
 void *shebang_load(struct binfmt_args *args)
 {
     char *buf = (char *) args->file_signature;
     char *end = buf + BINFMT_SIGNATURE_LENGTH;
+
+    if (args->is_interp)
+    {
+        /* Tried to load a shebang file as an interpreter? This is not going to work. Stop now
+         * instead of recursing further. */
+        pr_warn_once(
+            "exec: %s[%d]: Tried to load a shebang executable as an interpreter, failing\n",
+            get_current_process()->comm, get_current_process()->pid_);
+        return errno = ENOEXEC, nullptr;
+    }
 
     char *p = strnchr(buf, BINFMT_SIGNATURE_LENGTH, '\n');
     if (!p)
@@ -182,7 +176,6 @@ void *shebang_load(struct binfmt_args *args)
     free(old_kargs);
 
     args->interp_path = interp;
-    args->needs_interp = false;
 
     void *entry = bin_do_interp(args);
 