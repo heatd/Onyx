heatd/Onyx

commit 99064721f81710d0c0d40f9a6820052cc00178dc

fs/dcache: Do not null out d_parent on unlink
UNIX wants unattached (unlinked) directories to still be able to do
open("..") and get its parent inode. As such, don't NULL it out, and
simply unhash it.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/fs/dentry.cpp
@@ -418,7 +418,8 @@ static struct dentry *d_destroy(struct dentry *dentry)
         spin_lock(&parent->d_lock);
         /* Freeze refs. We'll see if we want to whack the parent dentry and return it if so */
         d_freeze_refs(parent);
-        list_remove(&dentry->d_parent_dir_node);
+        if (dentry->d_flags & DENTRY_FLAG_HASHED)
+            list_remove(&dentry->d_parent_dir_node);
     }
 
     if (dentry->d_name_length >= INLINE_NAME_MAX)
@@ -717,11 +718,19 @@ void dentry_do_unlink(dentry *entry)
 
     auto parent = entry->d_parent;
     DCHECK(spin_lock_held(&parent->d_lock));
-    dput_locked(parent);
     if ((entry->d_flags & (DENTRY_FLAG_LRU | DENTRY_FLAG_SHRINK)) == DENTRY_FLAG_LRU)
         d_remove_lru(entry);
-    entry->d_parent = nullptr;
 
+    d_freeze_refs(entry);
+    if (1 || d_refs(entry) > 1)
+    {
+        /* If we do not hold the only reference to entry, unhash it. */
+        dentry_remove_from_cache(entry, parent);
+    }
+
+    d_unfreeze_refs(entry);
+
+    /* TODO: Rework locking around this, so we get to create a negative dentry from this */
     if (!d_is_negative(entry))
     {
         inode_dec_nlink(entry->d_inode);
@@ -733,7 +742,6 @@ void dentry_do_unlink(dentry *entry)
         }
     }
 
-    dentry_remove_from_cache(entry, parent);
     spin_unlock(&entry->d_lock);
 
     // We can do this because we're holding the parent dir's lock

commit d87b8c384249e9c874a4d67922dedac0cebc9c09

fs/dcache: Fix parent list removal on d_destroy
Since the linked commit, we unlink ourselves from d_parent_dir_node iff
we were hashed. This was done by an "if (dentry->d_flags &
DENTRY_FLAG_HASHED)". However, since before it we do the same check, and
unhash ourselves - including the clearing of this flag - this check was
never true.

This (probably) fixes UAFs in dcache code.

Fixes: 99064721f817 ("fs/dcache: Do not null out d_parent on unlink")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/fs/dentry.cpp
@@ -382,18 +382,17 @@ locked:
     return false;
 }
 
-static unsigned long d_stroyed = 0;
-
 static struct dentry *d_destroy(struct dentry *dentry)
 {
     /* Refs frozen, d_lock held */
     struct dentry *parent;
+    bool hashed;
+
     DCHECK(READ_ONCE(dentry->d_ref) & D_REF_LOCKED);
     DCHECK(spin_lock_held(&dentry->d_lock));
 
-    d_stroyed++;
-
-    if (dentry->d_flags & DENTRY_FLAG_HASHED)
+    hashed = dentry->d_flags & DENTRY_FLAG_HASHED;
+    if (hashed)
         dentry_remove_from_cache(dentry, dentry->d_parent);
 
     if ((dentry->d_flags & (DENTRY_FLAG_LRU | DENTRY_FLAG_SHRINK)) == DENTRY_FLAG_LRU)
@@ -418,7 +417,7 @@ static struct dentry *d_destroy(struct dentry *dentry)
         spin_lock(&parent->d_lock);
         /* Freeze refs. We'll see if we want to whack the parent dentry and return it if so */
         d_freeze_refs(parent);
-        if (dentry->d_flags & DENTRY_FLAG_HASHED)
+        if (hashed)
             list_remove(&dentry->d_parent_dir_node);
     }
 