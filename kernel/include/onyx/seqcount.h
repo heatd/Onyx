heatd/Onyx

commit 5f171725d9f8db1963abdc948bdc30b93222ec6b

seqcount: Add seqcount_t
Add seqcount_t, that implements linux(ish) sequence counters, with
proper memory barriers.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/include/onyx/seqcount.h
@@ -1,0 +1,48 @@
+/*
+ * Copyright (c) 2024 Pedro Falcato
+ * This file is part of Onyx, and is released under the terms of the GPLv2 License
+ * check LICENSE at the root directory for more information
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+#ifndef _ONYX_SEQCOUNT_H
+#define _ONYX_SEQCOUNT_H
+
+#include <onyx/atomic.h>
+#include <onyx/compiler.h>
+#include <onyx/cpu.h>
+#include <onyx/seqcount_types.h>
+
+static inline void seqcount_init(seqcount_t *seq)
+{
+    *seq = 0;
+}
+
+static inline unsigned int read_seqcount_begin(const seqcount_t *seq)
+{
+    unsigned int s;
+    while (unlikely((s = READ_ONCE(*seq)) & 1))
+        cpu_relax();
+    smp_rmb();
+    return s;
+}
+
+static inline int read_seqcount_retry(const seqcount_t *seq, unsigned int old)
+{
+    smp_rmb();
+    return unlikely(READ_ONCE(*seq) != old);
+}
+
+static inline void write_seqcount_begin(seqcount_t *seq)
+{
+    WRITE_ONCE(seq, seq + 1);
+    smp_wmb();
+}
+
+static inline void write_seqcount_end(seqcount_t *seq)
+{
+    __atomic_store_n(&seq, (seq + 1), __ATOMIC_RELEASE);
+}
+
+#endif

kernel/include/onyx/seqcount_types.h
@@ -1,0 +1,14 @@
+/*
+ * Copyright (c) 2024 Pedro Falcato
+ * This file is part of Onyx, and is released under the terms of the GPLv2 License
+ * check LICENSE at the root directory for more information
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ */
+
+#ifndef _ONYX_SEQCOUNT_TYPES_H
+#define _ONYX_SEQCOUNT_TYPES_H
+
+typedef unsigned int seqcount_t;
+
+#endif

commit 7e9810972c2087cedcdcaab8289e4010f1f77727

seqcount: Fix write seqcount's implementation
We were actually never writing anything, so seqcounts have been broken
all along.

Fixes: 5f171725d9f8 ("seqcount: Add seqcount_t")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/include/onyx/seqcount.h
@@ -36,13 +36,13 @@ static inline int read_seqcount_retry(const seqcount_t *seq, unsigned int old)
 
 static inline void write_seqcount_begin(seqcount_t *seq)
 {
-    WRITE_ONCE(seq, seq + 1);
+    *seq = *seq + 1;
     smp_wmb();
 }
 
 static inline void write_seqcount_end(seqcount_t *seq)
 {
-    __atomic_store_n(&seq, (seq + 1), __ATOMIC_RELEASE);
+    __atomic_store_n(seq, *seq + 1, __ATOMIC_RELEASE);
 }
 
 #endif