heatd/Onyx

commit 99064721f81710d0c0d40f9a6820052cc00178dc

fs/dcache: Do not null out d_parent on unlink
UNIX wants unattached (unlinked) directories to still be able to do
open("..") and get its parent inode. As such, don't NULL it out, and
simply unhash it.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/fs/dentry.cpp
@@ -418,7 +418,8 @@ static struct dentry *d_destroy(struct dentry *dentry)
         spin_lock(&parent->d_lock);
         /* Freeze refs. We'll see if we want to whack the parent dentry and return it if so */
         d_freeze_refs(parent);
-        list_remove(&dentry->d_parent_dir_node);
+        if (dentry->d_flags & DENTRY_FLAG_HASHED)
+            list_remove(&dentry->d_parent_dir_node);
     }
 
     if (dentry->d_name_length >= INLINE_NAME_MAX)
@@ -717,11 +718,19 @@ void dentry_do_unlink(dentry *entry)
 
     auto parent = entry->d_parent;
     DCHECK(spin_lock_held(&parent->d_lock));
-    dput_locked(parent);
     if ((entry->d_flags & (DENTRY_FLAG_LRU | DENTRY_FLAG_SHRINK)) == DENTRY_FLAG_LRU)
         d_remove_lru(entry);
-    entry->d_parent = nullptr;
 
+    d_freeze_refs(entry);
+    if (1 || d_refs(entry) > 1)
+    {
+        /* If we do not hold the only reference to entry, unhash it. */
+        dentry_remove_from_cache(entry, parent);
+    }
+
+    d_unfreeze_refs(entry);
+
+    /* TODO: Rework locking around this, so we get to create a negative dentry from this */
     if (!d_is_negative(entry))
     {
         inode_dec_nlink(entry->d_inode);
@@ -733,7 +742,6 @@ void dentry_do_unlink(dentry *entry)
         }
     }
 
-    dentry_remove_from_cache(entry, parent);
     spin_unlock(&entry->d_lock);
 
     // We can do this because we're holding the parent dir's lock

commit 49e38303cf76c5ffdf528ecf0d64c015765bf07e

fs/dcache: Fix dcache parent_dir removal
So, it turns out current semantics are broken.
Fix it by carefully (but, possibly half-assedly) checking if indeed the
list_head is inserted somewhere, in d_destroy. If so, remove it. This
(correctly) decouples the presence in the parent's child list from the
dentry being hashed or not.

While we're at it, we fix dentry lookup failing and make sure we never
retain a dentry that had a failed lookup.

Fixes: 99064721f817 ("fs/dcache: Do not null out d_parent on unlink")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/fs/dentry.cpp
@@ -335,6 +335,9 @@ static bool d_should_retain(struct dentry *dentry, bool locked)
     if (!(flags & DENTRY_FLAG_HASHED))
         return false;
 
+    if (flags & DENTRY_FLAG_FAILED)
+        return false;
+
     if (!(flags & DENTRY_FLAG_LRU))
     {
         /* If not in an LRU, try to add it (if locked) */
@@ -432,7 +435,8 @@ static struct dentry *d_destroy(struct dentry *dentry)
         spin_lock(&parent->d_lock);
         /* Freeze refs. We'll see if we want to whack the parent dentry and return it if so */
         d_freeze_refs(parent);
-        if (hashed)
+        if (!list_is_empty(&dentry->d_parent_dir_node) &&
+            dentry->d_parent_dir_node.next != LIST_REMOVE_POISON)
             list_remove(&dentry->d_parent_dir_node);
     }
 
@@ -472,8 +476,6 @@ void dput(struct dentry *d)
  */
 void dentry_fail_lookup(struct dentry *d)
 {
-    dentry_remove_from_cache(d, d->d_parent);
-
     {
         d->d_flags |= DENTRY_FLAG_FAILED;
         d->d_flags &= ~DENTRY_FLAG_PENDING;
@@ -532,6 +534,7 @@ dentry *dentry_create(const char *name, inode *inode, dentry *parent, u16 flags)
     new_dentry->d_name_length = name_length;
     new_dentry->d_name_hash = fnv_hash(new_dentry->d_name, new_dentry->d_name_length);
     new_dentry->d_inode = inode;
+    INIT_LIST_HEAD(&new_dentry->d_parent_dir_node);
 
     /* We need this if() because we might call dentry_create before retrieving an inode */
     if (inode)