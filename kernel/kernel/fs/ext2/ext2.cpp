heatd/Onyx

commit c8a0a4aa49444d8130506a2e7cc9ca61312ec0d9

ext2: Fix race in ext2_writepage_endio
Fix a possible race in ext2_writepage_endio where writeback could
accidentally never end, losing data in the process. Use the
newly-added pagestate_lock to serialize writeback ending.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/fs/ext2/ext2.cpp
@@ -104,9 +104,14 @@ static void ext2_writepage_endio(struct bio_req *req)
 {
     struct page *page = req->vec[0].page;
     struct block_buf *buf = (struct block_buf *) req->b_private;
+    struct block_buf *head = (struct block_buf *) page->priv;
+    DCHECK(head != nullptr);
+
+    spin_lock(&head->pagestate_lock);
     bb_clear_flag(buf, BLOCKBUF_FLAG_WRITEBACK);
     if (!page_has_writeback_bufs(page))
         page_end_writeback(page, page->owner->ino);
+    spin_unlock(&head->pagestate_lock);
 }
 
 ssize_t ext2_writepage(page *page, size_t off, inode *ino) REQUIRES(page) RELEASE(page)

commit c2e9bcbf30f6904198f07d8940f0fd88e527c449

ext2: Fix use-after-free in ext2_writepage_endio
We cannot hold pagestate_lock across page_end_writeback(), due to
page_end_writeback() allowing pages to go away. So the situation of:

spin_lock(&buf->pagestate_lock);
page_end_writeback(page);
	page_clear_writeback(page);
	/* other thread (e.g pagedaemon) frees buf */
spin_unlock(&buf->pagestate_lock); /* UAF */

It's possible to avoid this by simply not taking the lock across
page_end_writeback(). Correctness is still maintained.

Fixes: c8a0a4aa4944 ("ext2: Fix race in ext2_writepage_endio")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/fs/ext2/ext2.cpp
@@ -111,13 +111,15 @@ static void ext2_writepage_endio(struct bio_req *req)
     struct page *page = req->vec[0].page;
     struct block_buf *buf = (struct block_buf *) req->b_private;
     struct block_buf *head = (struct block_buf *) page->priv;
+    bool has_writeback;
     DCHECK(head != nullptr);
 
     spin_lock(&head->pagestate_lock);
     bb_clear_flag(buf, BLOCKBUF_FLAG_WRITEBACK);
-    if (!page_has_writeback_bufs(page))
-        page_end_writeback(page);
+    has_writeback = page_has_writeback_bufs(page);
     spin_unlock(&head->pagestate_lock);
+    if (!has_writeback)
+        page_end_writeback(page);
 }
 
 static ssize_t ext2_writepage(struct vm_object *obj, page *page, size_t off) REQUIRES(page)