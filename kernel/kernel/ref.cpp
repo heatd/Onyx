400e8e13b5182421debad9fb354dff553126d41a
Pedro Falcato <pedro.falcato@gmail.com>
2025-12-03 16:52:02 +0000

    ref: Add more functions
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/kernel/ref.cpp
+++ kernel/kernel/ref.cpp
@@ -28,3 +28,79 @@ void ref_release(struct ref *ref)
             ref->release(ref);
     }
 }
+
+static bool refcount_dec_not_one(refcount_t *r)
+{
+    unsigned int new_val, val = READ_ONCE(r->refs);
+
+    do
+    {
+        if (unlikely(val == 1))
+            return false;
+        new_val = val - 1;
+    } while (!__atomic_compare_exchange_n(&r->refs, &val, new_val, true, __ATOMIC_RELEASE,
+                                          __ATOMIC_RELAXED));
+    return true;
+}
+
+/**
+ * refcount_dec_and_mutex_lock - return holding mutex if able to decrement
+ *                               refcount to 0
+ * @r: the refcount
+ * @lock: the mutex to be locked
+ *
+ * Similar to atomic_dec_and_mutex_lock(), it will WARN on underflow and fail
+ * to decrement when saturated at REFCOUNT_SATURATED.
+ *
+ * Provides release memory ordering, such that prior loads and stores are done
+ * before, and provides a control dependency such that free() must come after.
+ * See the comment on top.
+ *
+ * Return: true and hold mutex if able to decrement refcount to 0, false
+ *         otherwise
+ */
+bool refcount_dec_and_mutex_lock(refcount_t *r, struct mutex *lock)
+{
+    if (refcount_dec_not_one(r))
+        return false;
+
+    mutex_lock(lock);
+    if (!refcount_dec_and_test(r))
+    {
+        mutex_unlock(lock);
+        return false;
+    }
+
+    return true;
+}
+
+/**
+ * refcount_dec_and_lock - return holding spinlock if able to decrement
+ *                         refcount to 0
+ * @r: the refcount
+ * @lock: the spinlock to be locked
+ *
+ * Similar to atomic_dec_and_lock(), it will WARN on underflow and fail to
+ * decrement when saturated at REFCOUNT_SATURATED.
+ *
+ * Provides release memory ordering, such that prior loads and stores are done
+ * before, and provides a control dependency such that free() must come after.
+ * See the comment on top.
+ *
+ * Return: true and hold spinlock if able to decrement refcount to 0, false
+ *         otherwise
+ */
+bool refcount_dec_and_lock(refcount_t *r, struct spinlock *lock)
+{
+    if (refcount_dec_not_one(r))
+        return false;
+
+    spin_lock(lock);
+    if (!refcount_dec_and_test(r))
+    {
+        spin_unlock(lock);
+        return false;
+    }
+
+    return true;
+}
