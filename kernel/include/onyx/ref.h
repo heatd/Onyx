c459e6d5dcbd33d6f23d3179f57d46f19e4ebd07
Pedro Falcato <pedro.falcato@gmail.com>
2025-12-03 16:52:02 +0000

    ref: Add more functions
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/include/onyx/ref.h
+++ kernel/include/onyx/ref.h
@@ -13,6 +13,8 @@
 
 #include <onyx/atomic.h>
 #include <onyx/compiler.h>
+#include <onyx/mutex.h>
+#include <onyx/spinlock.h>
 
 struct ref
 {
@@ -31,7 +33,12 @@ typedef struct __refcount_t
     unsigned int refs;
 } refcount_t;
 
-#define REFCOUNT_INIT(refcount) ((refcount_t){.refs = (refcount)})
+#define REFCOUNT_INIT(refcount) ((refcount_t) {.refs = (refcount)})
+
+static inline void refcount_set(refcount_t *r, unsigned int refs)
+{
+    WRITE_ONCE(r->refs, refs);
+}
 
 static inline void refcount_inc(refcount_t *r)
 {
@@ -63,6 +70,42 @@ static inline unsigned int refcount_read(const refcount_t *r)
 {
     return __atomic_load_n(&r->refs, __ATOMIC_RELAXED);
 }
+
+/**
+ * refcount_dec_and_mutex_lock - return holding mutex if able to decrement
+ *                               refcount to 0
+ * @r: the refcount
+ * @lock: the mutex to be locked
+ *
+ * Similar to atomic_dec_and_mutex_lock(), it will WARN on underflow and fail
+ * to decrement when saturated at REFCOUNT_SATURATED.
+ *
+ * Provides release memory ordering, such that prior loads and stores are done
+ * before, and provides a control dependency such that free() must come after.
+ * See the comment on top.
+ *
+ * Return: true and hold mutex if able to decrement refcount to 0, false
+ *         otherwise
+ */
+bool refcount_dec_and_mutex_lock(refcount_t *r, struct mutex *lock);
+
+/**
+ * refcount_dec_and_lock - return holding spinlock if able to decrement
+ *                         refcount to 0
+ * @r: the refcount
+ * @lock: the spinlock to be locked
+ *
+ * Similar to atomic_dec_and_lock(), it will WARN on underflow and fail to
+ * decrement when saturated at REFCOUNT_SATURATED.
+ *
+ * Provides release memory ordering, such that prior loads and stores are done
+ * before, and provides a control dependency such that free() must come after.
+ * See the comment on top.
+ *
+ * Return: true and hold spinlock if able to decrement refcount to 0, false
+ *         otherwise
+ */
+bool refcount_dec_and_lock(refcount_t *r, struct spinlock *lock);
 
 __END_CDECLS
 
