8d3bbd92eab2d4d96ac23e07579f9e39846aaed0
Pedro Falcato <pedro.falcato@gmail.com>
2026-02-07 23:19:09 +0000

    dev: Make dev_printk and bus_printk signatures linux-compatible
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/include/onyx/dev_printk.h
+++ kernel/include/onyx/dev_printk.h
@@ -21,28 +21,28 @@ struct device;
 #endif
 
 __BEGIN_CDECLS
-__attribute__((format(printf, 3, 4))) int dev_printk(struct device *dev, const char *log_lvl,
+__attribute__((format(printf, 3, 4))) int dev_printk(const char *log_lvl, const struct device *dev,
                                                      const char *fmt, ...);
-__attribute__((format(printf, 3, 4))) int bus_printk(struct device *dev, const char *log_lvl,
+__attribute__((format(printf, 3, 4))) int bus_printk(const char *log_lvl, const struct device *dev,
                                                      const char *fmt, ...);
 __END_CDECLS
 
-#define dev_emerg(dev, fmt, ...)  dev_printk(dev, KERN_EMERG, dev_fmt(fmt), ##__VA_ARGS__)
-#define dev_alert(dev, fmt, ...)  dev_printk(dev, KERN_ALERT, dev_fmt(fmt), ##__VA_ARGS__)
-#define dev_crit(dev, fmt, ...)   dev_printk(dev, KERN_CRIT, dev_fmt(fmt), ##__VA_ARGS__)
-#define dev_err(dev, fmt, ...)    dev_printk(dev, KERN_ERR, dev_fmt(fmt), ##__VA_ARGS__)
-#define dev_warn(dev, fmt, ...)   dev_printk(dev, KERN_WARN, dev_fmt(fmt), ##__VA_ARGS__)
-#define dev_notice(dev, fmt, ...) dev_printk(dev, KERN_NOTICE, dev_fmt(fmt), ##__VA_ARGS__)
-#define dev_info(dev, fmt, ...)   dev_printk(dev, KERN_INFO, dev_fmt(fmt), ##__VA_ARGS__)
-#define dev_debug(dev, fmt, ...)  dev_printk(dev, KERN_DEBUG, dev_fmt(fmt), ##__VA_ARGS__)
+#define dev_emerg(dev, fmt, ...)  dev_printk(KERN_EMERG, dev, dev_fmt(fmt), ##__VA_ARGS__)
+#define dev_alert(dev, fmt, ...)  dev_printk(KERN_ALERT, dev, dev_fmt(fmt), ##__VA_ARGS__)
+#define dev_crit(dev, fmt, ...)   dev_printk(KERN_CRIT, dev, dev_fmt(fmt), ##__VA_ARGS__)
+#define dev_err(dev, fmt, ...)    dev_printk(KERN_ERR, dev, dev_fmt(fmt), ##__VA_ARGS__)
+#define dev_warn(dev, fmt, ...)   dev_printk(KERN_WARN, dev, dev_fmt(fmt), ##__VA_ARGS__)
+#define dev_notice(dev, fmt, ...) dev_printk(KERN_NOTICE, dev, dev_fmt(fmt), ##__VA_ARGS__)
+#define dev_info(dev, fmt, ...)   dev_printk(KERN_INFO, dev, dev_fmt(fmt), ##__VA_ARGS__)
+#define dev_debug(dev, fmt, ...)  dev_printk(KERN_DEBUG, dev, dev_fmt(fmt), ##__VA_ARGS__)
 
-#define bus_emerg(dev, fmt, ...)  bus_printk(dev, KERN_EMERG, bus_fmt(fmt), ##__VA_ARGS__)
-#define bus_alert(dev, fmt, ...)  bus_printk(dev, KERN_ALERT, bus_fmt(fmt), ##__VA_ARGS__)
-#define bus_crit(dev, fmt, ...)   bus_printk(dev, KERN_CRIT, bus_fmt(fmt), ##__VA_ARGS__)
-#define bus_err(dev, fmt, ...)    bus_printk(dev, KERN_ERR, bus_fmt(fmt), ##__VA_ARGS__)
-#define bus_warn(dev, fmt, ...)   bus_printk(dev, KERN_WARN, bus_fmt(fmt), ##__VA_ARGS__)
-#define bus_notice(dev, fmt, ...) bus_printk(dev, KERN_NOTICE, bus_fmt(fmt), ##__VA_ARGS__)
-#define bus_info(dev, fmt, ...)   bus_printk(dev, KERN_INFO, bus_fmt(fmt), ##__VA_ARGS__)
-#define bus_debug(dev, fmt, ...)  bus_printk(dev, KERN_DEBUG, bus_fmt(fmt), ##__VA_ARGS__)
+#define bus_emerg(dev, fmt, ...)  bus_printk(KERN_EMERG, dev, bus_fmt(fmt), ##__VA_ARGS__)
+#define bus_alert(dev, fmt, ...)  bus_printk(KERN_ALERT, dev, bus_fmt(fmt), ##__VA_ARGS__)
+#define bus_crit(dev, fmt, ...)   bus_printk(KERN_CRIT, dev, bus_fmt(fmt), ##__VA_ARGS__)
+#define bus_err(dev, fmt, ...)    bus_printk(KERN_ERR, dev, bus_fmt(fmt), ##__VA_ARGS__)
+#define bus_warn(dev, fmt, ...)   bus_printk(KERN_WARN, dev, bus_fmt(fmt), ##__VA_ARGS__)
+#define bus_notice(dev, fmt, ...) bus_printk(KERN_NOTICE, dev, bus_fmt(fmt), ##__VA_ARGS__)
+#define bus_info(dev, fmt, ...)   bus_printk(KERN_INFO, dev, bus_fmt(fmt), ##__VA_ARGS__)
+#define bus_debug(dev, fmt, ...)  bus_printk(KERN_DEBUG, dev, bus_fmt(fmt), ##__VA_ARGS__)
 
 #endif
