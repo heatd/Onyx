heatd/Onyx

commit 9f1ffc58b064e7e9b0ae49f1a8d547c5d667e918

fs/ext2: Use i_version to guard against bad directory walks
There has existed a longstanding problem with ext2 where readdir()
wasn't unlink (or rename or ...) safe. Guard against these issues by
incrementing i_version when someone changes the directory, and
re-validate the directory walk in ext2_getdirent.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/fs/ext2/ext2.cpp
@@ -912,42 +912,63 @@ static const u8 ft_to_dt_table[] = {
     DT_UNKNOWN, DT_REG, DT_DIR, DT_CHR, DT_BLK, DT_FIFO, DT_SOCK, DT_LNK,
 };
 
+static unsigned int ext2_revalidate_dir(u8 *buf, unsigned long off, unsigned int block_size)
+{
+    ext2_dir_entry_t *base = (ext2_dir_entry_t *) (buf + (off & (block_size - 1)));
+    ext2_dir_entry_t *top = (ext2_dir_entry_t *) (buf + off);
+    /* Revalidate and reposition ourselves in the directory stream by walking through the particular
+     * block's directory entries. */
+    while (base < top)
+        base = (ext2_dir_entry_t *) (((u8 *) base) + base->rec_len);
+    return ((unsigned long) base) & (PAGE_SIZE - 1);
+}
+
 off_t ext2_getdirent(struct dirent *buf, off_t off, struct file *f)
 {
+    struct inode *ino = f->f_ino;
+    ext2_dir_entry_t *entry;
+    struct folio *folio;
+    bool needs_reval;
     off_t new_off;
-    ext2_dir_entry_t entry;
     ssize_t read;
+    u8 *p;
+
+    needs_reval = (unsigned long) f->private_data != inode_query_iversion(ino);
 
 retry:
-    unsigned long old = thread_change_addr_limit(VM_KERNEL_ADDR_LIMIT);
-
-    /* Read a dir entry from the offset */
-    read = file_read_cache(&entry, sizeof(ext2_dir_entry_t), f->f_ino, off);
-    if (read < 0)
-        return read;
-
-    thread_change_addr_limit(old);
-
-    /* If we reached the end of the directory buffer, return 0 */
-    if (read == 0)
+    if ((size_t) off >= ino->i_size)
         return 0;
 
-    /* Should ignore this entry, increment off and retry */
-    if (!entry.inode)
+    read = filemap_find_folio(ino, off >> PAGE_SHIFT, FIND_PAGE_ACTIVATE, &folio, &f->f_ra_state);
+    if (read < 0)
+        return read;
+    p = (u8 *) FOLIO_TO_VIRT(folio);
+    if (needs_reval)
     {
-        off += entry.rec_len;
+        off = folio_offset(folio) + ext2_revalidate_dir(p, off, ino->i_sb->s_block_size);
+        f->private_data = (void *) inode_query_iversion(ino);
+        needs_reval = false;
+    }
+
+    entry = (ext2_dir_entry_t *) (p + (off & (folio_size(folio) - 1)));
+
+    /* Should ignore this entry, increment off and retry */
+    if (!entry->inode)
+    {
+        off += entry->rec_len;
+        folio_put(folio);
         goto retry;
     }
 
-    memcpy(buf->d_name, entry.name, entry.name_len);
-    buf->d_name[entry.name_len] = '\0';
-    buf->d_ino = entry.inode;
+    memcpy(buf->d_name, entry->name, entry->name_len);
+    buf->d_name[entry->name_len] = '\0';
+    buf->d_ino = entry->inode;
     buf->d_off = off;
-    buf->d_reclen = sizeof(struct dirent) - (256 - (entry.name_len + 1));
-    buf->d_type = entry.file_type >= EXT2_FT_MAX ? DT_UNKNOWN : ft_to_dt_table[entry.file_type];
+    buf->d_reclen = sizeof(struct dirent) - (256 - (entry->name_len + 1));
+    buf->d_type = entry->file_type >= EXT2_FT_MAX ? DT_UNKNOWN : ft_to_dt_table[entry->file_type];
 
-    new_off = off + entry.rec_len;
-
+    new_off = off + entry->rec_len;
+    folio_put(folio);
     return new_off;
 }
 

kernel/kernel/fs/ext2/ext2_ll.cpp
@@ -263,7 +263,7 @@ int ext2_add_direntry(const char *name, uint32_t inum, struct ext2_inode *ino, i
                     e->name_len = entry.name_len;
                     strlcpy(e->name, entry.name, sizeof(entry.name));
                     e->file_type = entry.file_type;
-
+                    inode_inc_iversion(dir);
                     if (st = file_write_cache_unlocked(buffer, fs->block_size, dir, off); st < 0)
                     {
                         free(buffer);
@@ -280,7 +280,7 @@ int ext2_add_direntry(const char *name, uint32_t inum, struct ext2_inode *ino, i
                     entry.rec_len = e->rec_len - actual_size;
                     e->rec_len = actual_size;
                     memcpy(d, &entry, dirent_size);
-
+                    inode_inc_iversion(dir);
                     if (st = file_write_cache_unlocked(buffer, fs->block_size, dir, off); st < 0)
                     {
                         free(buffer);
@@ -300,7 +300,7 @@ int ext2_add_direntry(const char *name, uint32_t inum, struct ext2_inode *ino, i
         {
             entry.rec_len = fs->block_size;
             memcpy(buf, &entry, dirent_size);
-
+            inode_inc_iversion(dir);
             if (int st = file_write_cache_unlocked(buf, fs->block_size, dir, off); st < 0)
             {
                 return st;

commit c7d44347712d1c39ccdf82c1fb8486fcdf09f630

fs/ext2: Fix out-of-bounds when listing directories larger than a page
The offset passed to ext2_revalidate_dir needs to be relative to the
folio, not to the file.

Fixes: 9f1ffc58b064 ("fs/ext2: Use i_version to guard against bad
directory walks")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/fs/ext2/ext2.cpp
@@ -963,7 +963,8 @@ retry:
     p = (u8 *) FOLIO_TO_VIRT(folio);
     if (needs_reval)
     {
-        off = folio_offset(folio) + ext2_revalidate_dir(p, off, ino->i_sb->s_block_size);
+        off = folio_offset(folio) +
+              ext2_revalidate_dir(p, off & (folio_size(folio) - 1), ino->i_sb->s_block_size);
         f->private_data = (void *) inode_query_iversion(ino);
         needs_reval = false;
     }