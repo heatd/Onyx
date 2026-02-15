heatd/Onyx

commit 611b3a2710280853c653357b66b8d61035aaeaa7

tmpfs: Default the root directory to 1777
tmpfs users expect 1777 as the default mode, so default to that. This
should guarantee sane /tmp semantics.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/fs/tmpfs.cpp
@@ -345,7 +345,7 @@ inode *tmpfs_mount(blockdev *bdev)
 
     new_sb->s_devnr = blockdev->dev();
 
-    auto node = new_sb->create_inode(S_IFDIR | S_IRWXU | S_IRGRP | S_IXGRP | S_IROTH | S_IXOTH);
+    auto node = new_sb->create_inode(S_IFDIR | 1777);
     if (!node)
     {
         dev_unregister_dev(blockdev, true);

commit fad2ccdcaec5e43cba6048cb5d6814d53ac176da

tmpfs: Fix the root inode's permissions
The change of the mode from 0777 to 1777 makes the number stop being
octal. Make it 01777 instead.

Fixes: 611b3a271028 ("tmpfs: Default the root directory to 1777")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/fs/tmpfs.cpp
@@ -421,7 +421,7 @@ struct superblock *tmpfs_mount(struct vfs_mount_info *info)
 
     new_sb->s_devnr = blockdev->dev();
 
-    auto node = new_sb->create_inode(S_IFDIR | 1777);
+    auto node = new_sb->create_inode(S_IFDIR | 01777);
     if (!node)
     {
         dev_unregister_dev(blockdev, true);