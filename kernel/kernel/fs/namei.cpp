heatd/Onyx

commit d3897d2a198c8e4db5bfee6855ac3488adaf0e58

namei: Rework dot-dot following on mounts
We had a nasty hack with regards to .. following and mount roots. Fix it
by doing proper mount parents. This also fixes '..' on mounts, which
ended up broken and causing mount reference leaks.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/fs/namei.cpp
@@ -12,6 +12,7 @@
 #include <onyx/mount.h>
 #include <onyx/namei.h>
 #include <onyx/process.h>
+#include <onyx/seqlock.h>
 #include <onyx/user.h>
 
 #include <uapi/fcntl.h>
@@ -200,6 +201,122 @@ static int dentry_follow_symlink(nameidata &data, dentry *symlink, unsigned int 
 #define NAMEI_NO_FOLLOW_SYM  (1U << 1)
 #define NAMEI_ALLOW_NEGATIVE (1U << 2)
 
+static void follow_mount_up(struct mount *mnt, struct path *out)
+{
+    struct dentry *dentry = mnt->mnt_root, *mountpoint;
+
+    while (mnt->mnt_parent)
+    {
+        mountpoint = mnt->mnt_point;
+        mnt = mnt->mnt_parent;
+        if (mnt->mnt_root != dentry)
+        {
+            out->mount = mnt;
+            out->dentry = mountpoint;
+            return;
+        }
+    }
+
+    /* Should not be hittable, I think... */
+    CHECK(0);
+}
+
+static bool finish_mount_up(struct path *path, unsigned int seq)
+{
+    mnt_get(path->mount);
+    smp_mb();
+
+    if (path->mount->mnt_flags & MNT_DOOMED)
+    {
+        mnt_put(path->mount);
+        goto retry;
+    }
+
+    /* I don't think there's a way this can fail, if we grabbed the mount itself */
+    dget(path->dentry);
+
+    if (read_seqretry(&mount_lock, seq))
+        goto retry;
+
+    return true;
+retry:
+    return false;
+}
+
+static int mount_dotdot(struct mount *mnt, struct path *path)
+{
+    rcu_read_lock();
+
+    for (;;)
+    {
+        unsigned int seq = read_seqbegin(&mount_lock);
+        follow_mount_up(mnt, path);
+
+        /* Commit this follow_up by grabbing a reference to mount and mountpoint */
+        if (finish_mount_up(path, seq))
+            break;
+    }
+
+    rcu_read_unlock();
+    return 0;
+}
+
+static int do_dotdot(nameidata &data, struct path *out)
+{
+    struct path *curr = &data.cur;
+    if (curr->dentry == curr->mount->mnt_root)
+    {
+        /* We're the mount's root? Gotta take things in a different way. */
+        return mount_dotdot(curr->mount, out);
+    }
+
+    struct dentry *dentry = dentry_parent(curr->dentry);
+    if (!dentry)
+    {
+        /* /.. = right where we are */
+        return 0;
+    }
+
+    struct path p = {dentry, curr->mount};
+    mnt_get(curr->mount);
+    *out = p;
+    return 0;
+}
+
+static int __namei_walk_component(std::string_view v, nameidata &data, struct path *out,
+                                  unsigned int flags)
+{
+    if (!v.compare("."))
+    {
+        *out = data.cur;
+        path_get(out);
+        return 0;
+    }
+
+    if (!v.compare(".."))
+        return do_dotdot(data, out);
+
+    struct dentry *dent = dentry_open_from_cache(data.cur.dentry, v);
+
+    if (dent)
+    {
+        if (dent->d_flags & DENTRY_FLAG_PENDING)
+            dent = dentry_wait_for_pending(dent);
+    }
+
+    if (!dent)
+    {
+        dent = __dentry_try_to_open(v, data.cur.dentry, !(flags & DENTRY_LOOKUP_UNLOCKED));
+        if (!dent)
+            return -errno;
+    }
+
+    struct path p = {.dentry = dent, .mount = data.cur.mount};
+    mnt_get(data.cur.mount);
+    *out = p;
+    return 0;
+}
+
 static int namei_walk_component(std::string_view v, nameidata &data, unsigned int flags = 0)
 {
     const bool is_last_name =
@@ -207,9 +324,7 @@ static int namei_walk_component(std::string_view v, nameidata &data, unsigned in
     const bool dont_follow_last = data.lookup_flags & LOOKUP_NOFOLLOW;
     const bool unlocked_lookup = flags & NAMEI_UNLOCKED;
 
-    auto_dentry dwrapper;
-    dentry *new_found;
-
+    struct path path;
     file f;
     f.f_ino = data.cur.dentry->d_inode;
 
@@ -224,42 +339,33 @@ static int namei_walk_component(std::string_view v, nameidata &data, unsigned in
         /* Stop from escaping the chroot */
         return 0;
     }
-    else
-    {
-        dwrapper = dentry_lookup_internal(v, data.cur.dentry,
-                                          unlocked_lookup ? DENTRY_LOOKUP_UNLOCKED : 0);
-        if (!dwrapper)
-        {
-            DCHECK(errno != 0);
-            return -errno;
-        }
+
+    path_init(&path);
+    int err = __namei_walk_component(v, data, &path, unlocked_lookup ? DENTRY_LOOKUP_UNLOCKED : 0);
+    if (err < 0)
+        return err;
 
 #if 0
-        printk("Lookup %s found %p%s\n", v.data(), new_found,
-               d_is_negative(new_found) ? " (negative)" : "");
+    pr_warn("Lookup %s found %p%s\n", v.data(), path.dentry,
+            d_is_negative(path.dentry) ? " (negative)" : "");
 #endif
-    }
 
-    new_found = dwrapper.get_dentry();
-    struct mount *mnt = data.cur.mount;
+    struct mount *mnt = path.mount;
 
-    if (d_is_negative(new_found))
+    if (d_is_negative(path.dentry))
     {
         /* Check if the caller tolerates negative dentries as the lookup result. This only applies
          * for the last name. For !last_name, negative is always ENOENT */
         if (!is_last_name || !(flags & NAMEI_ALLOW_NEGATIVE))
-            return -ENOENT;
+        {
+            err = -ENOENT;
+            goto err_out;
+        }
     }
-    else if (dentry_is_symlink(new_found))
+    else if (dentry_is_symlink(path.dentry))
     {
         if (flags & NAMEI_NO_FOLLOW_SYM)
-        {
-            /* Save parent and location for the caller */
-            struct path p = path{dwrapper.release(), mnt};
-            mnt_get(mnt);
-            data.setcur(p);
-            return 0;
-        }
+            goto out;
         /* POSIX states that paths that end in a trailing slash are required to be the same as
          * /. For example: open("/usr/bin/") == open("/usr/bin/."). Therefore, we have to
          * special case that.
@@ -270,7 +376,10 @@ static int namei_walk_component(std::string_view v, nameidata &data, unsigned in
 
         // printk("Following symlink for path elem %s\n", v.data());
         if (is_last_name && (data.lookup_flags & LOOKUP_FAIL_IF_LINK))
-            return -ELOOP;
+        {
+            err = -ELOOP;
+            goto err_out;
+        }
         else if (is_last_name && !should_follow_symlink)
         {
             // printk("Cannot follow symlink. Trailing slash: %s\n", must_be_dir ? "yes" :
@@ -278,26 +387,31 @@ static int namei_walk_component(std::string_view v, nameidata &data, unsigned in
         }
         else [[likely]]
         {
-            return dentry_follow_symlink(data, new_found);
+            err = dentry_follow_symlink(data, path.dentry);
+            path_put(&path);
+            return err;
         }
     }
-    else if (dentry_is_mountpoint(new_found))
+    else if (dentry_is_mountpoint(path.dentry))
     {
-        struct mount *new_mount = mnt_traverse(new_found);
+        struct mount *new_mount = mnt_traverse(path.dentry);
         if (new_mount)
         {
-            dwrapper = new_mount->mnt_root;
-            dget(dwrapper.get_dentry());
+            struct dentry *d = new_mount->mnt_root;
+            dget(d);
             mnt = new_mount;
-            new_found = dwrapper.get_dentry();
+            data.setcur((struct path){d, mnt});
+            path_put(&path);
+            return 0;
         }
     }
 
-    if (mnt == data.cur.mount)
-        mnt_get(mnt);
-    data.setcur(path{dwrapper.release(), mnt});
-
+out:
+    data.setcur(path);
     return 0;
+err_out:
+    path_put(&path);
+    return err;
 }
 
 /**

kernel/include/onyx/dentry.h
@@ -295,6 +295,9 @@ bool dentry_does_not_have_parent(dentry *dir, dentry *to_not_have);
 void dentry_do_unlink(dentry *entry);
 void dentry_rename(dentry *dent, const char *name, dentry *parent, dentry *dst);
 void dentry_move(dentry *target, dentry *new_parent);
+dentry *__dentry_try_to_open(std::string_view name, dentry *dir, bool lock_ino);
+dentry *dentry_open_from_cache(dentry *dent, std::string_view name);
+dentry *dentry_wait_for_pending(dentry *dent);
 
 #endif
 

kernel/include/onyx/mount.h
@@ -22,6 +22,7 @@ struct mount
     struct dentry *mnt_root;
     struct superblock *mnt_sb;
     struct dentry *mnt_point;
+    struct mount *mnt_parent;
     unsigned int mnt_flags;
     /* TODO: percpu */
     unsigned long mnt_count;
@@ -29,6 +30,8 @@ struct mount
     struct rcu_head mnt_rcu;
     struct list_head mnt_mp_node;
     struct list_head mnt_node;
+    struct list_head mnt_submounts;
+    struct list_head mnt_submount_node;
 };
 
 static inline void mnt_get(struct mount *mnt)

kernel/kernel/fs/d_path.c
@@ -60,21 +60,47 @@ static void prepend_dentry(struct rbuf *rbuf, struct dentry *dentry)
     spin_unlock(&dentry->d_lock);
 }
 
+static bool follow_mount_up(struct mount *mnt, struct path *out)
+{
+    struct dentry *dentry = mnt->mnt_root, *mountpoint;
+
+    while (mnt->mnt_parent)
+    {
+        mountpoint = mnt->mnt_point;
+        mnt = mnt->mnt_parent;
+        if (mnt->mnt_root != dentry)
+        {
+            out->mount = mnt;
+            out->dentry = mountpoint;
+            return true;
+        }
+    }
+
+    return false;
+}
+
 static void walk_path(const struct path *path, const struct path *root, struct rbuf *rbuf)
 {
     /* TODO: While d_parent on .. Just Works, we don't need to keep track of the struct mnt. This
      * will need to be changed once that changes (mnt should keep track of mnt_parent).
      **/
     struct dentry *dentry = path->dentry;
+    struct mount *mnt = path->mount;
 
     prepend_char(rbuf, '\0');
     while (dentry != NULL && dentry != root->dentry)
     {
-        if (dentry->d_flags & DENTRY_FLAG_MOUNT_ROOT)
-            goto skip;
+        if (dentry == mnt->mnt_root)
+        {
+            struct path p;
+            if (!follow_mount_up(mnt, &p))
+                break;
+            dentry = p.dentry;
+            mnt = p.mount;
+        }
+
         prepend_dentry(rbuf, dentry);
         prepend_char(rbuf, '/');
-    skip:
         dentry = dentry->d_parent;
     }
 

kernel/kernel/fs/dentry.cpp
@@ -101,7 +101,7 @@ static dentry *d_lookup_internal(dentry *dent, std::string_view name)
     return nullptr;
 }
 
-static dentry *dentry_open_from_cache(dentry *dent, std::string_view name)
+dentry *dentry_open_from_cache(dentry *dent, std::string_view name)
 {
     unsigned int old;
     struct dentry *found;
@@ -561,7 +561,7 @@ static expected<dentry *, int> dentry_create_pending_lookup(const char *name, in
     return dentry_add_to_cache_careful(dent, parent);
 }
 
-static dentry *__dentry_try_to_open(std::string_view name, dentry *dir, bool lock_ino)
+dentry *__dentry_try_to_open(std::string_view name, dentry *dir, bool lock_ino)
 {
     DCHECK(dentry_is_dir(dir));
     if (auto d = dentry_open_from_cache(dir, name); d)

kernel/kernel/fs/dev.cpp
@@ -611,8 +611,13 @@ static off_t devfs_getdirent(struct dirent *buf, off_t off, struct file *file)
     {
         /* .. */
         auto parent = dentry_parent(dent);
-        put_dentry_to_dirent(buf, parent, "..");
-        dput(parent);
+        if (parent)
+        {
+            put_dentry_to_dirent(buf, parent, "..");
+            dput(parent);
+        }
+        else
+            put_dentry_to_dirent(buf, dent, "..");
     }
     else
     {

kernel/kernel/fs/mount.c
@@ -18,6 +18,7 @@
 #include <onyx/list.h>
 #include <onyx/mm/slab.h>
 #include <onyx/mount.h>
+#include <onyx/namei.h>
 #include <onyx/rculist.h>
 #include <onyx/rcupdate.h>
 #include <onyx/seqlock.h>
@@ -46,6 +47,8 @@ static void mnt_init(struct mount *mnt, unsigned long flags)
     mnt->mnt_flags = flags;
     mnt->mnt_point = mnt->mnt_root = NULL;
     mnt->mnt_sb = NULL;
+    mnt->mnt_parent = NULL;
+    INIT_LIST_HEAD(&mnt->mnt_submounts);
 }
 
 static unsigned int mnt_hashbucket(struct mount *mnt)
@@ -169,21 +172,16 @@ static int mnt_commit(struct mount *mnt, const char *target)
      * flags on a dentry, etc. */
     if (strcmp(target, "/"))
     {
-        struct file *filp = open_vfs(AT_FDCWD, target);
-        if (!filp)
-            return -errno;
-        if (!dentry_is_dir(filp->f_dentry))
-        {
-            fd_put(filp);
-            return -ENOTDIR;
-        }
+        struct path mountpoint;
+        int err;
 
-        mnt->mnt_point = filp->f_dentry;
-        dget(mnt->mnt_point);
-        /* Another hack... */
-        mnt->mnt_root->d_parent = mnt->mnt_point;
-        /* TODO: This isn't quite safe when we get proper mnt putting and umount */
-        fd_put(filp);
+        err = path_openat(AT_FDCWD, target, LOOKUP_MUST_BE_DIR, &mountpoint);
+        if (err < 0)
+            return err;
+
+        /* Path reference gets dilluted into these two members */
+        mnt->mnt_point = mountpoint.dentry;
+        mnt->mnt_parent = mountpoint.mount;
     }
     else
     {
@@ -200,6 +198,9 @@ static int mnt_commit(struct mount *mnt, const char *target)
 
     list_add_tail(&mnt->mnt_mp_node, &mp_hashtable[mnt_mp_hashbucket(mnt)]);
     list_add_tail(&mnt->mnt_node, &mount_hashtable[mnt_hashbucket(mnt)]);
+
+    if (mnt->mnt_parent)
+        list_add_tail(&mnt->mnt_submount_node, &mnt->mnt_parent->mnt_submounts);
 
     /* Ref up for the mount root */
     dget(mnt->mnt_root);
@@ -337,6 +338,12 @@ static bool attempt_disconnect(struct mount *mount)
         struct dentry *mp = mount->mnt_point;
         list_remove(&mount->mnt_mp_node);
         list_remove(&mount->mnt_node);
+        if (mount->mnt_parent)
+        {
+            list_remove(&mount->mnt_submount_node);
+            mnt_put(mount->mnt_parent);
+        }
+
         ok = true;
 
         /* Check if we have nothing mounted at mp anymore. If so, unset DENTRY_FLAG_MOUNTPOINT.
@@ -375,8 +382,6 @@ static int do_umount_path(struct path *path, int flags)
 
     WARN_ON(mount->mnt_root->d_ref != 1);
 
-    /* Undo our fake d_parent... */
-    mount->mnt_root->d_parent = NULL;
     /* Finally, put our root */
     dput(mount->mnt_root);
 
@@ -391,18 +396,21 @@ out_put_path:
 
 int sys_umount2(const char *utarget, int flags)
 {
+    int err = -EINVAL;
+    const char *target;
+
     if (!is_root_user())
         return -EPERM;
-    const char *target = strcpy_from_user(utarget);
+
+    target = strcpy_from_user(utarget);
     if (!target)
         return -errno;
     if (flags & ~UMOUNT_NOFOLLOW)
-        return -EINVAL;
+        goto out;
 
     struct path path;
-    int err =
-        path_openat(AT_FDCWD, target,
-                    LOOKUP_MUST_BE_DIR | (flags & UMOUNT_NOFOLLOW ? LOOKUP_NOFOLLOW : 0), &path);
+    err = path_openat(AT_FDCWD, target,
+                      LOOKUP_MUST_BE_DIR | (flags & UMOUNT_NOFOLLOW ? LOOKUP_NOFOLLOW : 0), &path);
     if (err < 0)
         goto out;
 

kernel/kernel/fs/tmpfs.cpp
@@ -192,10 +192,13 @@ off_t tmpfs_getdirent(struct dirent *buf, off_t off, struct file *file)
     {
         /* .. */
         auto parent = dentry_parent(dent);
-        if (!parent) // We're root, so use ourselves
-            parent = dent;
-        put_dentry_to_dirent(buf, parent, "..");
-        dput(parent);
+        if (parent)
+        {
+            put_dentry_to_dirent(buf, parent, "..");
+            dput(parent);
+        }
+        else
+            put_dentry_to_dirent(buf, dent, "..");
     }
     else
     {

commit f07fc175e2e335d7ee1f042af1c475db21dbc892

namei: Fix ".." lookup on mount roots
Following the mount up was just grabbing the mountpoint, and leaving us
in the same place after the mountpoint was resolved. Fix it by grabbing
the mountpoint's parent.

Fixes: d3897d2a198c ("namei: Rework dot-dot following on mounts")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/fs/namei.cpp
@@ -233,6 +233,14 @@ static void follow_mount_up(struct mount *mnt, struct path *out)
 {
     struct dentry *dentry = mnt->mnt_root, *mountpoint;
 
+    if (unlikely(!mnt->mnt_parent))
+    {
+        out->mount = mnt;
+        out->dentry = mnt->mnt_root;
+        dget(out->dentry);
+        return;
+    }
+
     while (mnt->mnt_parent)
     {
         mountpoint = mnt->mnt_point;
@@ -240,7 +248,7 @@ static void follow_mount_up(struct mount *mnt, struct path *out)
         if (mnt->mnt_root != dentry)
         {
             out->mount = mnt;
-            out->dentry = mountpoint;
+            out->dentry = dentry_parent(mountpoint);
             return;
         }
     }
@@ -255,19 +263,15 @@ static bool finish_mount_up(struct path *path, unsigned int seq)
     smp_mb();
 
     if (path->mount->mnt_flags & MNT_DOOMED)
-    {
-        mnt_put(path->mount);
         goto retry;
-    }
 
-    /* I don't think there's a way this can fail, if we grabbed the mount itself */
-    dget(path->dentry);
-
+    /* follow_mount_up grabbed a dentry reference already. */
     if (read_seqretry(&mount_lock, seq))
         goto retry;
 
     return true;
 retry:
+    path_put(path);
     return false;
 }
 