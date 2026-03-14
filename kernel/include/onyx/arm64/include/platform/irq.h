ac132e18aa1a347beb946308854b6ecd01a05752
Pedro Falcato <pedro.falcato@gmail.com>
2026-03-14 17:48:00 +0000

    arm64: Implement platform/irqflags.h
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/include/onyx/arm64/include/platform/irq.h
+++ kernel/include/onyx/arm64/include/platform/irq.h
@@ -12,13 +12,13 @@
 #include <onyx/intrinsics.h>
 #include <onyx/registers.h>
 
+#include <platform/irqflags.h>
+
 // TODO: Correct values
 #define NR_IRQ                   223
 #define PCI_MSI_BASE_ADDRESS     0xFEE00000
 #define PCI_MSI_APIC_ID_SHIFT    12
 #define PCI_MSI_REDIRECTION_HINT (1 << 3)
-
-#define CPU_FLAGS_NO_IRQ 0
 
 struct irq_context
 {
@@ -27,33 +27,5 @@ struct irq_context
 };
 
 void softirq_try_handle(void);
-
-static inline void irq_disable()
-{
-    __asm__ __volatile__("msr daifset, #3" ::: "memory");
-}
-
-static inline unsigned long irq_save_and_disable()
-{
-    unsigned long flags = mrs("daif");
-    irq_disable();
-    return flags;
-}
-
-static inline void irq_restore(unsigned long flags)
-{
-    msr("daif", flags);
-}
-
-static inline void irq_enable()
-{
-    __asm__ __volatile__("msr daifclr, #3" ::: "memory");
-}
-
-inline bool irq_is_disabled()
-{
-    unsigned long flags = mrs("daif");
-    return flags & (1 << 7);
-}
 
 #endif
