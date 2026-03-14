6b1a658fcaccfedf43b2a18a1728f4fbf39deb51
Pedro Falcato <pedro.falcato@gmail.com>
2026-03-14 23:54:59 +0000

    swaputils: Fix infinite loop on arm64 (unsigned char)
    It turns out arm64 has an unsigned char. And this makes for an
    interestingly tricky bug:
    
        char flag;
    
        while ((flag = getopt_long(argc, argv, "vh", long_options, &indexptr)) != -1)
    
    where flag is unsigned, and thus it will hold the value "255" and not -1
    (which converted to unsigned will be 429...). This makes for an infinite
    loop. Fix it by making flag into an int.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- usystem/core/utils/swap/mkswap.c
+++ usystem/core/utils/swap/mkswap.c
@@ -49,7 +49,7 @@ int main(int argc, char **argv)
 {
     int indexptr = 0;
     int page_size = sysconf(_SC_PAGE_SIZE);
-    char flag;
+    int flag;
 
     while ((flag = getopt_long(argc, argv, "vhp", long_options, &indexptr)) != -1)
     {
