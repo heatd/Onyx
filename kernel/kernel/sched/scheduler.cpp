heatd/Onyx

commit 303c1644be2a2a75127ab33d271fdc9fd380dafb

kernel-wide: Various spinlock lockdep fixes and other misc changes
This will NOT bisect cleanly :/

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/sched/scheduler.cpp
@@ -80,9 +80,9 @@ static void ___sched_append_to_queue(int priority, unsigned int cpu, struct thre
 
 int sched_rbtree_cmp(const void *t1, const void *t2);
 static rb_tree glbl_thread_list = {.cmp_func = sched_rbtree_cmp};
-static spinlock glbl_thread_list_lock;
+static DEFINE_SPINLOCK(glbl_thread_list_lock);
 
-PER_CPU_VAR(spinlock scheduler_lock) = STATIC_SPINLOCK_INIT;
+PER_CPU_VAR(spinlock scheduler_lock) = STATIC_SPINLOCK_INIT(scheduler_lock);
 PER_CPU_VAR(thread *thread_queues_head[NUM_PRIO]);
 PER_CPU_VAR(thread *thread_queues_tail[NUM_PRIO]);
 PER_CPU_VAR(thread *current_thread);
@@ -439,6 +439,7 @@ void sched_load_thread(struct thread *prev, thread *thread, unsigned int cpu)
 {
     struct mm_address_space *mm = prev->active_mm ?: prev->aspace;
 
+    spin_unlock_irqrestore(get_per_cpu_ptr_any(scheduler_lock, cpu), irq_save_and_disable());
     write_per_cpu(current_thread, thread);
     errno = thread->errno_val;
 
@@ -481,8 +482,6 @@ void sched_load_thread(struct thread *prev, thread *thread, unsigned int cpu)
     write_per_cpu(sched_quantum, SCHED_QUANTUM);
 
     cputime_restart_accounting(thread);
-
-    spin_unlock_irqrestore(get_per_cpu_ptr_any(scheduler_lock, cpu), irq_save_and_disable());
 }
 
 extern "C" void asan_unpoison_stack_shadow_ctxswitch(struct registers *regs);
@@ -1242,6 +1241,7 @@ dequeue_thread_generic(sem, semaphore);
 void sem_init(semaphore *sem, long counter)
 {
     sem->counter = 0;
+    spinlock_init(&sem->lock);
 }
 
 static void sem_do_slow_path(semaphore *sem, unsigned long flags)

kernel/arch/riscv64/scheduler.cpp
@@ -94,6 +94,7 @@ thread *sched_spawn_thread(registers_t *regs, unsigned int flags, void *tp)
     new_thread->flags = flags;
     new_thread->canary = THREAD_STRUCT_CANARY;
     new_thread->fpu_area = nullptr;
+    spinlock_init(&new_thread->lock);
 
     bool is_user = !(flags & THREAD_KERNEL);
     auto pages = adding_guard_page ? 6 : 4;

kernel/arch/x86_64/apic.cpp
@@ -517,7 +517,9 @@ void apic_update_clock_monotonic()
 void apic_set_oneshot(hrtime_t deadline);
 
 PER_CPU_VAR(unsigned long apic_ticks) = 0;
-PER_CPU_VAR(struct timer lapic_timer);
+PER_CPU_VAR(struct timer lapic_timer) = {
+    .event_list_lock = STATIC_SPINLOCK_INIT(lapic_timer.lapic_timer),
+};
 extern uint32_t sched_quantum;
 
 irqstatus_t apic_timer_irq(struct irq_context *ctx, void *cookie)

kernel/arch/x86_64/cpu.cpp
@@ -450,7 +450,7 @@ void cpu_wait_for_msg_ack(volatile struct cpu_message *msg)
     WRITE_ONCE(msg->ack, false);
 }
 
-PER_CPU_VAR(struct spinlock msg_queue_lock);
+PER_CPU_VAR(struct spinlock msg_queue_lock) = __SPIN_LOCK_UNLOCKED(msg_queue_lock);
 PER_CPU_VAR(struct list_head message_queue);
 
 void cpu_messages_init(unsigned int cpu)

kernel/arch/x86_64/irq.cpp
@@ -25,6 +25,7 @@
 #include <onyx/x86/apic.h>
 #include <onyx/x86/idt.h>
 #include <onyx/x86/isr.h>
+#include <onyx/bug.h>
 
 int platform_install_irq(unsigned int irqn, struct interrupt_handler *h)
 {

kernel/arch/x86_64/isr.cpp
@@ -580,7 +580,7 @@ void (*const int_handlers[])(struct registers *ctx) = {div0_exception,
                                                        security_exception,
                                                        exception_panic};
 
-PER_CPU_VAR(struct spinlock isr_lock);
+PER_CPU_VAR(struct spinlock isr_lock) = __SPIN_LOCK_UNLOCKED(isr_lock);
 uint32_t in_isr = 0;
 
 static void exit_isr_handler(void)

kernel/arch/x86_64/thread.cpp
@@ -218,6 +218,7 @@ thread *sched_spawn_thread(registers_t *regs, unsigned int flags, void *fs)
     new_thread->id = curr_id++;
     new_thread->flags = flags;
     new_thread->canary = THREAD_STRUCT_CANARY;
+    spinlock_init(&new_thread->lock);
 
     bool is_user = !(flags & THREAD_KERNEL);
     void *original_entry = (void *) regs->rip;

kernel/drivers/pci/legacy/config.cpp
@@ -13,7 +13,7 @@ namespace pci
 
 constexpr uint16_t CONFIG_ADDRESS = 0xcf8;
 constexpr uint16_t CONFIG_DATA = 0xcfc;
-struct spinlock pci_lock;
+DEFINE_SPINLOCK(pci_lock);
 
 // TODO: Clean all this up
 
@@ -160,20 +160,20 @@ public:
         uint64_t val = 0;
         switch (size)
         {
-        case sizeof(uint16_t):
-            val = __pci_config_read_word(addr.bus, addr.device, addr.function, off);
-            break;
-        case sizeof(uint32_t):
-            val = __pci_config_read_dword(addr.bus, addr.device, addr.function, off);
-            break;
-        case sizeof(uint64_t):
-            val = __pci_config_read_dword(addr.bus, addr.device, addr.function, off);
-            break;
-        case sizeof(uint8_t):
-            val = __pci_read_byte(addr.bus, addr.device, addr.function, off);
-            break;
-        default:
-            __builtin_unreachable();
+            case sizeof(uint16_t):
+                val = __pci_config_read_word(addr.bus, addr.device, addr.function, off);
+                break;
+            case sizeof(uint32_t):
+                val = __pci_config_read_dword(addr.bus, addr.device, addr.function, off);
+                break;
+            case sizeof(uint64_t):
+                val = __pci_config_read_dword(addr.bus, addr.device, addr.function, off);
+                break;
+            case sizeof(uint8_t):
+                val = __pci_read_byte(addr.bus, addr.device, addr.function, off);
+                break;
+            default:
+                __builtin_unreachable();
         }
 
         return val;
@@ -189,20 +189,20 @@ public:
 
         switch (size)
         {
-        case sizeof(uint8_t):
-            __pci_write_byte(addr.bus, addr.device, addr.function, off, (uint8_t) value);
-            break;
-        case sizeof(uint16_t):
-            __pci_write_word(addr.bus, addr.device, addr.function, off, (uint16_t) value);
-            break;
-        case sizeof(uint32_t):
-            __pci_write_dword(addr.bus, addr.device, addr.function, off, (uint32_t) value);
-            break;
-        case sizeof(uint64_t):
-            __pci_write_qword(addr.bus, addr.device, addr.function, off, value);
-            break;
-        default:
-            __builtin_unreachable();
+            case sizeof(uint8_t):
+                __pci_write_byte(addr.bus, addr.device, addr.function, off, (uint8_t) value);
+                break;
+            case sizeof(uint16_t):
+                __pci_write_word(addr.bus, addr.device, addr.function, off, (uint16_t) value);
+                break;
+            case sizeof(uint32_t):
+                __pci_write_dword(addr.bus, addr.device, addr.function, off, (uint32_t) value);
+                break;
+            case sizeof(uint64_t):
+                __pci_write_qword(addr.bus, addr.device, addr.function, off, value);
+                break;
+            default:
+                __builtin_unreachable();
         }
     }
 };

commit 0dd97aef87c7f8c1921dd75ba3c976d072a3c679

sched: Unlock sched_lock after setting current_thread
There is an expectation in scheduler wake-up paths that current_thread
is stable for any given CPU as long as you hold its sched_lock. Ever
since 303c1644be2a, this has not been true, and buggy.

Fix it.

Fixes: 303c1644be2a ("kernel-wide: Various spinlock lockdep fixes and other misc changes")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/sched/scheduler.cpp
@@ -436,8 +436,8 @@ void sched_load_thread(struct thread *prev, thread *thread, unsigned int cpu)
 {
     struct mm_address_space *mm = prev->active_mm ?: prev->aspace;
 
-    spin_unlock_irqrestore(get_per_cpu_ptr_any(scheduler_lock, cpu), irq_save_and_disable());
     write_per_cpu(current_thread, thread);
+    spin_unlock_irqrestore(get_per_cpu_ptr_any(scheduler_lock, cpu), irq_save_and_disable());
     errno = thread->errno_val;
 
     native::arch_load_thread(thread, cpu);