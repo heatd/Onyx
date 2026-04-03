heatd/Onyx

commit 566cec024539f1f2fbcd91f94356df6c21c842be

net: Pass iovec_iter around the stack
Instead of getting a normal struct msghdr, create a new kernel_msghdr
that uses a struct iovec_iter instead of struct iovec. This makes it possible
to do zero-copy splice and other sorts of fun tricks with sockets. It also
simplifies and removes quite a bit of handrolled iovec and copy code.

This patch is general cleanup and should not affect behavior.

Fixes: #94
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/net/socket.cpp
@@ -118,7 +118,7 @@ int socket::connect(struct sockaddr *addr, socklen_t addrlen, int flags)
     return -EIO;
 }
 
-ssize_t socket::sendmsg(const struct msghdr *msg, int flags)
+ssize_t socket::sendmsg(const struct kernel_msghdr *msg, int flags)
 {
     (void) msg;
     (void) flags;
@@ -153,30 +153,9 @@ static inline int fd_flags_to_msg_flags(struct file *f)
     return flags;
 }
 
-ssize_t socket::recvmsg(struct msghdr *msg, int flags)
+ssize_t socket::recvmsg(struct kernel_msghdr *msg, int flags)
 {
     return -EIO;
-}
-
-size_t socket_write(size_t offset, size_t len, void *buffer, struct file *file)
-{
-    socket *s = file_to_socket(file);
-
-    msghdr msg;
-    msg.msg_control = nullptr;
-    msg.msg_controllen = 0;
-    msg.msg_flags = 0;
-
-    iovec vec0;
-    /* This cast is safe because sendmsg won't write to the iov */
-    vec0.iov_base = const_cast<void *>(buffer);
-    vec0.iov_len = len;
-    msg.msg_iov = &vec0;
-    msg.msg_iovlen = 1;
-    msg.msg_name = nullptr;
-    msg.msg_namelen = 0;
-
-    return s->sock_ops->sendmsg(s, &msg, fd_flags_to_msg_flags(file));
 }
 
 ssize_t socket_write_iter(file *filp, size_t offset, iovec_iter *iter, unsigned int flags)
@@ -185,50 +164,28 @@ ssize_t socket_write_iter(file *filp, size_t offset, iovec_iter *iter, unsigned 
     (void) flags;
     socket *s = file_to_socket(filp);
 
-    msghdr msg;
+    kernel_msghdr msg;
     msg.msg_control = nullptr;
     msg.msg_controllen = 0;
     msg.msg_flags = 0;
-    msg.msg_iov = iter->vec.data();
-    msg.msg_iovlen = iter->vec.size();
+    msg.msg_iter = iter;
     msg.msg_name = nullptr;
     msg.msg_namelen = 0;
 
     return s->sock_ops->sendmsg(s, &msg, fd_flags_to_msg_flags(filp));
 }
 
-size_t socket_read(size_t offset, size_t len, void *buffer, file *file)
-{
-    socket *s = file_to_socket(file);
-    msghdr msg;
-    msg.msg_control = nullptr;
-    msg.msg_controllen = 0;
-    msg.msg_flags = 0;
-
-    iovec vec0;
-    /* This cast is safe because sendmsg won't write to the iov */
-    vec0.iov_base = buffer;
-    vec0.iov_len = len;
-    msg.msg_iov = &vec0;
-    msg.msg_iovlen = 1;
-    msg.msg_name = nullptr;
-    msg.msg_namelen = 0;
-
-    return s->sock_ops->recvmsg(s, &msg, fd_flags_to_msg_flags(file));
-}
-
-ssize_t socker_read_iter(file *filp, size_t offset, iovec_iter *iter, unsigned int flags)
+ssize_t socket_read_iter(file *filp, size_t offset, iovec_iter *iter, unsigned int flags)
 {
     (void) offset;
     (void) flags;
     socket *s = file_to_socket(filp);
 
-    msghdr msg;
+    kernel_msghdr msg;
     msg.msg_control = nullptr;
     msg.msg_controllen = 0;
     msg.msg_flags = 0;
-    msg.msg_iov = iter->vec.data();
-    msg.msg_iovlen = iter->vec.size();
+    msg.msg_iter = iter;
     msg.msg_name = nullptr;
     msg.msg_namelen = 0;
 
@@ -420,11 +377,11 @@ unsigned int socket_ioctl(int request, void *argp, struct file *file)
 }
 
 struct file_ops socket_ops = {
-    .read = socket_read,
-    .write = socket_write,
     .close = socket_close,
     .ioctl = socket_ioctl,
     .poll = socket_poll,
+    .read_iter = socket_read_iter,
+    .write_iter = socket_write_iter,
 };
 
 auto_file get_socket_fd(int fd)
@@ -433,7 +390,7 @@ auto_file get_socket_fd(int fd)
     if (!desc)
         return errno = EBADF, nullptr;
 
-    if (desc->f_ino->i_fops->write != socket_write)
+    if (desc->f_ino->i_fops != &socket_ops)
     {
         fd_put(desc);
         return errno = ENOTSOCK, nullptr;
@@ -530,7 +487,7 @@ ssize_t sys_recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockad
             return -EFAULT;
     }
 
-    msghdr msg;
+    kernel_msghdr msg;
     msg.msg_control = nullptr;
     msg.msg_controllen = 0;
     msg.msg_flags = 0;
@@ -539,8 +496,8 @@ ssize_t sys_recvfrom(int sockfd, void *buf, size_t len, int flags, struct sockad
     /* This cast is safe because sendmsg won't write to the iov */
     vec0.iov_base = const_cast<void *>(buf);
     vec0.iov_len = len;
-    msg.msg_iov = &vec0;
-    msg.msg_iovlen = 1;
+    iovec_iter iter{{&vec0, 1}, len};
+    msg.msg_iter = &iter;
     msg.msg_name = src_addr ? &sa : nullptr;
     msg.msg_namelen = src_addr ? addrlen : 0;
 
@@ -1232,15 +1189,35 @@ int copy_msghdr_from_user(msghdr *msg, const msghdr *umsg, msghdr_guard &guard)
     return 0;
 }
 
-ssize_t socket_sendmsg(socket *sock, msghdr *umsg, int flags)
+static kernel_msghdr msghdr_to_kmsghdr(msghdr *msg, struct iovec_iter *iter)
+{
+    kernel_msghdr kmsg;
+    kmsg.msg_iter = iter;
+    kmsg.msg_control = msg->msg_control;
+    kmsg.msg_controllen = msg->msg_controllen;
+    kmsg.msg_flags = msg->msg_flags;
+    kmsg.msg_name = msg->msg_name;
+    kmsg.msg_namelen = msg->msg_namelen;
+    return kmsg;
+}
+
+static ssize_t socket_sendmsg(socket *sock, msghdr *umsg, int flags)
 {
     msghdr msg;
     msghdr_guard g;
+    kernel_msghdr kmsg;
+    ssize_t len;
 
     if (int st = copy_msghdr_from_user(&msg, umsg, g); st < 0)
         return st;
 
-    return sock->sock_ops->sendmsg(sock, &msg, flags);
+    len = iovec_count_length(msg.msg_iov, msg.msg_iovlen);
+    if (len < 0)
+        return len;
+
+    iovec_iter iter{{msg.msg_iov, (size_t) msg.msg_iovlen}, (size_t) len};
+    kmsg = msghdr_to_kmsghdr(&msg, &iter);
+    return sock->sock_ops->sendmsg(sock, &kmsg, flags);
 }
 
 ssize_t sys_sendto(int sockfd, const void *buf, size_t len, int flags, struct sockaddr *addr,
@@ -1264,7 +1241,7 @@ ssize_t sys_sendto(int sockfd, const void *buf, size_t len, int flags, struct so
             return -EFAULT;
     }
 
-    msghdr msg;
+    kernel_msghdr msg;
     msg.msg_control = nullptr;
     msg.msg_controllen = 0;
     msg.msg_flags = 0;
@@ -1273,8 +1250,8 @@ ssize_t sys_sendto(int sockfd, const void *buf, size_t len, int flags, struct so
     /* This cast is safe because sendmsg won't write to the iov */
     vec0.iov_base = const_cast<void *>(buf);
     vec0.iov_len = len;
-    msg.msg_iov = &vec0;
-    msg.msg_iovlen = 1;
+    iovec_iter iter{{&vec0, 1}, len};
+    msg.msg_iter = &iter;
     msg.msg_name = addr ? &sa : nullptr;
     msg.msg_namelen = addr ? addrlen : 0;
 
@@ -1299,12 +1276,21 @@ ssize_t socket_recvmsg(socket *sock, msghdr *umsg, int flags)
 {
     msghdr msg;
     msghdr_guard g;
+    struct kernel_msghdr kmsg;
+    ssize_t iovlen;
 
     if (int st = copy_msghdr_from_user(&msg, umsg, g); st < 0)
         return st;
 
+    iovlen = iovec_count_length(msg.msg_iov, msg.msg_iovlen);
+    if (iovlen < 0)
+        return iovlen;
+
+    iovec_iter iter{{msg.msg_iov, (size_t) msg.msg_iovlen}, (size_t) iovlen};
+    kmsg = msghdr_to_kmsghdr(&msg, &iter);
+
     socklen_t len = msg.msg_controllen;
-    auto st = sock->sock_ops->recvmsg(sock, &msg, flags);
+    auto st = sock->sock_ops->recvmsg(sock, &kmsg, flags);
 
     if (st < 0)
         return st;
@@ -1312,17 +1298,17 @@ ssize_t socket_recvmsg(socket *sock, msghdr *umsg, int flags)
     msg.msg_control = g.ucontrol;
     msg.msg_iov = (iovec *) g.uiov;
     msg.msg_name = g.uname;
-    msg.msg_controllen = len - msg.msg_controllen;
+    msg.msg_controllen = len - kmsg.msg_controllen;
 
     if (msg.msg_control)
     {
-        if (copy_to_user(msg.msg_control, g.msg_control, msg.msg_controllen) < 0)
+        if (copy_to_user(msg.msg_control, g.msg_control, kmsg.msg_controllen) < 0)
             return -EFAULT;
     }
 
     if (msg.msg_name)
     {
-        if (copy_to_user(msg.msg_name, g.uname, msg.msg_namelen) < 0)
+        if (copy_to_user(msg.msg_name, g.uname, kmsg.msg_namelen) < 0)
             return -EFAULT;
     }
 

kernel/include/onyx/iovec_iter.h
@@ -23,21 +23,29 @@ enum iovec_type
 };
 
 #ifdef __cplusplus
-
-#include <onyx/assert.h>
-
 #include <onyx/slice.hpp>
 #include <onyx/utility.hpp>
+#endif
+#include <onyx/assert.h>
+
+__BEGIN_CDECLS
+struct iovec_iter;
+
+void iovec_iter_advance(struct iovec_iter *iter, size_t len);
 
 struct iovec_iter
 {
-    cul::slice<iovec> vec;
-    size_t pos_{0};
+    union {
+        const struct iovec *vec;
+    };
+    size_t nr_vecs;
+    size_t pos_;
     size_t bytes;
     enum iovec_type type;
 
+#ifdef __cplusplus
     iovec_iter(cul::slice<iovec> vec, size_t length, enum iovec_type type = IOVEC_USER)
-        : vec{cul::move(vec)}, bytes{length}, type{type}
+        : vec{vec.cbegin()}, nr_vecs{vec.size()}, pos_{0}, bytes{length}, type{type}
     {
         if (length > 0)
             skip_zero_len();
@@ -48,44 +56,31 @@ struct iovec_iter
         /* Required so users never need to explicitly handle zero-len iov_lens. It's all handled by
          * skip_zero_len in the ctor, and advance().
          */
-        while (vec.front().iov_len == 0)
-            vec.adjust(1);
+        if (type != IOVEC_USER)
+            return;
+        while (vec->iov_len == 0)
+        {
+            vec++;
+            nr_vecs--;
+        }
     }
 
     [[nodiscard]] bool empty()
     {
-        return vec.size() == 0;
+        return nr_vecs == 0;
     }
 
     [[nodiscard]] iovec curiovec() const
     {
-        return {(void *) ((u8 *) vec.front().iov_base + pos_), vec.front().iov_len - pos_};
+        return {(void *) ((u8 *) vec->iov_base + pos_), vec->iov_len - pos_};
     }
 
     void advance(size_t len)
     {
-        const auto &cur = vec.front();
-
-        DCHECK(cur.iov_len >= pos_ + len);
-        DCHECK(bytes >= len);
-        pos_ += len;
-        bytes -= len;
-
-        while (!empty() && pos_ == vec.front().iov_len)
-        {
-            vec.adjust(1);
-            pos_ = 0;
-        }
+        iovec_iter_advance(this, len);
     }
-};
-#else
-
-/* Opaque definition of iovec_iter */
-struct iovec_iter;
-
 #endif
-
-__BEGIN_CDECLS
+};
 
 static inline ssize_t iovec_count_length(struct iovec *vec, unsigned int n)
 {
@@ -133,6 +128,16 @@ ssize_t copy_to_iter(struct iovec_iter *iter, const void *buf, size_t len);
  * @return True if aligned, else false
  */
 bool iovec_is_aligned(struct iovec_iter *iter, unsigned long alignment);
+
+static inline size_t iovec_iter_bytes(struct iovec_iter *iter)
+{
+    return iter->bytes;
+}
+
+static inline bool iovec_iter_empty(struct iovec_iter *iter)
+{
+    return iter->nr_vecs == 0;
+}
 
 __END_CDECLS
 

kernel/include/onyx/net/icmp.h
@@ -129,11 +129,11 @@ public:
 
     int bind(struct sockaddr *addr, socklen_t addrlen);
     int connect(struct sockaddr *addr, socklen_t addrlen, int flags);
-    ssize_t sendmsg(const struct msghdr *msg, int flags);
+    ssize_t sendmsg(const struct kernel_msghdr *msg, int flags);
     int getsockopt(int level, int optname, void *val, socklen_t *len);
     int setsockopt(int level, int optname, const void *val, socklen_t len);
     short poll(void *poll_file, short events);
-    ssize_t recvmsg(msghdr *msg, int flags);
+    ssize_t recvmsg(kernel_msghdr *msg, int flags);
 
     bool match_filter(const icmp_header *header)
     {

kernel/include/onyx/net/icmpv6.h
@@ -94,11 +94,11 @@ public:
 
     int bind(struct sockaddr *addr, socklen_t addrlen);
     int connect(struct sockaddr *addr, socklen_t addrlen, int flags);
-    ssize_t sendmsg(const struct msghdr *msg, int flags);
+    ssize_t sendmsg(const struct kernel_msghdr *msg, int flags);
     int getsockopt(int level, int optname, void *val, socklen_t *len);
     int setsockopt(int level, int optname, const void *val, socklen_t len);
     short poll(void *poll_file, short events);
-    ssize_t recvmsg(msghdr *msg, int flags);
+    ssize_t recvmsg(kernel_msghdr *msg, int flags);
 
     bool match_filter(const icmpv6_header *header)
     {

kernel/include/onyx/net/inet_socket.h
@@ -23,7 +23,7 @@ struct inet_proto_family;
 namespace ip
 {
 
-void copy_msgname_to_user(struct msghdr *msg, packetbuf *buf, bool isv6, in_port_t port);
+void copy_msgname_to_user(struct kernel_msghdr *msg, packetbuf *buf, bool isv6, in_port_t port);
 
 }; // namespace ip
 

kernel/include/onyx/net/netkernel.h
@@ -69,8 +69,8 @@ public:
     int getsockopt(int level, int optname, void *optval, socklen_t *optlen);
     int setsockopt(int level, int optname, const void *optval, socklen_t optlen);
     int connect(sockaddr *addr, socklen_t addrlen, int flags);
-    ssize_t sendmsg(const struct msghdr *msg, int flags);
-    ssize_t recvmsg(struct msghdr *msg, int flags);
+    ssize_t sendmsg(const struct kernel_msghdr *msg, int flags);
+    ssize_t recvmsg(struct kernel_msghdr *msg, int flags);
 
     expected<packetbuf *, int> get_datagram(int flags)
     {

kernel/include/onyx/net/socket.h
@@ -39,6 +39,16 @@
 
 struct socket;
 
+struct kernel_msghdr
+{
+    void *msg_name;
+    socklen_t msg_namelen;
+    struct iovec_iter *msg_iter;
+    void *msg_control;
+    socklen_t msg_controllen;
+    int msg_flags;
+};
+
 struct socket_ops
 {
     void (*destroy)(struct socket *);
@@ -46,8 +56,8 @@ struct socket_ops
     struct socket *(*accept)(struct socket *, int flags);
     int (*bind)(struct socket *, struct sockaddr *addr, socklen_t addrlen);
     int (*connect)(struct socket *, struct sockaddr *addr, socklen_t addrlen, int flags);
-    ssize_t (*sendmsg)(struct socket *, const struct msghdr *msg, int flags);
-    ssize_t (*recvmsg)(struct socket *, struct msghdr *msg, int flags);
+    ssize_t (*sendmsg)(struct socket *, const struct kernel_msghdr *msg, int flags);
+    ssize_t (*recvmsg)(struct socket *, struct kernel_msghdr *msg, int flags);
     int (*getsockname)(struct socket *, struct sockaddr *addr, socklen_t *addrlen);
     int (*getpeername)(struct socket *, struct sockaddr *addr, socklen_t *addrlen);
     int (*shutdown)(struct socket *, int how);
@@ -180,8 +190,8 @@ public:
     socket *accept(int flags);
     int bind(sockaddr *addr, socklen_t addrlen);
     int connect(sockaddr *addr, socklen_t addrlen, int flags);
-    ssize_t sendmsg(const struct msghdr *msg, int flags);
-    ssize_t recvmsg(struct msghdr *msg, int flags);
+    ssize_t sendmsg(const struct kernel_msghdr *msg, int flags);
+    ssize_t recvmsg(struct kernel_msghdr *msg, int flags);
     int getsockname(sockaddr *addr, socklen_t *addrlen);
     int getpeername(sockaddr *addr, socklen_t *addrlen);
     int shutdown(int how);
@@ -247,13 +257,13 @@ int cpp_connect(struct socket *sock, struct sockaddr *addr, socklen_t addrlen, i
 }
 
 template <typename T>
-ssize_t cpp_sendmsg(struct socket *sock, const struct msghdr *msg, int flags)
+ssize_t cpp_sendmsg(struct socket *sock, const struct kernel_msghdr *msg, int flags)
 {
     return ((T *) sock)->sendmsg(msg, flags);
 }
 
 template <typename T>
-ssize_t cpp_recvmsg(struct socket *sock, struct msghdr *msg, int flags)
+ssize_t cpp_recvmsg(struct socket *sock, struct kernel_msghdr *msg, int flags)
 {
     return ((T *) sock)->recvmsg(msg, flags);
 }

kernel/include/onyx/net/udp.h
@@ -66,7 +66,7 @@ class udp_socket : public inet_socket
     }
 
     template <typename AddrType>
-    ssize_t udp_sendmsg(const msghdr *msg, int flags, const inet_sock_address &dst);
+    ssize_t udp_sendmsg(const kernel_msghdr *msg, int flags, const inet_sock_address &dst);
 
     unsigned int wants_cork : 1;
 
@@ -80,12 +80,10 @@ public:
 
     int bind(sockaddr *addr, socklen_t len);
     int connect(sockaddr *addr, socklen_t len, int flags);
-    ssize_t sendmsg(const msghdr *msg, int flags);
+    ssize_t sendmsg(const kernel_msghdr *msg, int flags);
     int getsockopt(int level, int optname, void *val, socklen_t *len);
     int setsockopt(int level, int optname, const void *val, socklen_t len);
-    int send_packet(const msghdr *msg, ssize_t payload_size, in_port_t source_port,
-                    in_port_t dest_port, inet_route &route, int msg_domain);
-    ssize_t recvmsg(msghdr *msg, int flags);
+    ssize_t recvmsg(kernel_msghdr *msg, int flags);
 
     void rx_dgram(packetbuf *buf);
 

commit c94bb999bac1669c284637a0eeab968d7d3887e7

net: Copy the correct amount of CMSG bytes on recvmsg
There's a difference between the msg_controllen in kmsg vs userspace
msghdr. kernel_msghdr's msg_controllen counts the number of bytes
*remaining* in the cmsg buffer. Not the number of bytes that were
actually placed there by the kernel.

As such, copy msg.msg_controllen bytes instead.

This fixes mysterious problems passing cmsg data to userspace, where it
may not see the data correctly if kmsg.msg_controllen <
msg.msg_controllen.

Fixes: 566cec024539 ("net: Pass iovec_iter around the stack")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/kernel/net/socket.cpp
@@ -1404,7 +1404,7 @@ ssize_t socket_recvmsg(socket *sock, msghdr *umsg, int flags)
 
     if (msg.msg_control)
     {
-        if (copy_to_user(msg.msg_control, g.msg_control, kmsg.msg_controllen) < 0)
+        if (copy_to_user(msg.msg_control, g.msg_control, msg.msg_controllen) < 0)
             return -EFAULT;
     }
 