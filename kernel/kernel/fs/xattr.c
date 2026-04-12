23bdd2d9efbb66e1eb4fc835e29a59bc68998587
Pedro Falcato <pedro.falcato@gmail.com>
2026-04-12 22:18:09 +0100

    fs/xattr: add stubs for the rest of the xattr syscalls
    None are yet implemented. But dnf seems to cope with it.
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- kernel/kernel/fs/xattr.c
+++ kernel/kernel/fs/xattr.c
@@ -21,3 +21,48 @@ ssize_t sys_llistxattr(const char *path, char *list, size_t size)
 {
     return -ENOTSUP;
 }
+
+int sys_setxattr(const char *path, const char *name, const void *value, size_t size, int flags)
+{
+    return -ENOTSUP;
+}
+
+int sys_lsetxattr(const char *path, const char *name, const void *value, size_t size, int flags)
+{
+    return -ENOTSUP;
+}
+
+int sys_fsetxattr(int fd, const char *name, const void *value, size_t size, int flags)
+{
+    return -ENOTSUP;
+}
+
+ssize_t sys_getxattr(const char *path, const char *name, void *value, size_t size)
+{
+    return -ENOTSUP;
+}
+
+ssize_t sys_lgetxattr(const char *path, const char *name, void *value, size_t size)
+{
+    return -ENOTSUP;
+}
+
+ssize_t sys_fgetxattr(int fd, const char *name, void *value, size_t size)
+{
+    return -ENOTSUP;
+}
+
+int sys_removexattr(const char *path, const char *name)
+{
+    return -ENOTSUP;
+}
+
+int sys_lremovexattr(const char *path, const char *name)
+{
+    return -ENOTSUP;
+}
+
+int sys_fremovexattr(int fd, const char *name)
+{
+    return -ENOTSUP;
+}
