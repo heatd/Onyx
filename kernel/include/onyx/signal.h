heatd/Onyx

commit d5bbf4b51c16d7da19f03b58172fb13356fada39

fork: Implement proper sys_clone semantics
Implement proper sys_clone semantics. Now every struct process has a
single struct thread. A good part of signals has been reworked, wait and
exit too.

Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/include/onyx/signal.h
@@ -88,17 +88,6 @@ struct sigpending
     siginfo_t *info;
     int signum;
     struct list_head list_node;
-
-#ifdef __cplusplus
-    constexpr sigpending() : info{nullptr}, signum{}, list_node{}
-    {
-    }
-
-    ~sigpending()
-    {
-        delete info;
-    }
-#endif
 };
 
 static inline bool signal_is_realtime(int sig)
@@ -111,9 +100,8 @@ static inline bool signal_is_standard(int sig)
     return !signal_is_realtime(sig);
 }
 
-#define THREAD_SIGNAL_STOPPING        (1 << 0)
-#define THREAD_SIGNAL_EXITING         (1 << 1)
-#define THREAD_SIGNAL_ORIGINAL_SIGSET (1 << 2)
+#define THREAD_SIGNAL_STOPPING (1 << 0)
+#define THREAD_SIGNAL_EXITING  (1 << 1)
 
 struct sighand_struct
 {
@@ -131,176 +119,28 @@ static inline void sighand_init(struct sighand_struct *s)
 
 struct process;
 
-struct signal_info
+struct sigqueue
 {
-    /* Signal mask */
-    sigset_t sigmask;
-
-    struct spinlock lock;
-
-    /* Pending signal set */
-    sigset_t pending_set;
-
+    /* Note: pending, pending_head are protected by sighand->signal_lock */
+    sigset_t pending;
     struct list_head pending_head;
-
-    unsigned short flags;
-
-    unsigned long times_interrupted;
-    bool signal_pending;
-
-    /* No need for a lock here since any possible changes
-     * to this variable happen in kernel mode, in this exact thread.
-     */
-    stack_t altstack;
-
-    // Used by pselect, ppoll, sigsuspend
-    sigset_t original_sigset;
-
-#ifdef __cplusplus
-
-private:
-    bool is_signal_pending_internal() const
-    {
-        const sigset_t &set = pending_set;
-        const sigset_t &blocked_set = sigmask;
-        sigset_t temp = blocked_set;
-        signotset(&temp);
-        sigset_t s;
-        sigandset(&s, &set, &temp);
-
-        return !sigisemptyset(&s);
-    }
-
-public:
-    sigset_t get_mask()
-    {
-        scoped_lock g{lock};
-        return sigmask;
-    }
-
-    sigset_t get_pending_set()
-    {
-        scoped_lock g{lock};
-        return pending_set;
-    }
-
-    sigset_t get_effective_pending()
-    {
-        scoped_lock g{lock};
-        auto set = get_pending_set();
-        auto blocked_set = get_mask();
-        sigandset(&set, &set, &blocked_set);
-
-        return set;
-    }
-
-    sigset_t __add_blocked(const sigset_t *blocked, bool update_pending = true)
-    {
-        auto old = sigmask;
-        sigorset(&sigmask, &sigmask, blocked);
-        sigdelset(&sigmask, SIGKILL);
-        sigdelset(&sigmask, SIGSTOP);
-
-        if (update_pending)
-            __update_pending();
-        return old;
-    }
-
-    sigset_t add_blocked(const sigset_t *blocked, bool update_pending = true)
-    {
-        scoped_lock g{lock};
-        return __add_blocked(blocked, update_pending);
-    }
-
-    sigset_t set_blocked(const sigset_t *blocked, bool update_pending = true)
-    {
-        scoped_lock g{lock};
-        auto old = sigmask;
-        memcpy(&sigmask, blocked, sizeof(sigset_t));
-        sigdelset(&sigmask, SIGKILL);
-        sigdelset(&sigmask, SIGSTOP);
-
-        if (update_pending)
-            __update_pending();
-        return old;
-    }
-
-    sigset_t unblock(sigset_t &mask, bool update_pending = true)
-    {
-        scoped_lock g{lock};
-        auto old = sigmask;
-        signotset(&mask);
-        sigandset(&sigmask, &sigmask, &mask);
-
-        if (update_pending)
-            __update_pending();
-        return old;
-    }
-
-    void __update_pending()
-    {
-        MUST_HOLD_LOCK(&lock);
-
-        signal_pending = flags != 0 || is_signal_pending_internal();
-    }
-
-    void update_pending()
-    {
-        scoped_lock g{lock};
-        __update_pending();
-    }
-
-    void reroute_signals(process *p);
-
-    bool add_pending(struct sigpending *pend)
-    {
-        scoped_lock g{lock};
-
-        if (signal_is_standard(pend->signum) && sigismember(&pending_set, pend->signum))
-            return false;
-
-        list_add(&pend->list_node, &pending_head);
-
-        sigaddset(&pending_set, pend->signum);
-
-        if (!sigismember(&sigmask, pend->signum))
-            signal_pending = true;
-
-        return true;
-    }
-
-    bool try_to_route(struct sigpending *pend)
-    {
-        scoped_lock g{lock};
-
-        if (sigismember(&sigmask, pend->signum))
-            return false;
-
-        if (signal_is_standard(pend->signum) && sigismember(&pending_set, pend->signum))
-            return false;
-
-        list_add(&pend->list_node, &pending_head);
-        sigaddset(&pending_set, pend->signum);
-        signal_pending = true;
-
-        return true;
-    }
-
-    constexpr signal_info()
-        : sigmask{}, lock{}, pending_set{}, pending_head{}, flags{}, times_interrupted{},
-          signal_pending{}, altstack{}
-    {
-        INIT_LIST_HEAD(&pending_head);
-        altstack.ss_flags = SS_DISABLE;
-    }
-
-    ~signal_info();
-#endif
 };
 
+static inline void sigqueue_init(struct sigqueue *queue)
+{
+    queue->pending = (sigset_t){};
+    INIT_LIST_HEAD(&queue->pending_head);
+}
+
 #define SIGNAL_GROUP_STOPPED (1 << 0)
+/* SIGNAL_GROUP_PENDING is set when there's a CONT status to reap (since we last looked at this
+ * process in wait4). */
 #define SIGNAL_GROUP_CONT    (1 << 1)
 #define SIGNAL_GROUP_EXIT    (1 << 2)
+
+/* We set CONT_PENDING if we're pending the parent notification on SIGCONT. It is set when sending
+ * SIGCONT. */
+#define SIGNAL_GROUP_CONT_PENDING (1 << 3)
 
 struct process;
 struct thread;
@@ -314,7 +154,6 @@ void handle_signal(struct registers *regs);
 #define SIGNAL_IN_BROADCAST (1 << 1)
 
 int kernel_raise_signal(int sig, struct process *process, unsigned int flags, siginfo_t *info);
-int kernel_tkill(int signal, struct thread *thread, unsigned int flags, siginfo_t *info);
 int signal_kill_pg(int sig, int flags, siginfo_t *info, pid_t pid);
 void signal_context_init(struct thread *new_thread);
 void signal_do_execve(struct process *proc);
@@ -324,6 +163,67 @@ static inline bool signal_is_stopping(int sig)
 {
     return sig == SIGSTOP || sig == SIGTSTP || sig == SIGTTIN || sig == SIGTTOU;
 }
+
+static inline void sigaltstack_init(stack_t *stack)
+{
+    stack->ss_size = 0;
+    stack->ss_sp = NULL;
+    stack->ss_flags = SS_DISABLE;
+}
+
+int raise_sig_thr(int sig, struct process *task, unsigned int flags, siginfo_t *info);
+int raise_sig_curthr(int sig, unsigned int flags, siginfo_t *info);
+
+/**
+ * @brief Low-level helper for signal-related code (do not use, probably)
+ * Sets TF_SIGPENDING and tries to wake it up, if possible.
+ *
+ * @param task Task (thread) to wake up
+ * @param signal Signal number
+ */
+void signal_interrupt_task(struct process *task, int signal);
+
+/* The two following helpers implement saved sigmask semantics for Onyx. System calls that take
+ * sigmasks are supposed to use signal_setmask_and_save at the start, and signal_restore_sigmask if
+ * not interrupted by a signal. If interrupted by a signal, the signal handling code will save the
+ * sigmask on the stack, which will be transparently restored by sigreturn. */
+
+/**
+ * @brief Set the mask and store the old one for saving later
+ * To be used by syscalls that need such semantics (ppoll, pselect, sigsuspend, etc)
+ *
+ * @param mask New sigmask
+ */
+void signal_setmask_and_save(const sigset_t *mask);
+
+/**
+ * @brief Restore the saved sigmask
+ * To be used by syscalls that need such semantics (ppoll, pselect, sigsuspend, etc)
+ *
+ */
+void signal_restore_sigmask(void);
+
+void signal_setmask(const sigset_t *mask);
+
+/**
+ * @brief Notify this task's parent that we're exiting
+ * We have to be careful and check if we need to, e.g, autoreap. write_lock needs to be held when
+ * calling.
+ *
+ * @param exit_code Exit code to notify with
+ * @retval true If task should be autoreaped (thus no signal was sent, nor did we wake anyone up)
+ */
+bool parent_notify(unsigned int exit_code);
+
+/**
+ * @brief Notify this task's parent that we're stopping/continuing
+ * We have to be careful and check if we need to, e.g, not send anything. tasklist read_lock needs
+ * to be held when calling.
+ *
+ * @param exit_code Stop code to notify with
+ * @retval true If task was woken up
+ */
+bool notify_process_stop_cont(struct process *task, unsigned int exit_code);
 
 __END_CDECLS
 

kernel/arch/riscv64/interrupts.S
@@ -277,3 +277,10 @@ return_from_execve:
     xor x1, x1, x1
 
     sret
+
+.global ret_from_fork_asm
+.type ret_from_fork_asm, @function
+ret_from_fork_asm:
+    call ret_from_fork
+    RISCV_RESTORE_CONTEXT
+    sret

kernel/arch/riscv64/process.cpp
@@ -14,6 +14,22 @@
 
 #include <uapi/clone.h>
 
+namespace riscv::internal
+{
+void thread_setup_stack(thread *thread, bool is_user, const registers_t *regs);
+} // namespace riscv::internal
+
+extern "C" void ret_from_fork_asm(void);
+
+extern "C" void ret_from_fork(void)
+{
+    /* We take care of everything that needs to be done post-fork on the child process (that we
+     * couldn't really do beforehand) */
+    struct process *current = get_current_process();
+    if (current->set_tid)
+        copy_to_user(current->set_tid, &current->pid_, sizeof(pid_t));
+}
+
 struct thread *process_fork_thread(thread_t *src, struct process *dest, unsigned int flags,
                                    unsigned long stack, unsigned long tls)
 {
@@ -33,40 +49,19 @@ struct thread *process_fork_thread(thread_t *src, struct process *dest, unsigned
     if (!thread)
         return nullptr;
 
+    regs.epc = (unsigned long) &ret_from_fork_asm;
+    riscv::internal::thread_setup_stack(thread, false, &regs);
     save_fpu(thread->fpu_area);
 
     thread->owner = dest;
     thread->set_aspace(dest->get_aspace());
-
-    list_add_tail(&thread->thread_list_head, &dest->thread_list);
-
-    dest->nr_threads = 1;
-
+    dest->thr = thread;
     return thread;
 }
 
-#define CLONE_FORK        (1 << 0)
-#define CLONE_SPAWNTHREAD (1 << 1)
-long valid_flags = CLONE_FORK | CLONE_SPAWNTHREAD;
-
-struct tid_out
+int sys_clone(void *fn, void *child_stack, int flags, void *arg, struct tid_out *out, void *tls)
 {
-    /* TID is placed here */
-    pid_t *ptid;
-    /* This location is zero'd when the thread exits */
-    pid_t *ctid;
-};
-
-/* Hmmm, I don't think this is 100% correct but it's good enough */
-static void inherit_signal_flags(thread *newt)
-{
-    auto current_thread = get_current_thread();
-
-    scoped_lock g{current_thread->sinfo.lock};
-    scoped_lock g2{newt->sinfo.lock};
-
-    newt->sinfo.flags |= current_thread->sinfo.flags;
-    newt->sinfo.__update_pending();
+    return -ENOSYS;
 }
 
 int sys_clone(void *fn, void *child_stack, int flags, void *arg, struct tid_out *out, void *tls)

kernel/arch/riscv64/traps.cpp
@@ -97,7 +97,7 @@ static void instruction_address_misaligned(registers_t *ctx)
     info.si_code = BUS_ADRALN;
     info.si_addr = (void *) ctx->tval;
 
-    kernel_tkill(SIGBUS, get_current_thread(), SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGBUS, SIGNAL_FORCE, &info);
 }
 
 static void instruction_access_fault(registers_t *ctx)
@@ -106,7 +106,7 @@ static void instruction_access_fault(registers_t *ctx)
     info.si_code = BUS_ADRERR;
     info.si_addr = (void *) ctx->tval;
 
-    kernel_tkill(SIGBUS, get_current_thread(), SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGBUS, SIGNAL_FORCE, &info);
 }
 
 static void illegal_instruction(registers_t *ctx)
@@ -115,7 +115,7 @@ static void illegal_instruction(registers_t *ctx)
     info.si_code = ILL_ILLOPC;
     info.si_addr = (void *) ctx->tval;
 
-    kernel_tkill(SIGILL, get_current_thread(), SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGILL, SIGNAL_FORCE, &info);
 }
 
 static void breakpoint_trap(registers_t *ctx)
@@ -123,7 +123,7 @@ static void breakpoint_trap(registers_t *ctx)
     siginfo_t info = {};
     info.si_code = TRAP_BRKPT;
 
-    kernel_tkill(SIGTRAP, get_current_thread(), SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGTRAP, SIGNAL_FORCE, &info);
 }
 
 static void load_address_misaligned(registers_t *ctx)
@@ -132,7 +132,7 @@ static void load_address_misaligned(registers_t *ctx)
     info.si_code = BUS_ADRALN;
     info.si_addr = (void *) ctx->tval;
 
-    kernel_tkill(SIGBUS, get_current_thread(), SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGBUS, SIGNAL_FORCE, &info);
 }
 
 static void load_access_fault(registers_t *ctx)
@@ -141,7 +141,7 @@ static void load_access_fault(registers_t *ctx)
     info.si_code = BUS_ADRERR;
     info.si_addr = (void *) ctx->tval;
 
-    kernel_tkill(SIGBUS, get_current_thread(), SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGBUS, SIGNAL_FORCE, &info);
 }
 
 static void store_address_misaligned(registers_t *ctx)
@@ -150,7 +150,7 @@ static void store_address_misaligned(registers_t *ctx)
     info.si_code = BUS_ADRALN;
     info.si_addr = (void *) ctx->tval;
 
-    kernel_tkill(SIGBUS, get_current_thread(), SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGBUS, SIGNAL_FORCE, &info);
 }
 
 static void store_access_fault(registers_t *ctx)
@@ -159,7 +159,7 @@ static void store_access_fault(registers_t *ctx)
     info.si_code = BUS_ADRERR;
     info.si_addr = (void *) ctx->tval;
 
-    kernel_tkill(SIGBUS, get_current_thread(), SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGBUS, SIGNAL_FORCE, &info);
 }
 
 #define PF_R (1 << 0)

kernel/arch/x86_64/interrupts.S
@@ -192,6 +192,11 @@ ENTRY(x86_scheduler_exit)
     jmp 2b
 END(x86_scheduler_exit)
 
+ENTRY(ret_from_fork_asm)
+    call ret_from_fork
+    jmp x86_scheduler_exit
+END(ret_from_fork_asm)
+
 .pushsection .text.x86_entry
 
 /* Everything past this point is permanently mapped */

kernel/arch/x86_64/isr.cpp
@@ -25,6 +25,7 @@
 #include <onyx/x86/isr.h>
 #include <onyx/x86/ktrace.h>
 #include <onyx/x86/mce.h>
+#include <onyx/x86/msr.h>
 
 /* TODO: Move scope_guard somewhere else */
 #include <onyx/trace/trace_base.h>
@@ -94,12 +95,36 @@ void div0_exception(struct registers *ctx)
         panic("Divide by zero exception");
     }
 
-    struct thread *current = get_current_thread();
     siginfo_t info = {};
     info.si_code = SI_KERNEL;
 
-    kernel_tkill(SIGFPE, current, SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGFPE, SIGNAL_FORCE, &info);
 }
+
+#define MSR_IA32_DEBUGCTLMSR      0x000001d9
+#define MSR_IA32_LASTBRANCHFROMIP 0x000001db
+#define MSR_IA32_LASTBRANCHTOIP   0x000001dc
+#define MSR_IA32_LASTINTFROMIP    0x000001dd
+#define MSR_IA32_LASTINTTOIP      0x000001de
+
+#define MSR_IA32_PASID       0x00000d93
+#define MSR_IA32_PASID_VALID BIT_ULL(31)
+
+/* DEBUGCTLMSR bits (others vary by model): */
+#define DEBUGCTLMSR_LBR_BIT               0 /* last branch recording */
+#define DEBUGCTLMSR_LBR                   (1UL << DEBUGCTLMSR_LBR_BIT)
+#define DEBUGCTLMSR_BTF_SHIFT             1
+#define DEBUGCTLMSR_BTF                   (1UL << 1) /* single-step on branches */
+#define DEBUGCTLMSR_BUS_LOCK_DETECT       (1UL << 2)
+#define DEBUGCTLMSR_TR                    (1UL << 6)
+#define DEBUGCTLMSR_BTS                   (1UL << 7)
+#define DEBUGCTLMSR_BTINT                 (1UL << 8)
+#define DEBUGCTLMSR_BTS_OFF_OS            (1UL << 9)
+#define DEBUGCTLMSR_BTS_OFF_USR           (1UL << 10)
+#define DEBUGCTLMSR_FREEZE_LBRS_ON_PMI    (1UL << 11)
+#define DEBUGCTLMSR_FREEZE_PERFMON_ON_PMI (1UL << 12)
+#define DEBUGCTLMSR_FREEZE_IN_SMM_BIT     14
+#define DEBUGCTLMSR_FREEZE_IN_SMM         (1UL << DEBUGCTLMSR_FREEZE_IN_SMM_BIT)
 
 void debug_trap(struct registers *ctx)
 {
@@ -110,12 +135,13 @@ void debug_trap(struct registers *ctx)
         panic("Debug trap");
     }
 
-    struct thread *current = get_current_thread();
-
+    pr_info("%d trap at %lx\n", get_current_process()->pid_, ctx->rip);
+    wrmsr(MSR_IA32_DEBUGCTLMSR, DEBUGCTLMSR_LBR | DEBUGCTLMSR_BTF);
+    return;
     siginfo_t info = {};
     info.si_code = SI_KERNEL;
 
-    kernel_tkill(SIGTRAP, current, SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGTRAP, SIGNAL_FORCE, &info);
 }
 
 void nmi_exception(struct registers *ctx)
@@ -133,12 +159,10 @@ void overflow_trap(struct registers *ctx)
         panic("Overflow trap");
     }
 
-    struct thread *current = get_current_thread();
-
     siginfo_t info = {};
     info.si_code = SEGV_BNDERR;
 
-    kernel_tkill(SIGSEGV, current, SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGSEGV, SIGNAL_FORCE, &info);
 }
 
 void boundrange_exception(struct registers *ctx)
@@ -149,12 +173,10 @@ void boundrange_exception(struct registers *ctx)
         panic("Bound range trap");
     }
 
-    struct thread *current = get_current_thread();
-
     siginfo_t info = {};
     info.si_code = SI_KERNEL;
 
-    kernel_tkill(SIGILL, current, SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGILL, SIGNAL_FORCE, &info);
 }
 
 bool handle_bug(struct registers *ctx);
@@ -168,12 +190,10 @@ void invalid_opcode_exception(struct registers *ctx)
         panic("Invalid instruction exception");
     }
 
-    struct thread *current = get_current_thread();
-
     siginfo_t info = {};
     info.si_code = SI_KERNEL;
 
-    kernel_tkill(SIGILL, current, SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGILL, SIGNAL_FORCE, &info);
 }
 
 void device_not_avail_excp(struct registers *ctx)
@@ -187,12 +207,10 @@ void device_not_avail_excp(struct registers *ctx)
         panic("FPU exception");
     }
 
-    struct thread *current = get_current_thread();
-
     siginfo_t info = {};
     info.si_code = SI_KERNEL;
 
-    kernel_tkill(SIGFPE, current, SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGFPE, SIGNAL_FORCE, &info);
 }
 void __double_fault(struct registers *ctx)
 {
@@ -214,12 +232,10 @@ void stack_segment_fault(struct registers *ctx)
         panic("Stack segment fault");
     }
 
-    struct thread *current = get_current_thread();
-
     siginfo_t info = {};
     info.si_code = SEGV_BNDERR;
 
-    kernel_tkill(SIGSEGV, current, SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGSEGV, SIGNAL_FORCE, &info);
 }
 
 #ifdef CONFIG_VERBOSE_SEGV
@@ -335,7 +351,7 @@ void general_protection_fault(struct registers *ctx)
 
     dumpstack(ctx->rip, (const void *) ctx->rsp);
 
-    kernel_tkill(SIGSEGV, current, SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGSEGV, SIGNAL_FORCE, &info);
 }
 
 void stack_trace_user(uintptr_t *stack);
@@ -405,12 +421,10 @@ void x87_fpu_exception(struct registers *ctx)
         panic("FPU exception");
     }
 
-    struct thread *current = get_current_thread();
-
     siginfo_t info = {};
     info.si_code = SI_KERNEL;
 
-    kernel_tkill(SIGFPE, current, SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGFPE, SIGNAL_FORCE, &info);
 }
 
 void alignment_check_excp(struct registers *ctx)
@@ -421,12 +435,10 @@ void alignment_check_excp(struct registers *ctx)
         panic("Alignment check exception");
     }
 
-    struct thread *current = get_current_thread();
-
     siginfo_t info = {};
     info.si_code = SEGV_ACCERR;
 
-    kernel_tkill(SIGSEGV, current, SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGSEGV, SIGNAL_FORCE, &info);
 }
 
 void simd_fpu_exception(struct registers *ctx)
@@ -440,12 +452,10 @@ void simd_fpu_exception(struct registers *ctx)
         panic("FPU exception");
     }
 
-    struct thread *current = get_current_thread();
-
     siginfo_t info = {};
     info.si_code = SI_KERNEL;
 
-    kernel_tkill(SIGFPE, current, SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGFPE, SIGNAL_FORCE, &info);
 }
 
 void virtualization_exception(struct registers *ctx)
@@ -456,12 +466,10 @@ void virtualization_exception(struct registers *ctx)
         panic("Virtualization exception");
     }
 
-    struct thread *current = get_current_thread();
-
     siginfo_t info = {};
     info.si_code = SI_KERNEL;
 
-    kernel_tkill(SIGSEGV, current, SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGSEGV, SIGNAL_FORCE, &info);
 }
 
 void security_exception(struct registers *ctx)
@@ -472,12 +480,10 @@ void security_exception(struct registers *ctx)
         panic("Security exception");
     }
 
-    struct thread *current = get_current_thread();
-
     siginfo_t info = {};
     info.si_code = SI_KERNEL;
 
-    kernel_tkill(SIGSEGV, current, SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGSEGV, SIGNAL_FORCE, &info);
 }
 
 #ifdef CONFIG_KTRACE
@@ -510,12 +516,10 @@ void breakpoint_exception(struct registers *ctx)
         panic("Breakpoint exception");
     }
 
-    struct thread *current = get_current_thread();
-
     siginfo_t info = {};
     info.si_code = SI_KERNEL;
-
-    kernel_tkill(SIGTRAP, current, SIGNAL_FORCE, &info);
+    dumpstack(ctx->rip, (const void *) ctx->rsp);
+    raise_sig_curthr(SIGTRAP, SIGNAL_FORCE, &info);
 }
 
 void invalid_tss_exception(struct registers *ctx)
@@ -532,12 +536,10 @@ void segment_not_present_excp(struct registers *ctx)
         panic("Segment not present exception");
     }
 
-    struct thread *current = get_current_thread();
-
     siginfo_t info = {};
     info.si_code = SI_KERNEL;
 
-    kernel_tkill(SIGSEGV, current, SIGNAL_FORCE, &info);
+    raise_sig_curthr(SIGSEGV, SIGNAL_FORCE, &info);
 }
 
 void machine_check(struct registers *ctx)

kernel/arch/x86_64/process.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 - 2021 Pedro Falcato
+ * Copyright (c) 2020 - 2025 Pedro Falcato
  * This file is part of Onyx, and is released under the terms of the GPLv2 License
  * check LICENSE at the root directory for more information
  *
@@ -14,23 +14,20 @@
 
 #include <uapi/clone.h>
 
-struct thread *process_create_thread(struct process *proc, thread_callback_t callback,
-                                     uint32_t flags)
+namespace x86::internal
 {
-    thread_t *thread = sched_create_thread(callback, flags, nullptr);
+void thread_setup_stack(thread *thread, bool is_user, registers_t *regs);
+}
 
-    if (!thread)
-        return nullptr;
+extern "C" void ret_from_fork_asm(void);
 
-    spin_lock(&proc->thread_list_lock);
-
-    list_add_tail(&thread->thread_list_head, &proc->thread_list);
-
-    spin_unlock(&proc->thread_list_lock);
-
-    thread->owner = proc;
-
-    return thread;
+extern "C" void ret_from_fork(void)
+{
+    /* We take care of everything that needs to be done post-fork on the child process (that we
+     * couldn't really do beforehand) */
+    struct process *current = get_current_process();
+    if (current->set_tid)
+        copy_to_user(current->set_tid, &current->pid_, sizeof(pid_t));
 }
 
 struct thread *process_fork_thread(thread_t *src, struct process *dest, unsigned int flags,
@@ -73,15 +70,14 @@ struct thread *process_fork_thread(thread_t *src, struct process *dest, unsigned
     if (flags & CLONE_SETTLS)
         thread->fs = (void *) tls;
 
+    regs.rip = (unsigned long) &ret_from_fork_asm;
+    x86::internal::thread_setup_stack(thread, false, &regs);
+
     save_fpu(thread->fpu_area);
 
     thread->owner = dest;
     thread->set_aspace(dest->get_aspace());
-
-    list_add_tail(&thread->thread_list_head, &dest->thread_list);
-
-    dest->nr_threads = 1;
-
+    dest->thr = thread;
     return thread;
 }
 
@@ -97,85 +93,7 @@ int process_alloc_stack(struct stack_info *info)
     return 0;
 }
 
-struct thread *process_create_main_thread(struct process *proc, thread_callback_t callback,
-                                          void *sp)
-{
-    registers_t regs = {};
-    regs.rsp = reinterpret_cast<unsigned long>(sp);
-    regs.rip = reinterpret_cast<unsigned long>(callback);
-    regs.rflags = default_rflags;
-
-    auto t = sched_spawn_thread(&regs, 0, nullptr);
-    if (!t)
-        return nullptr;
-
-    t->owner = proc;
-
-    /* No need to lock here because the thread isn't even alive yet. */
-    list_add_tail(&t->thread_list_head, &proc->thread_list);
-
-    proc->nr_threads = 1;
-
-    return t;
-}
-
-#define CLONE_FORK        (1 << 0)
-#define CLONE_SPAWNTHREAD (1 << 1)
-long valid_flags = CLONE_FORK | CLONE_SPAWNTHREAD;
-
-struct tid_out
-{
-    /* TID is placed here */
-    pid_t *ptid;
-    /* This location is zero'd when the thread exits */
-    pid_t *ctid;
-};
-
-/* Hmmm, I don't think this is 100% correct but it's good enough */
-static void inherit_signal_flags(thread *newt)
-{
-    auto current_thread = get_current_thread();
-
-    scoped_lock g{current_thread->sinfo.lock};
-    scoped_lock g2{newt->sinfo.lock};
-
-    newt->sinfo.flags |= current_thread->sinfo.flags;
-    newt->sinfo.__update_pending();
-}
-
 int sys_clone(void *fn, void *child_stack, int flags, void *arg, struct tid_out *out, void *tls)
 {
-    struct tid_out ktid_out;
-    if (copy_from_user(&ktid_out, out, sizeof(ktid_out)) < 0)
-        return -EFAULT;
-
-    if (flags & ~valid_flags)
-        return -EINVAL;
-    if (flags & CLONE_FORK)
-        return -EINVAL; /* TODO: Add CLONE_FORK */
-    thread_callback_t start = (thread_callback_t) fn;
-
-    registers_t regs = {};
-    regs.rsp = (unsigned long) child_stack;
-    regs.rflags = default_rflags;
-    regs.rip = (unsigned long) start;
-    regs.rdi = (unsigned long) arg;
-
-    thread_t *thread = sched_spawn_thread(&regs, 0, tls);
-    if (!thread)
-        return -errno;
-
-    if (copy_to_user(ktid_out.ptid, &thread->id, sizeof(pid_t)) < 0)
-    {
-        thread_destroy(thread);
-        return -errno;
-    }
-
-    thread->ctid = ktid_out.ctid;
-
-    process_add_thread(get_current_process(), thread);
-    inherit_signal_flags(thread);
-    sched_start_thread(thread);
-
-    return 0;
+    return -ENOSYS;
 }

commit 96c31dad5e5b7ee1c50c6fc92f0b6c1f76458863

signal: fix si_status value on process stop/conts
Pass the current si_status (should be simply the signal that stopped the
task).

Fixes: d5bbf4b51c16 ("fork: Implement proper sys_clone semantics")
Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

kernel/include/onyx/signal.h
@@ -224,16 +224,6 @@ void signal_setmask(const sigset_t *mask);
  */
 bool parent_notify(unsigned int exit_code);
 
-/**
- * @brief Notify this task's parent that we're stopping/continuing
- * We have to be careful and check if we need to, e.g, not send anything. tasklist read_lock needs
- * to be held when calling.
- *
- * @param exit_code Stop code to notify with
- * @retval true If task was woken up
- */
-bool notify_process_stop_cont(struct process *task, unsigned int exit_code);
-
 void force_sigsegv(int sig);
 
 /* Used when forcing signals, such that no one racing with us can change this signal while another

kernel/kernel/signal.c
@@ -24,6 +24,7 @@
 
 #include <uapi/signal.h>
 
+static bool notify_process_stop_cont(struct process *task, unsigned int code);
 static int send_signal_to_task(int signal, struct process *task, unsigned int flags,
                                siginfo_t *info, enum pid_type type);
 
@@ -310,7 +311,7 @@ static void do_signal_stop(int signo)
         /* Ok, group stop finished (either we're a single thread, or the last thread in the group
          * stop). Notify the parent */
         read_lock(&tasklist_lock);
-        notify_process_stop_cont(current, sig->signal_group_exit_code);
+        notify_process_stop_cont(current, CLD_STOPPED);
         read_unlock(&tasklist_lock);
     }
 
@@ -329,7 +330,7 @@ static void do_sigcont_notify(void)
 
     if (sig->signal_group_flags & SIGNAL_GROUP_CONT_PENDING)
     {
-        notify_process_stop_cont(current, W_CONTINUED);
+        notify_process_stop_cont(current, CLD_CONTINUED);
         did = true;
     }
 
@@ -1477,10 +1478,11 @@ bool parent_notify(unsigned int exit_code)
  * We have to be careful and check if we need to, e.g, not send anything. tasklist read_lock needs
  * to be held when calling.
  *
- * @param exit_code Stop code to notify with
+ * @param task task that's stopping
+ * @param code si_code (CLD_*)
  * @retval true If task was woken up
  */
-bool notify_process_stop_cont(struct process *task, unsigned int exit_code)
+static bool notify_process_stop_cont(struct process *task, unsigned int code)
 {
     struct process *parent = rcu_dereference_protected(task->parent, lockdep_tasklist_lock_held());
     struct sighand_struct *sighand = parent->sighand;
@@ -1494,15 +1496,21 @@ bool notify_process_stop_cont(struct process *task, unsigned int exit_code)
     info.si_uid = task->cred.ruid;
     tg_cputime_clock_t(current, &info.si_utime, &info.si_stime);
 
-    if (WIFEXITED(exit_code))
+    info.si_code = code;
+    switch (code)
     {
-        info.si_code = CLD_EXITED;
-        info.si_status = WEXITSTATUS(exit_code);
-    }
-    else if (WIFSIGNALED(exit_code))
-    {
-        info.si_code = CLD_KILLED;
-        info.si_status = WTERMSIG(exit_code);
+        case CLD_STOPPED:
+            info.si_status = task->sig->signal_group_exit_code;
+            break;
+        case CLD_CONTINUED:
+            info.si_status = SIGCONT;
+            break;
+        case CLD_TRAPPED:
+            info.si_status = task->exit_code;
+            break;
+        default:
+            WARN_ON(1);
+            break;
     }
 
     /* Take the parent's signal_lock. We're going to atomically check if need to send a signal or